5475946249eb6584f73a2bd36748603d5c078534
Leverson Carlos <lcjohnny@hotmail.com>
2019-07-28 12:43:44 -0300

    Clear branch folders

--- srcs/FriendCash.Web/Content/knockout/knockout-notifyer.js
+++
@@ -1,8 +0,0 @@
-﻿ko.bindingHandlers.notifyer = {
-   init: function (element, valueAccessor, allBindingsAccessor, data, context) {
-   },
-   update: function (element, valueAccessor, allBindingsAccessor, data, context) {
-      var unwrapAccessor = ko.utils.unwrapObservable(valueAccessor());  // unwrap to get subscription
-      unwrapAccessor(element);
-   }
-};
