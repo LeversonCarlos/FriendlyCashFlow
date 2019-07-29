7789d2a6f419b36195375894ede046e433c8c9d6
Leverson Carlos <lcjohnny@hotmail.com>
2019-07-29 15:09:04 -0300

    spa services

--- srcs/Startup.cs
+++ srcs/Startup.cs
@@ -1,8 +1,5 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -20,25 +17,19 @@ namespace FriendlyCashFlow
       public void ConfigureServices(IServiceCollection services)
       {
          this.AddMvcServices(services);
-
-         // In production, the Angular files will be served from this directory
-         services.AddSpaStaticFiles(configuration =>
-         {
-            configuration.RootPath = "ClientApp/dist";
-         });
+         this.AddSpaServices(services);
       }
 
-      // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
       public void Configure(IApplicationBuilder app, IHostingEnvironment env)
       {
          if (env.IsDevelopment())
-         {
-            app.UseDeveloperExceptionPage();
-         }
+         { app.UseDeveloperExceptionPage(); }
          else
          {
             app.UseExceptionHandler("/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            // The default HSTS value is 30 days.
+            // You may want to change this for production scenarios,
+            // see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
          }
 
@@ -46,19 +37,7 @@ namespace FriendlyCashFlow
          app.UseStaticFiles();
          app.UseSpaStaticFiles();
          this.UseMvcServices(app, env);
-
-         app.UseSpa(spa =>
-         {
-            // To learn more about options for serving an Angular SPA from ASP.NET Core,
-            // see https://go.microsoft.com/fwlink/?linkid=864501
-
-            spa.Options.SourcePath = "ClientApp";
-
-            if (env.IsDevelopment())
-            {
-               spa.UseAngularCliServer(npmScript: "start");
-            }
-         });
+         this.UseSpaServices(app, env);
       }
    }
 }
