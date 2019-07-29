d85fd4b4c9c564c73dfe33b1d336362fb8b3b1e6
Leverson Carlos <lcjohnny@hotmail.com>
2019-07-29 16:13:04 -0300

    appSettings

--- srcs/ServerApi/Shared/dbContext.cs
+++ srcs/ServerApi/Shared/dbContext.cs
@@ -10,5 +10,10 @@ namespace FriendlyCashFlow.API.Shared
       public dbContext(DbContextOptions<dbContext> options, IServiceProvider _serviceProvider) : base(options)
       { this.serviceProvider = _serviceProvider; }
 
+      /*
+         dotnet ef migrations add Initial
+         dotnet ef database update
+       */
+
    }
 }
