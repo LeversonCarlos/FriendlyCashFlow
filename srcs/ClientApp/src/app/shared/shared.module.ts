d01aa70ccddd8a049840007f381759dee995fc27
Leverson Carlos <lcjohnny@hotmail.com>
2019-07-30 18:48:29 -0300

    full layout

--- srcs/ClientApp/src/app/shared/shared.module.ts
+++ srcs/ClientApp/src/app/shared/shared.module.ts
@@ -3,15 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BaseLayoutComponent } from './base-layout/base-layout.component';
+import { FullLayoutComponent } from './full-layout/full-layout.component';
 
 @NgModule({
-   declarations: [BaseLayoutComponent],
+   declarations: [BaseLayoutComponent, FullLayoutComponent],
    imports: [
       CommonModule,
       MatToolbarModule
    ],
    exports: [
-      MatToolbarModule, BaseLayoutComponent
+      MatToolbarModule, BaseLayoutComponent, FullLayoutComponent
    ]
 })
 export class SharedModule { }
