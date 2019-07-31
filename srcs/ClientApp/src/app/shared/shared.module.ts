dc4118491a568ba37e7717f4532b91b7d05be08d
Leverson Carlos <lcjohnny@hotmail.com>
2019-07-31 09:32:01 -0300

    material tabs

--- srcs/ClientApp/src/app/shared/shared.module.ts
+++ srcs/ClientApp/src/app/shared/shared.module.ts
@@ -6,6 +6,7 @@ import 'hammerjs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -18,11 +19,11 @@ import { SideNavComponent } from './side-nav/side-nav.component';
    declarations: [BaseLayoutComponent, FullLayoutComponent, SideNavComponent],
    imports: [
       CommonModule, RouterModule,
-      MatToolbarModule, MatSidenavModule, MatCardModule, MatListModule,
+      MatToolbarModule, MatSidenavModule, MatCardModule, MatTabsModule, MatListModule,
       MatIconModule, MatButtonModule
    ],
    exports: [
-      MatToolbarModule, MatSidenavModule, MatCardModule, MatListModule,
+      MatToolbarModule, MatSidenavModule, MatCardModule, MatTabsModule, MatListModule,
       MatIconModule, MatButtonModule,
       BaseLayoutComponent, FullLayoutComponent
    ]
