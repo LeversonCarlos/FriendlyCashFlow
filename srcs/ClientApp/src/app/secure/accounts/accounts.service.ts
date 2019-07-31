d260cca6b02375d90d5647d48e38e8d75f883260
Leverson Carlos <lcjohnny@hotmail.com>
2019-07-31 09:59:47 -0300

    icon property for accounts

--- srcs/ClientApp/src/app/secure/accounts/accounts.service.ts
+++ srcs/ClientApp/src/app/secure/accounts/accounts.service.ts
@@ -1,12 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
+export enum enAccountType { General = 0, Bank = 1, CreditCard = 2, Investment = 3, Service = 4 };
 export class Account {
    AccountID: number;
    Text: string;
-   Type: number;
+   Type: enAccountType;
    DueDay?: number;
    Active: boolean;
+   get Icon(): string {
+      switch (this.Type) {
+         case enAccountType.Bank:
+            return 'account_balance';
+         case enAccountType.CreditCard:
+            return 'credit_card';
+         case enAccountType.Investment:
+            return 'local_atm';
+         case enAccountType.Service:
+            return 'card_giftcard';
+         default:
+            return 'account_balance_wallet';
+      }
+   }
 }
 
 @Injectable({
@@ -18,7 +34,9 @@ export class AccountsService {
 
    public async getAccounts(): Promise<Account[]> {
       try {
-         const dataList = await this.http.get<Account[]>("api/accounts").toPromise();
+         const dataList = await this.http.get<Account[]>("api/accounts")
+            .pipe(map(items => items.map(item => Object.assign(new Account, item))))
+            .toPromise();
          return dataList;
       }
       catch (ex) { console.error(ex); return null; }
