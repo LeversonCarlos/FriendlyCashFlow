8c8ea8038566fc30eadb9c1f394c73379ee140ad
Leverson Carlos <lcjohnny@hotmail.com>
2019-08-01 17:36:53 -0300

    wip

--- srcs/ClientApp/src/app/secure/accounts/account-details/account-details.component.ts
+++ srcs/ClientApp/src/app/secure/accounts/account-details/account-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { AccountsService } from '../accounts.service';
+import { AccountsService, Account } from '../accounts.service';
 
 @Component({
    selector: 'fs-account-details',
