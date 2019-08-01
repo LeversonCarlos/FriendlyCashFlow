0a7ea3fc0697137b494d74b5709c042b472fd582
Leverson Carlos <lcjohnny@hotmail.com>
2019-08-01 15:59:42 -0300

    account detais

--- srcs/ClientApp/src/app/secure/accounts/account-details/account-details.component.ts
+++ srcs/ClientApp/src/app/secure/accounts/account-details/account-details.component.ts
@@ -11,13 +11,13 @@ export class AccountDetailsComponent implements OnInit {
 
    constructor(private service: AccountsService, private route: ActivatedRoute) { }
 
+   public Data: Account;
+
    public async ngOnInit() {
       const accountID: number = Number(this.route.snapshot.params.id)
       if (!accountID || accountID == 0) { console.error('todo'); return; }
 
-      const account = await this.service.getAccount(accountID);
-      if (!account) { console.error('todo'); return; }
-      console.log(account);
+      this.Data = await this.service.getAccount(accountID);
    }
 
 }
