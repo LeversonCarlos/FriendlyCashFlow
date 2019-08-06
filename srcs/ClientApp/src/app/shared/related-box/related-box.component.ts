b3bc55c04c15d554fb01e6a988b537be597d605d
Leverson Carlos <lcjohnny@hotmail.com>
2019-08-06 08:16:25 -0300

    related box

--- srcs/ClientApp/src/app/shared/related-box/related-box.component.ts
+++ srcs/ClientApp/src/app/shared/related-box/related-box.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ControlValueAccessor } from '@angular/forms';
 
 @Component({
    selector: 'related-box',
    templateUrl: './related-box.component.html',
    styleUrls: ['./related-box.component.scss']
 })
-export class RelatedBoxComponent implements OnInit, OnDestroy {
+export class RelatedBoxComponent implements OnInit, OnDestroy, ControlValueAccessor {
 
    constructor() { }
 
    public ngOnInit() {
+   }
+
+   writeValue(obj: any): void {
+      throw new Error("Method not implemented.");
+   }
+   registerOnChange(fn: any): void {
+      throw new Error("Method not implemented.");
+   }
+   registerOnTouched(fn: any): void {
+      throw new Error("Method not implemented.");
+   }
+   setDisabledState?(isDisabled: boolean): void {
+      throw new Error("Method not implemented.");
    }
 
    public ngOnDestroy(): void {
