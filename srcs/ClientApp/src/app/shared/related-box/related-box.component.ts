86c0251bfee1c5240db6af0baf78007d784203eb
Leverson Carlos <lcjohnny@hotmail.com>
2019-08-06 09:15:53 -0300

    ControlValueAccessor

--- srcs/ClientApp/src/app/shared/related-box/related-box.component.ts
+++ srcs/ClientApp/src/app/shared/related-box/related-box.component.ts
@@ -1,33 +1,59 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ControlValueAccessor } from '@angular/forms';
+import { Component, OnInit, OnDestroy, Input, Optional, Self, ElementRef } from '@angular/core';
+import { ControlValueAccessor, NgControl } from '@angular/forms';
+import { fromEvent, Observable } from 'rxjs';
+import { map, debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { MatFormFieldControl } from '@angular/material/form-field';
 
 @Component({
    selector: 'related-box',
    templateUrl: './related-box.component.html',
-   styleUrls: ['./related-box.component.scss']
+   styleUrls: ['./related-box.component.scss'],
+   providers: [{ provide: MatFormFieldControl, useExisting: RelatedBoxComponent }]
 })
-export class RelatedBoxComponent implements OnInit, OnDestroy, ControlValueAccessor {
+export class RelatedBoxComponent implements OnInit, OnDestroy, ControlValueAccessor, MatFormFieldControl<string> {
 
-   constructor() { }
+   constructor(@Optional() @Self() private ngControl: NgControl, private elRef: ElementRef<HTMLElement>) {
+      if (this.ngControl != null) {
+         this.ngControl.valueAccessor = this;
+      }
+   }
 
    public ngOnInit() {
+      this.inputValue = this.value;
+      this.eventStream = fromEvent(this.elRef.nativeElement, 'keyup')
+         .pipe(
+            map(() => this.inputValue),
+            debounceTime(this.delay),
+            distinctUntilChanged()
+         );
+      this.eventStream.subscribe(val => this.writeValue(val))
    }
 
-   writeValue(obj: any): void {
-      throw new Error("Method not implemented.");
+   /* PROPERTIES */
+   @Input() public delay: number = 500;
+   @Input() public value: string;
+   public inputValue: string
+   private eventStream: Observable<string>;
+
+   /* VALUE ACCESSOR  */
+   writeValue(val: string): void {
+      this.value = val
+      this.onChange(val);
    }
+   onChange = (val: string) => { };
    registerOnChange(fn: any): void {
-      throw new Error("Method not implemented.");
+      this.onChange = fn;
    }
    registerOnTouched(fn: any): void {
-      throw new Error("Method not implemented.");
+      // throw new Error("Method not implemented.");
    }
    setDisabledState?(isDisabled: boolean): void {
-      throw new Error("Method not implemented.");
+      // throw new Error("Method not implemented.");
    }
 
+   // DESTROY
    public ngOnDestroy(): void {
-      throw new Error("Method not implemented.");
+      this.eventStream = null;
    }
 
 }
