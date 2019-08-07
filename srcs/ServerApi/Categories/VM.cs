b5e9d219d5e33d8dc76c6382cfa9b5a1ddbbdeec
Leverson Carlos <lcjohnny@hotmail.com>
2019-08-07 09:13:27 -0300

    account vm

--- srcs/ServerApi/Categories/VM.cs
+++ srcs/ServerApi/Categories/VM.cs
@@ -9,4 +9,38 @@ namespace FriendlyCashFlow.API.Categories
       public string Text { get; set; }
    }
 
+   public class CategoryVM
+   {
+
+      public long CategoryID { get; set; }
+      public string Text { get; set; }
+      public enCategoryType Type { get; set; }
+      public long? ParentID { get; set; }
+      public string HierarchyText { get; set; }
+      public string[] SplitedText { get; set; }
+
+      internal static CategoryVM Convert(CategoryData value)
+      {
+         var result = new CategoryVM
+         {
+            CategoryID = value.CategoryID,
+            Text = value.Text,
+            Type = (enCategoryType)value.Type,
+            ParentID = value.ParentID,
+            HierarchyText = value.HierarchyText
+         };
+         if (!string.IsNullOrEmpty(result.HierarchyText))
+         {
+            result.SplitedText = result.HierarchyText.Split(" / ", System.StringSplitOptions.RemoveEmptyEntries);
+            if (result.SplitedText.Length > 1)
+            {
+               for (int i = 0; i < result.SplitedText.Length - 2; i++)
+               { result.SplitedText[i] = ""; }
+            }
+         }
+         return result;
+      }
+
+   }
+
 }
