047b6cb11927c5eadf8641bb7602e9827c901f6e
Leverson Carlos <lcjohnny@hotmail.com>
2019-08-10 12:16:40 -0300

    some category validation

--- srcs/ServerApi/Categories/SERVICE.ValidateData.cs
+++ srcs/ServerApi/Categories/SERVICE.ValidateData.cs
@@ -1,5 +1,6 @@
 
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -15,8 +16,27 @@ namespace FriendlyCashFlow.API.Categories
          {
 
             // VALIDATE DUPLICITY
-            if (await this.GetDataQuery().CountAsync(x => x.CategoryID != value.CategoryID && x.Text == value.Text) != 0)
+            if (await this.GetDataQuery().CountAsync(x => x.CategoryID != value.CategoryID && x.ParentID == x.ParentID && x.Text == value.Text) != 0)
             { return this.WarningResponse(this.GetTranslation("CATEGORIES_CATEGORY_TEXT_ALREADY_EXISTS_WARNING")); }
+
+            // CHECK HIERARCHY BREAK
+            if (value.CategoryID > 0)
+            {
+               var hierarchyText = await this.GetDataQuery().Where(x => x.CategoryID == value.CategoryID).Select(x => x.HierarchyText).FirstOrDefaultAsync();
+
+               // CHECK IF WE ARE DEFINING ANY CHILD AS A PARENT
+               var foundChildren = await this.GetDataQuery()
+                  .Where(x => x.CategoryID == value.ParentID && x.HierarchyText.Contains(hierarchyText))
+                  .AnyAsync();
+               if (foundChildren) { return this.WarningResponse(this.GetTranslation("CATEGORIES_CHILDREN_CANT_BE_DEFINED_AS_PARENT_CATEGORY_WARNING")); }
+
+               // CHECK IF WE ARE DEFINING A PARENT AS A CHLID
+               var foundParent = await this.GetDataQuery()
+                  .Where(x => x.CategoryID == value.ParentID && hierarchyText.Contains(x.HierarchyText))
+                  .AnyAsync();
+               if (foundChildren) { return this.WarningResponse(this.GetTranslation("CATEGORIES_PARENT_CANT_BE_DEFINED_AS_CHILD_CATEGORY_WARNING")); }
+
+            }
 
             // RESULT
             return this.OkResponse(true);
