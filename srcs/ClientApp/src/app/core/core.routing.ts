09067a70267c38b0ddfd0a0af757e56072de255a
Leverson Carlos <lcjohnny@hotmail.com>
2019-08-20 10:42:43 -0300

    unguard

--- srcs/ClientApp/src/app/core/core.routing.ts
+++ srcs/ClientApp/src/app/core/core.routing.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './home/home.component';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
 import { SignInComponent } from './auth/sign-in/sign-in.component';
 import { ActivateComponent } from './auth/activate/activate.component';
+import { AuthGuard } from './auth/auth.guard';
+import { AuthUnguard } from './auth/auth.unguard';
 
 export const coreRoutes: Routes = [
    { path: '', redirectTo: 'home', pathMatch: 'full' },
-   { path: 'home', component: HomeComponent },
-   { path: 'signup', component: SignUpComponent },
-   { path: 'signin', component: SignInComponent },
+   { path: 'home', canActivate: [AuthGuard], component: HomeComponent },
+   { path: 'signup', canActivate: [AuthUnguard], component: SignUpComponent },
+   { path: 'signin', canActivate: [AuthUnguard], component: SignInComponent },
    { path: 'activate/:id/:code', component: ActivateComponent },
 ];
