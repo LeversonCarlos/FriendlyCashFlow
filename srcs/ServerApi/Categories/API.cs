b6f99453653f2121cde3b20486d3dc5d7c5cecbe
Leverson Carlos <lcjohnny@hotmail.com>
2019-08-20 08:43:31 -0300

    RemoveDataAsync

--- srcs/ServerApi/Categories/API.cs
+++ srcs/ServerApi/Categories/API.cs
@@ -12,16 +12,6 @@ namespace FriendlyCashFlow.API.Categories
    public partial class CategoriesController : Base.BaseController
    {
       public CategoriesController(IServiceProvider _serviceProvider) : base(_serviceProvider) { }
-
-
-      [HttpDelete("{id:long}")]
-      [Authorize(Roles = "Editor")]
-      public async Task<ActionResult<bool>> RemoveDataAsync(long id)
-      {
-         using (var service = new CategoriesService(this.serviceProvider))
-         { return await service.RemoveDataAsync(id); }
-      }
-
    }
 
 }
