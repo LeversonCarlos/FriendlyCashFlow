e6d5faae38538ed7d83c5b98744caca912891bda
Leverson Carlos <lcjohnny@hotmail.com>
2019-08-20 13:25:46 -0300

    disabling unavaliabled menus

--- srcs/ClientApp/src/app/core/home/home.component.ts
+++ srcs/ClientApp/src/app/core/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
    selector: 'fs-home',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-   constructor() { }
+   constructor(public auth: AuthService ) { }
 
    public async ngOnInit() {
+
    }
 
 }
