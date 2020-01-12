aab0101592399399ec69612d8b14f3e0d760b359
Leverson Carlos <lcjohnny@hotmail.com>
2020-01-12 12:32:27 -0300

    transfer api

---
+++ srcs/ServerApi/Transfers/API.Get.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FriendlyCashFlow.API.Transfers
+{
+
+   partial class TransfersService
+   {
+
+      internal async Task<ActionResult<TransferVM>> GetDataAsync(string transferID)
+      {
+         try
+         {
+
+            // LOAD ENTRIES
+            var user = this.GetService<Helpers.User>();
+            var entries = await this.dbContext.Entries
+               .Where(x => x.RowStatus == (short)Base.enRowStatus.Active && x.ResourceID == user.ResourceID && x.TransferID == transferID)
+               .ToListAsync();
+            if (entries == null || entries.Count != 0) { return this.WarningResponse("ENTRIES_RECORD_NOT_FOUND_WARNING"); }
+
+            // INIT VIEW MODEL
+            var viewModel = new TransferVM
+            {
+               TransferID = transferID,
+               TransferDate = entries[0].DueDate,
+               TransferValue = Math.Abs(entries[0].EntryValue)
+            };
+
+            // EXPENSE
+            var expenseEntry = entries.Where(x => x.Type == (short)Categories.enCategoryType.Expense).FirstOrDefault();
+            if (expenseEntry == null) { return this.WarningResponse("ENTRIES_RECORD_NOT_FOUND_WARNING"); }
+            viewModel.ExpenseEntryID = expenseEntry.EntryID;
+            if (expenseEntry.AccountID.HasValue)
+            {
+               var accountMessage = await this.GetService<Accounts.AccountsService>().GetDataAsync(expenseEntry.AccountID.Value);
+               viewModel.ExpenseAccountRow = this.GetValue(accountMessage);
+               viewModel.ExpenseAccountID = expenseEntry.AccountID.Value;
+            }
+
+            // INCOME
+            var incomeEntry = entries.Where(x => x.Type == (short)Categories.enCategoryType.Income).FirstOrDefault();
+            if (incomeEntry == null) { return this.WarningResponse("ENTRIES_RECORD_NOT_FOUND_WARNING"); }
+            viewModel.IncomeEntryID = incomeEntry.EntryID;
+            viewModel.IncomeAccountID = incomeEntry.AccountID.Value;
+            if (expenseEntry.AccountID.HasValue)
+            {
+               var accountMessage = await this.GetService<Accounts.AccountsService>().GetDataAsync(incomeEntry.AccountID.Value);
+               viewModel.IncomeAccountRow = this.GetValue(accountMessage);
+               viewModel.IncomeAccountID = incomeEntry.AccountID.Value;
+            }
+
+            return this.OkResponse(viewModel);
+         }
+         catch (Exception ex) { return this.ExceptionResponse(ex); }
+      }
+
+   }
+
+   partial class TransfersController
+   {
+
+      [HttpGet("{id}")]
+      public async Task<ActionResult<TransferVM>> GetDataAsync(string id)
+      {
+         return await this.GetService<TransfersService>().GetDataAsync(id);
+      }
+
+   }
+
+}
--- srcs/ServerApi/Transfers/VMs.cs
+++ srcs/ServerApi/Transfers/VMs.cs
@@ -11,10 +11,12 @@ namespace FriendlyCashFlow.API.Transfers
 
       public long IncomeEntryID { get; set; }
       public long IncomeAccountID { get; set; }
+      public Accounts.AccountVM IncomeAccountRow { get; set; }
       // public string IncomeAccountText { get; set; }
 
       public long ExpenseEntryID { get; set; }
       public long ExpenseAccountID { get; set; }
+      public Accounts.AccountVM ExpenseAccountRow { get; set; }
       // public string ExpenseAccountText { get; set; }
    }
 
