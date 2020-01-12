866b38ba57dd70d77ce90bc4cca3337354f76792
Leverson Carlos <lcjohnny@hotmail.com>
2020-01-12 18:23:46 -0300

    month picker

--- srcs/ClientApp/src/app/secure/entries/entries-flow/entries-flow.component.ts
+++ srcs/ClientApp/src/app/secure/entries/entries-flow/entries-flow.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EntriesService } from '../entries.service';
 import { EntryFlow, Entry } from '../entries.viewmodels';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
    selector: 'fs-entries-flow',
@@ -9,13 +10,63 @@ import { EntryFlow, Entry } from '../entries.viewmodels';
 })
 export class EntriesFlowComponent implements OnInit {
 
-   constructor(private service: EntriesService) { }
-
+   constructor(private service: EntriesService, private route: ActivatedRoute) { }
    public get FlowList(): EntryFlow[] { return this.service.FlowList }
 
+
+   /* INIT */
    public ngOnInit() {
+      try {
+
+         let year: number = Number(this.route.snapshot.params.year);
+         let month: number = Number(this.route.snapshot.params.month);
+         let account: number = Number(this.route.snapshot.params.account);
+
+         // DEFAULT YEAR/MONTH
+         if (
+            (!year || isNaN(year) || year < 1901 || year > 3000) ||
+            (!month || isNaN(month) || month < 1 || month > 12)
+         ) {
+            const date = new Date();
+            year = date.getFullYear();
+            month = date.getMonth() + 1;
+            this.service.showFlow(year, month);
+            return;
+         }
+
+         this.CurrentAccount = account;
+         this.CurrentMonth = new Date(`${year.toString().padStart(4, "20")}-${(month + 0).toString().padStart(2, "0")}-01 12:00:00`);
+
+      }
+      catch (ex) { console.error(ex); }
    }
 
+
+   /* CURRENT ACCOUNT */
+   public get CurrentAccount(): number {
+      return this.service.CurrentData.CurrentAccount || 0;
+   }
+   public set CurrentAccount(val: number) {
+      this.service.CurrentData.CurrentAccount = val;
+   }
+
+
+   /* CURRENT MONTH */
+   public get CurrentMonth(): Date {
+      return this.service.CurrentData.CurrentMonth;
+   }
+   public set CurrentMonth(val: Date) {
+      console.log('currentMonth', val)
+      this.service.CurrentData.setFlow(val, this.CurrentAccount);
+      const currentYear = this.service.CurrentData.CurrentMonth.getFullYear()
+      const currentMonth = this.service.CurrentData.CurrentMonth.getMonth() + 1
+      this.service
+         .loadFlowList(currentYear, currentMonth, this.CurrentAccount)
+         .then(() => this.service.showCurrentList());
+   }
+
+
+   /* ITEM CLICK */
    public OnItemClick(item: Entry) {
       if (item.TransferID && item.TransferID != '') {
          this.service.showTransferDetails(item.TransferID)
--- srcs/ClientApp/src/app/secure/entries/entries-flow/month-picker/month-picker.component.ts
+++ srcs/ClientApp/src/app/secure/entries/entries-flow/month-picker/month-picker.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { EntriesService } from '../../entries.service';
+import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
 
 @Component({
    selector: 'fs-month-picker',
@@ -9,50 +7,30 @@ import { EntriesService } from '../../entries.service';
 })
 export class MonthPickerComponent implements OnInit {
 
-   constructor(private service: EntriesService, private route: ActivatedRoute) { }
+   constructor() { }
 
+   /* INIT */
    public ngOnInit() {
-      try {
-         let year: number = Number(this.route.snapshot.params.year);
-         let month: number = Number(this.route.snapshot.params.month);
-         let account: number = Number(this.route.snapshot.params.account);
-
-         if (
-            (!year || isNaN(year) || year < 1901 || year > 3000) ||
-            (!month || isNaN(month) || month < 1 || month > 12)
-         ) {
-            const date = new Date();
-            year = date.getFullYear();
-            month = date.getMonth() + 1;
-            this.service.showFlow(year, month);
-            return;
-         }
-
-         this.service.CurrentData.CurrentAccount = account;
-         this.SetCurrentMonth(new Date(`${year.toString().padStart(4, "20")}-${(month + 0).toString().padStart(2, "0")}-01 12:00:00`));
-      }
-      catch{ return; }
    }
 
+   /* CURRENT MONTH */
+   private currentMonth: Date
    public get CurrentMonth(): Date {
-      return this.service.CurrentData.CurrentMonth;
+      return this.currentMonth;
    }
-   public async SetCurrentMonth(val: Date) {
+   @Input() public set CurrentMonth(val: Date) {
       this.PreviousMonth = new Date(new Date(val).setMonth(val.getMonth() - 1));
-      this.service.CurrentData.setFlow(val, this.service.CurrentData.CurrentAccount);
+      this.currentMonth = val;
       this.NextMonth = new Date(new Date(val).setMonth(val.getMonth() + 1));
-
-      const currentYear = this.service.CurrentData.CurrentMonth.getFullYear()
-      const currentMonth = this.service.CurrentData.CurrentMonth.getMonth() + 1
-      const currentAccount = this.service.CurrentData.CurrentAccount || 0
-      await this.service.loadFlowList(currentYear, currentMonth, currentAccount);
-
-      this.service.showCurrentList()
    }
+   @Output() public CurrentMonthChanged: EventEmitter<Date> = new EventEmitter<Date>()
 
+   /* PREVIOUS MONTH */
    public PreviousMonth: Date;
-   public OnPreviousMonthClick() { this.SetCurrentMonth(this.PreviousMonth); }
+   public OnPreviousMonthClick() { this.CurrentMonth = this.PreviousMonth; this.CurrentMonthChanged.emit(this.CurrentMonth); }
+
+   /* NEXT MONTH */
    public NextMonth: Date;
-   public OnNextMonthClick() { this.SetCurrentMonth(this.NextMonth); }
+   public OnNextMonthClick() { this.CurrentMonth = this.NextMonth; this.CurrentMonthChanged.emit(this.CurrentMonth); }
 
 }
