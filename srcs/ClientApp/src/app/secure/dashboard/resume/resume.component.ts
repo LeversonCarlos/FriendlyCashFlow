06a90ca29d5a3622c20a024f74582a72bf043dfa
Leverson Carlos <lcjohnny@hotmail.com>
2020-01-23 12:35:56 -0300

    resume

--- srcs/ClientApp/src/app/secure/dashboard/resume/resume.component.ts
+++ srcs/ClientApp/src/app/secure/dashboard/resume/resume.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DashboardService } from '../dashboard.service';
 import { AppInsightsService } from 'src/app/shared/app-insights/app-insights.service';
 import { enCategoryType } from '../../categories/categories.service';
+import { enAccountType } from '../../accounts/accounts.viewmodels';
 
 class ResumeVM {
    Text: string
@@ -23,11 +24,21 @@ export class ResumeComponent implements OnInit {
 
    public async ngOnInit() {
       try {
-         const accountBalances = await this.dashboardService.getBalances();
 
          let income = Object.assign(new ResumeVM, { Text: 'DASHBOARD_RESUME_INCOME_TEXT', Value: 10, Type: enCategoryType.Income, Icon: 'add_circle' });
          let expense = Object.assign(new ResumeVM, { Text: 'DASHBOARD_RESUME_EXPENSE_TEXT', Value: 3, Type: enCategoryType.Expense, Icon: 'remove_circle' });
          let balance = Object.assign(new ResumeVM, { Text: 'DASHBOARD_RESUME_BALANCE_TEXT', Value: 7, Type: enCategoryType.None, Icon: 'monetization_on' });
+
+         let accountBalances = await this.dashboardService.getBalances();
+         if (accountBalances) {
+            for (let index = 0; index < accountBalances.length; index++) {
+               const account = accountBalances[index];
+               income.Value += account.IncomeForecast
+               expense.Value += account.ExpenseForecast
+               balance.Value += account.IncomeForecast
+               balance.Value += account.ExpenseForecast
+            }
+         }
 
          this.ResumeList = [income, expense, balance]
       }
