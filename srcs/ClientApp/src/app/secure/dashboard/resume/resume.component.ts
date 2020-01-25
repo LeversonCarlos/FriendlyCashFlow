fc5e6cb0ac12cbb342d4b55fd7b7acf47be63d67
Leverson Carlos <lcjohnny@hotmail.com>
2020-01-25 11:11:31 -0300

    we were initilizing the balance resume values

--- srcs/ClientApp/src/app/secure/dashboard/resume/resume.component.ts
+++ srcs/ClientApp/src/app/secure/dashboard/resume/resume.component.ts
@@ -25,9 +25,9 @@ export class ResumeComponent implements OnInit {
    public async ngOnInit() {
       try {
 
-         let income = Object.assign(new ResumeVM, { Text: 'DASHBOARD_RESUME_INCOME_TEXT', Value: 10, Type: enCategoryType.Income, Icon: 'add_circle' });
-         let expense = Object.assign(new ResumeVM, { Text: 'DASHBOARD_RESUME_EXPENSE_TEXT', Value: 3, Type: enCategoryType.Expense, Icon: 'remove_circle' });
-         let balance = Object.assign(new ResumeVM, { Text: 'DASHBOARD_RESUME_BALANCE_TEXT', Value: 7, Type: enCategoryType.None, Icon: 'monetization_on' });
+         let income = Object.assign(new ResumeVM, { Text: 'DASHBOARD_RESUME_INCOME_TEXT', Value: 0, Type: enCategoryType.Income, Icon: 'add_circle' });
+         let expense = Object.assign(new ResumeVM, { Text: 'DASHBOARD_RESUME_EXPENSE_TEXT', Value: 0, Type: enCategoryType.Expense, Icon: 'remove_circle' });
+         let balance = Object.assign(new ResumeVM, { Text: 'DASHBOARD_RESUME_BALANCE_TEXT', Value: 0, Type: enCategoryType.None, Icon: 'monetization_on' });
 
          let accountBalances = await this.dashboardService.getBalances();
          if (accountBalances) {
