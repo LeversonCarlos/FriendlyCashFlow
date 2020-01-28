f7a2221a9c51e593ffbd17ad1dfa7520d90695aa
Leverson Carlos <lcjohnny@hotmail.com>
2020-01-28 18:37:43 -0300

    handling 401 refresh tokens with concurrency

--- srcs/ClientApp/src/app/core/interceptors/auth.interceptor.ts
+++ srcs/ClientApp/src/app/core/interceptors/auth.interceptor.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError, of, BehaviorSubject } from 'rxjs';
-import { catchError, map, switchMap } from 'rxjs/operators';
+import { catchError, filter, switchMap, take } from 'rxjs/operators';
 import { AuthService } from '../auth/auth.service';
 import { AppInsightsService, SeverityLevel } from 'src/app/shared/app-insights/app-insights.service';
 
@@ -29,16 +29,25 @@ export class ResponseAuthInterceptor implements HttpInterceptor {
       return next.handle(req).pipe(
          catchError((error: HttpErrorResponse) => {
             if (error.status == 401) {
-               return this.intercept401(req, next);
+               return this.handleError401(req, next);
             }
             return throwError(error);
          })
       );
    }
 
+   private handleRequest(req: HttpRequest<any>, next: HttpHandler): Observable<any> {
+      req = req.clone({
+         setHeaders: {
+            Authorization: `Bearer ${this.auth.Token.AccessToken}`
+         }
+      });
+      return next.handle(req);
+   }
+
    private isRefreshingToken: boolean = false;
    private tokenSubject: BehaviorSubject<string> = new BehaviorSubject<string>(null);
-   private intercept401(req: HttpRequest<any>, next: HttpHandler): Observable<any> {
+   private handleError401(req: HttpRequest<any>, next: HttpHandler): Observable<any> {
       try {
 
          // IF HAS NO TOKEN, FORCE A LOCATION RELOAD AND THIS WILL SEND USER TO THE LOGIN PAGE
@@ -48,15 +57,36 @@ export class ResponseAuthInterceptor implements HttpInterceptor {
             return of(null);
          }
 
-         this.appInsights.trackTrace('Token Expired: Will Try to Refresh', SeverityLevel.Information, {
+         // IF IS ALREADY REFRESHING TOKEN, WAIT FOR IT THROUGH A BEHAVIOR SUBJECT
+         if (this.isRefreshingToken) {
+            this.appInsights.trackTrace('Token Expired: Will wait for the Refresh Instance', SeverityLevel.Information, {
+               refreshToken: this.auth.Token.RefreshToken,
+               requestedUrl: req.url,
+               locationUrl: location.href
+            });
+            return this.tokenSubject
+               .pipe(
+                  filter(token => token != null),
+                  take(1),
+                  switchMap(token => {
+                     return this.handleRequest(req, next);
+                  })
+               )
+         }
+
+         // ENTER IN REFRESHING STATE SO CONCURRENT INSTANCES WAIT FOR IT
+         this.isRefreshingToken = true
+         this.tokenSubject.next(null);
+         this.appInsights.trackTrace('Token Expired: Will refresh it', SeverityLevel.Information, {
             refreshToken: this.auth.Token.RefreshToken,
             requestedUrl: req.url,
             locationUrl: location.href
          });
+
          return this.auth.signRefresh()
             .pipe(
                catchError((refreshError) => {
-                  this.appInsights.trackTrace('Token Expired: Could not Refresh', SeverityLevel.Warning, {
+                  this.appInsights.trackTrace('Token Expired: Could not refresh', SeverityLevel.Warning, {
                      refreshToken: this.auth.Token.RefreshToken,
                      requestedUrl: req.url,
                      locationUrl: location.href,
@@ -67,12 +97,7 @@ export class ResponseAuthInterceptor implements HttpInterceptor {
                   return of(null);
                }),
                switchMap(() => {
-                  req = req.clone({
-                     setHeaders: {
-                        Authorization: `Bearer ${this.auth.Token.AccessToken}`
-                     }
-                  });
-                  return next.handle(req);
+                  return this.handleRequest(req, next);
                })
             );
 
