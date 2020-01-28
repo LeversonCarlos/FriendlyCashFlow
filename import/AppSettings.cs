d5ba1f7edf8c482978808e846688b0b96db07114
Leverson Carlos <lcjohnny@hotmail.com>
2020-01-28 19:16:06 -0300

    import program

---
+++ import/AppSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace Import
+{
+
+   public class AppSettings
+   {
+      public AppSettings_Api Api { get; set; }
+   }
+
+   public class AppSettings_Api
+   {
+      public string Url { get; set; }
+      public string Username { get; set; }
+      public string Password { get; set; }
+   }
+
+}
---
+++ import/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace Import
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+
+         var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+         var configuration = builder.Build();
+         var appSettingsSection = configuration.GetSection("AppSettings");
+         var appSettings = appSettingsSection.Get<AppSettings>();
+
+         Console.WriteLine("Hello World!");
+      }
+   }
+}
