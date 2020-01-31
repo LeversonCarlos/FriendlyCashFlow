568192ed76d32b5edb258443efd84b38fa5d5cdc
Leverson Carlos <lcjohnny@hotmail.com>
2020-01-31 17:51:18 -0300

    resume

---
+++ srcs/ClientApp/src/app/secure/common/resume/resume.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { DashboardService } from '../dashboard.service';
+import { AppInsightsService } from 'src/app/shared/app-insights/app-insights.service';
+import { enCategoryType } from '../../categories/categories.service';
+import { enAccountType } from '../../accounts/accounts.viewmodels';
+
+class ResumeVM {
+   Text: string
+   Type: enCategoryType
+   Icon: string
+   Value: number
+}
+
+@Component({
+   selector: 'fs-resume',
+   templateUrl: './resume.component.html',
+   styleUrls: ['./resume.component.scss']
+})
+export class ResumeComponent implements OnInit {
+
+   constructor(private dashboardService: DashboardService,
+      private appInsights: AppInsightsService) { }
+   public ResumeList: ResumeVM[]
+
+   public async ngOnInit() {
+      try {
+
+         let income = Object.assign(new ResumeVM, { Text: 'DASHBOARD_RESUME_INCOME_TEXT', Value: 0, Type: enCategoryType.Income, Icon: 'add_circle' });
+         let expense = Object.assign(new ResumeVM, { Text: 'DASHBOARD_RESUME_EXPENSE_TEXT', Value: 0, Type: enCategoryType.Expense, Icon: 'remove_circle' });
+         let balance = Object.assign(new ResumeVM, { Text: 'DASHBOARD_RESUME_BALANCE_TEXT', Value: 0, Type: enCategoryType.None, Icon: 'monetization_on' });
+
+         let accountBalances = await this.dashboardService.getBalances(true);
+         if (accountBalances) {
+            for (let index = 0; index < accountBalances.length; index++) {
+               const account = accountBalances[index];
+               const incomeValue = (account.PaidIncome + account.IncomeForecast);
+               const expenseValue = (account.PaidExpense + account.ExpenseForecast);
+               income.Value += incomeValue
+               expense.Value += expenseValue
+               balance.Value += incomeValue
+               balance.Value += expenseValue
+            }
+         }
+
+         this.ResumeList = [income, expense, balance]
+      }
+      catch (ex) { this.appInsights.trackException(ex) }
+   }
+
+}
