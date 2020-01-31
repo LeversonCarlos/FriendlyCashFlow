9d390edc00113a7f1b35c3ee9624a6aa6a8c91b8
Leverson Carlos <lcjohnny@hotmail.com>
2020-01-31 17:48:47 -0300

    resume

---
+++ srcs/ClientApp/src/app/secure/entries/resume/resume.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fs-resume',
+  templateUrl: './resume.component.html',
+  styleUrls: ['./resume.component.scss']
+})
+export class ResumeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
