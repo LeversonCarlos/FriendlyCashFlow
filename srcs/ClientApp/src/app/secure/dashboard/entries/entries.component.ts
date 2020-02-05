7ed3ce33bb7278b278f09d5c3bd72ec927852d09
Leverson Carlos <lcjohnny@hotmail.com>
2020-02-05 10:56:26 -0300

    Store and use cache for the dashboard's data (#44)

--- srcs/ClientApp/src/app/secure/dashboard/entries/entries.component.ts
+++ srcs/ClientApp/src/app/secure/dashboard/entries/entries.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DashboardService } from '../dashboard.service';
 import { Entry } from '../../entries/entries.viewmodels';
 import { AppInsightsService } from 'src/app/shared/app-insights/app-insights.service';
+import { BehaviorSubject } from 'rxjs';
 
 class EntriesVM {
    Text: string
@@ -16,29 +17,58 @@ class EntriesVM {
 })
 export class EntriesComponent implements OnInit {
 
-   constructor(private dashboardService: DashboardService,
-      private appInsights: AppInsightsService) { }
+   constructor(private dashboardService: DashboardService, private appInsights: AppInsightsService) { }
 
-   public EntryTypes: EntriesVM[]
+   private Key: string = 'DASHBOARD_ENTRIES_DATA'
+   public Data = new BehaviorSubject<EntriesVM[]>([])
 
    public async ngOnInit() {
+      this.cacheLoad()
+      const data = await this.getRefreshedData()
+      this.cacheSave(data)
+   }
+
+   private cacheLoad() {
       try {
-         this.EntryTypes = []
+         const jsonData = localStorage.getItem(this.Key)
+         if (jsonData && jsonData != '') {
+            const data: EntriesVM[] = JSON.parse(jsonData)
+            if (data) {
+               this.Data.next(data)
+            }
+         }
+      }
+      catch (ex) { this.appInsights.trackException(ex) }
+   }
+
+   private async getRefreshedData(): Promise<EntriesVM[]> {
+      try {
+         let entryTypes = []
          const entries = await this.dashboardService.getEntries();
          const today: any = (new Date()).toISOString()
 
          let overdueEntries = Object.assign(new EntriesVM, { Text: 'DASHBOARD_ENTRIES_OVERDUE_ENTRIES_TEXT', Overdue: true })
          overdueEntries.Entries = entries.filter(x => x.DueDate < today)
          if (overdueEntries.Entries && overdueEntries.Entries.length) {
-            this.EntryTypes.push(overdueEntries)
+            entryTypes.push(overdueEntries)
          }
 
          let nextEntries = Object.assign(new EntriesVM, { Text: 'DASHBOARD_ENTRIES_NEXT_ENTRIES_TEXT', Overdue: false })
          nextEntries.Entries = entries.filter(x => x.DueDate >= today)
          if (nextEntries.Entries && nextEntries.Entries.length) {
-            this.EntryTypes.push(nextEntries)
+            entryTypes.push(nextEntries)
          }
 
+         return entryTypes;
+      }
+      catch (ex) { this.appInsights.trackException(ex); return null; }
+   }
+
+   private async cacheSave(data: EntriesVM[]) {
+      try {
+         this.Data.next(data)
+         const jsonData = JSON.stringify(data);
+         localStorage.setItem(this.Key, jsonData)
       }
       catch (ex) { this.appInsights.trackException(ex) }
    }
--- srcs/ClientApp/src/app/secure/dashboard/resume/resume.component.ts
+++ srcs/ClientApp/src/app/secure/dashboard/resume/resume.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { DashboardService } from '../dashboard.service';
 import { AppInsightsService } from 'src/app/shared/app-insights/app-insights.service';
+import { BehaviorSubject } from 'rxjs';
+
+class DashboardResumeVM {
+   IncomeValue: number = 0;
+   ExpenseValue: number = 0;
+   BalanceValue: number = 0;
+}
 
 @Component({
    selector: 'fs-resume',
@@ -11,12 +18,31 @@ export class ResumeComponent implements OnInit {
 
    constructor(private dashboardService: DashboardService, private appInsights: AppInsightsService) { }
 
-   public IncomeValue: number = 0;
-   public ExpenseValue: number = 0;
-   public BalanceValue: number = 0;
+   private Key: string = 'DASHBOARD_RESUME_DATA'
+   public Data = new BehaviorSubject<DashboardResumeVM>(new DashboardResumeVM())
 
    public async ngOnInit() {
+      this.cacheLoad()
+      const data = await this.getRefreshedData()
+      this.cacheSave(data)
+   }
+
+   private cacheLoad() {
       try {
+         const jsonData = localStorage.getItem(this.Key)
+         if (jsonData && jsonData != '') {
+            const data: DashboardResumeVM = JSON.parse(jsonData)
+            if (data) {
+               this.Data.next(data)
+            }
+         }
+      }
+      catch (ex) { this.appInsights.trackException(ex) }
+   }
+
+   private async getRefreshedData(): Promise<DashboardResumeVM> {
+      try {
+         let data = new DashboardResumeVM()
 
          let accountBalances = await this.dashboardService.getBalances(true);
          if (accountBalances) {
@@ -24,13 +50,23 @@ export class ResumeComponent implements OnInit {
                const account = accountBalances[index];
                const incomeValue = (account.PaidIncome + account.IncomeForecast);
                const expenseValue = (account.PaidExpense + account.ExpenseForecast);
-               this.IncomeValue += incomeValue
-               this.ExpenseValue += expenseValue
-               this.BalanceValue += incomeValue
-               this.BalanceValue += expenseValue
+               data.IncomeValue += incomeValue
+               data.ExpenseValue += expenseValue
+               data.BalanceValue += incomeValue
+               data.BalanceValue += expenseValue
             }
          }
 
+         return data;
+      }
+      catch (ex) { this.appInsights.trackException(ex); return null; }
+   }
+
+   private async cacheSave(data: DashboardResumeVM) {
+      try {
+         this.Data.next(data)
+         const jsonData = JSON.stringify(data);
+         localStorage.setItem(this.Key, jsonData)
       }
       catch (ex) { this.appInsights.trackException(ex) }
    }
