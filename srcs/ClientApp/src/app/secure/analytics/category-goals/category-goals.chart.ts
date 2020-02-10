aa673b1c59cb0e1eb12b25379fb98849917d8663
Leverson Carlos <lcjohnny@hotmail.com>
2020-02-10 20:14:40 -0300

    chart renaming

--- srcs/ClientApp/src/app/secure/analytics/category-goals/category-goals.chart.ts
+++ srcs/ClientApp/src/app/secure/analytics/category-goals/category-goals.chart.ts
@@ -29,7 +29,7 @@ export class CategoryGoalsChart {
                drillUpText: '<<'
             }
          });
-         Highcharts.chart('container', options);
+         Highcharts.chart('categoryGoalsContainer', options);
       }
       catch (ex) { console.error(ex); }
    }
