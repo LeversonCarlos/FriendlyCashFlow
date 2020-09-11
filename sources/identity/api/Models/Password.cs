456c37fcb0073e757899d9dfdc6445b9ca0e107d
Leverson Carlos <lcjohnny@hotmail.com>
2020-09-11 08:37:25 -0300

    new password struct

---
+++ sources/identity/api/Models/Password.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace FriendlyCashFlow.Identity
+{
+   internal struct Password
+   {
+
+      public Password(string value)
+      {
+
+         if (string.IsNullOrEmpty(value) || value.Length < 5)
+            throw new ArgumentException("WARNING_IDENTITY_INVALID_PASSWORD_DATA");
+
+         _Value = value;
+         // var s = new SecureString();
+      }
+
+      readonly string _Value;
+
+      public static implicit operator string(Password value) =>
+         value._Value;
+
+      public static implicit operator Password(string value) =>
+         new Password(value);
+
+   }
+}
