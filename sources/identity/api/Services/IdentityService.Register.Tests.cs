9f70c2a7f07fcea4a53946d3f50c8c9688474125
Leverson Carlos <lcjohnny@hotmail.com>
2020-09-11 16:13:19 -0300

    tests for the register method

---
+++ sources/identity/api/Services/IdentityService.Register.Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FriendlyCashFlow.Identity.Tests
+{
+   public class IdentityServiceTests
+   {
+
+      [Fact]
+      public void Register_WithInvalidParameters_MustThrowException()
+      {
+         var exception = Assert.Throws<ArgumentException>(() => new User(userID, userName, password));
+
+         Assert.NotNull(exception);
+         Assert.Equal(exceptionText, exception.Message);
+      }
+
+   }
+}
---
+++ sources/identity/api/Services/IdentityService.Register.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FriendlyCashFlow.Identity
+{
+
+   partial class IdentityService
+   {
+   }
+
+   partial interface IIdentityService
+   {
+   }
+
+}
