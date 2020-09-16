b729b1a2bbd16a30d887a05bd6e452c7b4ee9973
Leverson Carlos <lcjohnny@hotmail.com>
2020-09-16 12:15:17 -0300

    compile tests with just the tests files

--- sources/identity/api/Services/IdentityService.Register.Tests.cs
+++ sources/identity/api/Services/IdentityService.Register.Tests.cs
@@ -10,7 +10,8 @@ namespace FriendlyCashFlow.Identity.Tests
       [Fact]
       public async void Register_WithInvalidParameters_MustThrowException()
       {
-         var provider = ProviderMocker.Create().WithIdentityService(new IdentityService()).Build().BuildServiceProvider();
+         var mongoClient = MongoMocker.Create().Build();
+         var provider = ProviderMocker.Create().WithIdentityService(new IdentityService(mongoClient)).Build().BuildServiceProvider();
          var service = (IIdentityService)provider.GetService<IIdentityService>();
 
          var expected = IdentityService.WARNING_IDENTITY_INVALID_REGISTER_PARAMETER;
