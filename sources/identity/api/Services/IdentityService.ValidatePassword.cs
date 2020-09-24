59a9e716e2f64645373bdb40b5b2ccab69fbaa0b
Leverson Carlos <lcjohnny@hotmail.com>
2020-09-24 18:06:32 -0300

    method to validate password strength

---
+++ sources/identity/api/Services/IdentityService.ValidatePassword.cs
@@ -0,0 +1,89 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FriendlyCashFlow.Identity
+{
+
+   partial class IdentityService
+   {
+
+      public async Task<ActionResult> ValidatePasswordAsync(string password)
+      {
+
+         var passwordChars = password
+            .ToCharArray()
+            .Select(item => new
+            {
+               Char = item,
+               IsUpper = Char.IsUpper(item),
+               IsLower = Char.IsLower(item),
+               IsNumber = Char.IsNumber(item),
+               IsSymbol = Char.IsSymbol(item)
+            })
+            .ToArray();
+
+         var passwordStrength = passwordChars
+            .Select(x => new
+            {
+               Upper = x.IsUpper ? 1 : 0,
+               Lower = x.IsLower ? 1 : 0,
+               Number = x.IsNumber ? 1 : 0,
+               Symbol = x.IsSymbol ? 1 : 0,
+               Size = 1
+            })
+            .GroupBy(x => new { })
+            .Select(x => new
+            {
+               Upper = x.Sum(s => s.Upper),
+               Lower = x.Sum(s => s.Lower),
+               Number = x.Sum(s => s.Number),
+               Symbol = x.Sum(s => s.Symbol),
+               Size = x.Sum(s => s.Size)
+            })
+            .FirstOrDefault();
+
+         var msgs = new List<string>();
+         var settings = _AppSettings.Value.Password;
+         if (passwordStrength.Upper < settings.MinimumUpperCases)
+            msgs.Add("USERS_PASSWORD_REQUIRE_UPPER_CASES_WARNING");
+         if (passwordStrength.Lower < settings.MinimumLowerCases)
+            msgs.Add("USERS_PASSWORD_REQUIRE_LOWER_CASES_WARNING");
+         if (passwordStrength.Number < settings.MinimumNumbers)
+            msgs.Add("USERS_PASSWORD_REQUIRE_NUMBERS_WARNING");
+         if (passwordStrength.Symbol < settings.MinimumSymbols)
+            msgs.Add("USERS_PASSWORD_REQUIRE_SYMBOLS_WARNING");
+         if (passwordStrength.Size < settings.MinimumSize)
+            msgs.Add("USERS_PASSWORD_MINIMUM_SIZE_WARNING");
+         if (msgs.Any())
+            return new BadRequestObjectResult(msgs.ToArray());
+
+         await Task.CompletedTask;
+         return new OkResult();
+      }
+
+   }
+
+   partial interface IIdentityService
+   {
+      Task<ActionResult> ValidatePasswordAsync(string password);
+   }
+
+   internal class ValidatePasswordSettings
+   {
+      public ValidatePasswordSettingsPassword Password { get; set; }
+   }
+
+   internal class ValidatePasswordSettingsPassword
+   {
+      // public string PasswordSalt { get; set; }
+      public int MinimumUpperCases { get; set; }
+      public int MinimumLowerCases { get; set; }
+      public int MinimumNumbers { get; set; }
+      public int MinimumSymbols { get; set; }
+      public int MinimumSize { get; set; }
+   }
+
+}
