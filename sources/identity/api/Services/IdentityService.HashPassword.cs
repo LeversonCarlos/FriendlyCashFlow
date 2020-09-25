d81dd2545563ef3c8eb70ed9827b20630de7832a
Leverson Carlos <lcjohnny@hotmail.com>
2020-09-25 14:30:36 -0300

    new method to hash password based on parameters

---
+++ sources/identity/api/Services/IdentityService.HashPassword.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace FriendlyCashFlow.Identity
+{
+
+   partial class IdentityService
+   {
+
+      string HashPassword(string password)
+      {
+
+         // INCOME BYTES
+         var saltStack = new Queue(Encoding.UTF8.GetBytes(_Settings.PasswordSalt));
+         var valueStack = new Queue(Encoding.UTF8.GetBytes(password));
+
+         // MERGE BYTES
+         byte[] mergedBytes = new byte[valueStack.Count + saltStack.Count];
+         int mergedIndex = 0; int mergedLength = mergedBytes.Length;
+         while (mergedIndex < mergedLength)
+         {
+            if (saltStack.Count > 0)
+            { mergedBytes[mergedIndex++] = (byte)saltStack.Dequeue(); }
+            if (valueStack.Count > 0)
+            { mergedBytes[mergedIndex++] = (byte)valueStack.Dequeue(); }
+         }
+
+         // HASH
+         byte[] computedHash;
+         using (var hasher = MD5.Create())
+         { computedHash = hasher.ComputeHash(mergedBytes); }
+
+         // RESULT
+         var result = Convert.ToBase64String(computedHash);
+         return result;
+
+      }
+
+   }
+
+}
