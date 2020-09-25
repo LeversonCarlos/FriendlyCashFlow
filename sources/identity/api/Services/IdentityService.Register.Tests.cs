244846c57cede7b5f108c38b74c0c7862fb0350a
Leverson Carlos <lcjohnny@hotmail.com>
2020-09-24 21:25:48 -0300

    test for the register method with invalid password

--- sources/identity/api/Services/IdentityService.Register.Tests.cs
+++ sources/identity/api/Services/IdentityService.Register.Tests.cs
@@ -21,6 +21,22 @@ namespace FriendlyCashFlow.Identity.Tests
       }
 
       [Fact]
+      public async void Register_WithInvalidPassword_MustReturnBadResult()
+      {
+         var mongoCollection = MongoCollectionMocker<IUser>.Create().Build();
+         var mongoDatabase = MongoDatabaseMocker.Create().WithCollection(mongoCollection).Build();
+         var identityService = new IdentityService(mongoDatabase, new ValidatePasswordSettings { MinimumSize = 10 });
+         var provider = ProviderMocker.Create().WithIdentityService(identityService).Build().BuildServiceProvider();
+         var registerParam = new RegisterVM { UserName = "userName", Password = "password" };
+
+         var result = await provider.GetService<IIdentityService>().RegisterAsync(registerParam);
+
+         Assert.NotNull(result);
+         Assert.IsType<Microsoft.AspNetCore.Mvc.BadRequestObjectResult>(result);
+         Assert.Equal(new string[] { "USERS_PASSWORD_MINIMUM_SIZE_WARNING" }, (result as Microsoft.AspNetCore.Mvc.BadRequestObjectResult).Value);
+      }
+
+      [Fact]
       public async void Register_WithValidParameters_MustReturnOkResult()
       {
          var mongoCollection = MongoCollectionMocker<IUser>.Create().Build();
