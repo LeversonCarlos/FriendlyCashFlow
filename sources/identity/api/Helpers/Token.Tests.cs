e1cd5a43853fad7d2f9b23bf6016f9a2a1e94f24
Leverson Carlos <lcjohnny@hotmail.com>
2020-10-07 22:07:13 -0300

    write some more tests for the get token descriptor function

--- sources/identity/api/Helpers/Token.Tests.cs
+++ sources/identity/api/Helpers/Token.Tests.cs
@@ -1,7 +1,10 @@
 using FriendlyCashFlow.Identity.Helpers;
 using Microsoft.IdentityModel.Tokens;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Security.Claims;
+using System.Security.Principal;
 using Xunit;
 
 namespace FriendlyCashFlow.Identity.Tests
@@ -63,6 +66,35 @@ namespace FriendlyCashFlow.Identity.Tests
 
          Assert.NotNull(value);
          Assert.Contains("The AccessExpirationInSeconds property on the Settings parameter is required for the GetTokenDescriptor function on the Token class", value.Message);
+      }
+
+      [Theory]
+      [InlineData("issuer", "audience", "userID", "userName", 1)]
+      [InlineData("my-issuer-name", "my-audience-name", "b36b857e-3748-4b2f-9f4d-6bacb8ffec51", "my-lengthy-username", 77)]
+      [InlineData("i.s.s.u.e.r", "a-u-d-i-e-n-c-e", "user-ID", "user-name", int.MaxValue)]
+      internal void GetTokenDescriptor_WithEspeciedParameters_MustResultExpected(string issuer, string audience, string userID, string userName, int accessExpirationInSeconds)
+      {
+         ClaimsIdentity identity = new ClaimsIdentity(
+            new GenericIdentity(userID, "Login"),
+            new List<Claim>{
+               new Claim(ClaimTypes.NameIdentifier, userName)
+            });
+         TokenSettings settings = new TokenSettings
+         {
+            SecuritySecret = "abc1234",
+            AccessExpirationInSeconds = accessExpirationInSeconds,
+            Audience = audience,
+            Issuer = issuer
+         };
+
+         var value = Token.GetTokenDescriptor(identity, settings);
+
+         Assert.NotNull(value);
+         Assert.Equal(issuer, value.Issuer);
+         Assert.Equal(audience, value.Audience);
+         Assert.Equal(userID, value.Subject.Name);
+         Assert.Equal(userName, value.Subject.Claims.Where(c => c.Type == ClaimTypes.NameIdentifier).Select(c => c.Value).FirstOrDefault());
+         Assert.Equal(accessExpirationInSeconds, (int)value.Expires.Value.Subtract(value.NotBefore.Value).TotalSeconds);
       }
 
    }
