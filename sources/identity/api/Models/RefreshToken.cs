ac43e986331d9083255e4269c30e8b99dda18ed9
Leverson Carlos <lcjohnny@hotmail.com>
2020-10-09 18:12:34 -0300

    new entity for the token

---
+++ sources/identity/api/Models/RefreshToken.cs
@@ -0,0 +1,72 @@
+using System;
+
+namespace FriendlyCashFlow.Identity
+{
+
+   public interface IRefreshToken
+   {
+      string TokenID { get; }
+      string UserID { get; }
+      DateTime ExpirationDate { get; }
+   }
+
+   internal class RefreshToken : IRefreshToken
+   {
+
+      RefreshToken() { }
+      internal partial struct WARNINGS { }
+
+      public static RefreshToken Create(string userID, int expirationInSeconds)
+      {
+         return new RefreshToken
+         {
+            TokenID = System.Guid.NewGuid().ToString(),
+            UserID = userID,
+            ExpirationDate = DateTime.UtcNow.AddSeconds(expirationInSeconds)
+         };
+      }
+
+      string _TokenID;
+      [MongoDB.Bson.Serialization.Attributes.BsonId]
+      public string TokenID
+      {
+         get => _TokenID;
+         private set
+         {
+            if (string.IsNullOrEmpty(value) || value.Length != 36)
+               throw new ArgumentException(WARNINGS.INVALID_TOKENID_PARAMETER);
+            _TokenID = value;
+         }
+      }
+      partial struct WARNINGS { public const string INVALID_TOKENID_PARAMETER = "WARNING_IDENTITY_INVALID_REFRESHTOKEN_TOKENID_PARAMETER"; }
+
+
+      string _UserID;
+      public string UserID
+      {
+         get => _UserID;
+         private set
+         {
+            if (string.IsNullOrEmpty(value) || value.Length != 36)
+               throw new ArgumentException(WARNINGS.INVALID_USERID_PARAMETER);
+            _UserID = value;
+         }
+      }
+      partial struct WARNINGS { public const string INVALID_USERID_PARAMETER = "WARNING_IDENTITY_INVALID_REFRESHTOKEN_USERID_PARAMETER"; }
+
+      DateTime _ExpirationDate;
+      public DateTime ExpirationDate
+      {
+         get => _ExpirationDate;
+         private set
+         {
+            if (value == null || value < DateTime.MinValue)
+               throw new ArgumentException(WARNINGS.INVALID_EXPIRATIONDATE_PARAMETER);
+            _ExpirationDate = value;
+         }
+      }
+      partial struct WARNINGS { public const string INVALID_EXPIRATIONDATE_PARAMETER = "WARNING_IDENTITY_INVALID_REFRESHTOKEN_EXPIRATIONDATE_PARAMETER"; }
+
+   }
+
+}
