a6cb2a7911b7e4332b0980cd467c02f4c35ee066
Leverson Carlos <lcjohnny@hotmail.com>
2020-11-03 20:53:56 -0300

    escrever teste para exceptoin causada por settings mal preenchida

--- sources/identity/api/Services/UserAuth/Service.Tests.cs
+++ sources/identity/api/Services/UserAuth/Service.Tests.cs
@@ -76,6 +76,28 @@ namespace Elesse.Identity.Tests
          };
 
       [Fact]
+      public async void UserAuth_WithInvalidSettingsThatThrowsException_MustReturnBadResult()
+      {
+         var userRepository = UserRepositoryMocker
+            .Create()
+            .WithGetUserByUserName(new User("userName@xpto.com", "X03MO1qnZdYdgyfeuILPmQ=="))
+            .Build();
+         var identitySettings = new IdentitySettings
+         {
+            PasswordRules = new PasswordRuleSettings { MinimumSize = 5 },
+            Token = new TokenSettings { }
+         };
+         var identityService = new IdentityService(identitySettings, userRepository, null);
+
+         var param = new UserAuthVM { UserName = "userName@xpto.com", Password = "password" };
+         var result = await identityService.UserAuthAsync(param);
+
+         Assert.NotNull(result);
+         Assert.IsType<BadRequestObjectResult>(result.Result);
+         Assert.Equal(new string[] { "The AccessExpirationInSeconds property on the Settings parameter is required for the GetTokenDescriptor function on the Token class (Parameter 'settings')" }, (result.Result as BadRequestObjectResult).Value);
+      }
+
+      [Fact]
       public async void UserAuth_WithValidParameters_MustReturnOkResult()
       {
          var userRepository = UserRepositoryMocker
