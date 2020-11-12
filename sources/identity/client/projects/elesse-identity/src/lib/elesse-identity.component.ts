619df99359b24ac3219f6de32e21505fb5a6b2e9
Leverson Carlos <lcjohnny@hotmail.com>
2020-11-11 21:29:55 -0300

    add new library ro the project

---
+++ sources/identity/client/projects/elesse-identity/src/lib/elesse-identity.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+   selector: 'identity-ElesseIdentity',
+   template: `
+    <p>
+      elesse-identity works!
+    </p>
+  `,
+   styles: [
+   ]
+})
+export class ElesseIdentityComponent implements OnInit {
+
+   constructor() { }
+
+   ngOnInit(): void {
+   }
+
+}
---
+++ sources/identity/client/projects/elesse-identity/src/lib/elesse-identity.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { ElesseIdentityComponent } from './elesse-identity.component';
+
+
+
+@NgModule({
+   declarations: [ElesseIdentityComponent],
+   imports: [
+   ],
+   exports: [ElesseIdentityComponent]
+})
+export class ElesseIdentityModule { }
