53bb10f80837aba9d81a80c96d94f734cad0df30
Leverson Carlos <lcjohnny@hotmail.com>
2020-11-12 21:35:30 -0300

    new library on the shared project

---
+++ sources/shared/client/projects/elesse-shared/src/lib/elesse-shared.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-ElesseShared',
+  template: `
+    <p>
+      elesse-shared works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ElesseSharedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
