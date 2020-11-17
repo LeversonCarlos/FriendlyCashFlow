dad62a6e3e34f56b32688af2d3c55040ef739afc
Leverson Carlos <lcjohnny@hotmail.com>
2020-11-17 20:47:57 -0300

    move common modules into shared proect

--- sources/identity/client/projects/elesse-identity/src/lib/elesse-identity.module.ts
+++ sources/identity/client/projects/elesse-identity/src/lib/elesse-identity.module.ts
@@ -1,7 +1,5 @@
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ElesseSharedModule } from 'elesse-shared';
 import { ElesseIdentityComponent } from './elesse-identity.component';
 import { ElesseIdentityRouting } from './elesse-identity.routing';
 import { RegisterComponent } from './register/register.component';
@@ -9,7 +7,7 @@ import { RegisterComponent } from './register/register.component';
 @NgModule({
    declarations: [ElesseIdentityComponent, RegisterComponent],
    imports: [
-      CommonModule, FormsModule, ReactiveFormsModule, HttpClientModule,
+      ElesseSharedModule,
       ElesseIdentityRouting
    ],
    exports: [ElesseIdentityComponent]
--- sources/shared/client/projects/elesse-shared/src/lib/elesse-shared.module.ts
+++ sources/shared/client/projects/elesse-shared/src/lib/elesse-shared.module.ts
@@ -1,10 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { BusyComponent } from './busy/busy.component';
 
 @NgModule({
    declarations: [BusyComponent],
    imports: [
+      CommonModule, FormsModule, ReactiveFormsModule, HttpClientModule,
    ],
-   exports: [BusyComponent]
+   exports: [
+      CommonModule, FormsModule, ReactiveFormsModule, HttpClientModule,
+      BusyComponent
+   ]
 })
 export class ElesseSharedModule { }
