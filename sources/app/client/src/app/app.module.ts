a3fcf39ba697af505acd9ba7e46f8b4c4633467a
Leverson Carlos <lcjohnny@hotmail.com>
2020-11-17 20:51:10 -0300

    dont need to import this module here

--- sources/app/client/src/app/app.module.ts
+++ sources/app/client/src/app/app.module.ts
@@ -4,15 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { ElesseIdentityModule } from 'elesse-identity';
-
 @NgModule({
    declarations: [
       AppComponent
    ],
    imports: [
       BrowserModule,
-      AppRoutingModule, ElesseIdentityModule
+      AppRoutingModule
    ],
    providers: [],
    bootstrap: [AppComponent]
