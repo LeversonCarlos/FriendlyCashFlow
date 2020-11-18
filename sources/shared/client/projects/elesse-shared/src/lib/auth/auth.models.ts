aa7124c411ec7f73bd7b7da2f0a106f4a92f508f
Leverson Carlos <lcjohnny@hotmail.com>
2020-11-17 21:05:37 -0300

    create a token vm on shared project

---
+++ sources/shared/client/projects/elesse-shared/src/lib/auth/auth.models.ts
@@ -0,0 +1,12 @@
+/*
+export class User {
+   UserID: string;
+   UserName: string;
+}
+*/
+
+export class TokenVM {
+   UserID: string;
+   AccessToken: string;
+   RefreshToken: string;
+}
--- sources/shared/client/projects/elesse-shared/src/public-api.ts
+++ sources/shared/client/projects/elesse-shared/src/public-api.ts
@@ -5,3 +5,4 @@
 export * from './lib/busy/busy.service';
 export * from './lib/busy/busy.component';
 export * from './lib/elesse-shared.module';
+export { TokenVM } from './lib/auth/auth.models';
