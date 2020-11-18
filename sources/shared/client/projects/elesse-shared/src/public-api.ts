3c771bf5702c632e0b9f68a67782155d450ae8c4
Leverson Carlos <lcjohnny@hotmail.com>
2020-11-17 21:21:14 -0300

    make the message service availabled throgh shared project

--- sources/shared/client/projects/elesse-shared/src/public-api.ts
+++ sources/shared/client/projects/elesse-shared/src/public-api.ts
@@ -2,7 +2,8 @@
  * Public API Surface of elesse-shared
  */
 
-export * from './lib/busy/busy.service';
-export * from './lib/busy/busy.component';
-export * from './lib/elesse-shared.module';
+export { ElesseSharedModule } from './lib/elesse-shared.module';
+export { BusyService } from './lib/busy/busy.service';
+export { BusyComponent } from './lib/busy/busy.component';
 export { TokenVM } from './lib/auth/auth.models';
+export { MessageService } from './lib/message/message.service';
