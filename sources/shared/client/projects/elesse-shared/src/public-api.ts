74df08f6d37620018b403d541d3f6dd942923711
Leverson Carlos <lcjohnny@hotmail.com>
2020-11-17 21:36:38 -0300

    expose a token service to be injected

--- sources/shared/client/projects/elesse-shared/src/public-api.ts
+++ sources/shared/client/projects/elesse-shared/src/public-api.ts
@@ -3,7 +3,11 @@
  */
 
 export { ElesseSharedModule } from './lib/elesse-shared.module';
+
 export { BusyService } from './lib/busy/busy.service';
 export { BusyComponent } from './lib/busy/busy.component';
-export { TokenVM } from './lib/auth/auth.models';
+
 export { MessageService } from './lib/message/message.service';
+
+export { TokenService } from './lib/auth/token.service';
+export { TokenVM } from './lib/auth/token.models';
