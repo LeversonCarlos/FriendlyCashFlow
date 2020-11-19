3d716e9de121ccc7c416b1970f83b292c98d4c53
Leverson Carlos <lcjohnny@hotmail.com>
2020-11-19 17:23:34 -0300

    provider for the error interceptor

--- sources/shared/client/projects/elesse-shared/src/lib/interceptors/error.interceptor.ts
+++ sources/shared/client/projects/elesse-shared/src/lib/interceptors/error.interceptor.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { MessageService } from '../message/message.service';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-   constructor() { }
+   constructor(private msg: MessageService) { }
 
    intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
       return next.handle(request);
    }
+
 }
+
+export const ErrorInterceptorProvider = {
+   provide: HTTP_INTERCEPTORS,
+   useClass: ErrorInterceptor,
+   multi: true
+};
