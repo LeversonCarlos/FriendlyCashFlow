8af6c01a4449e6a314f093888a893c2d7df4f7e1
Leverson Carlos <lcjohnny@hotmail.com>
2020-11-30 21:49:41 -0300

    create the login component

--- sources/identity/client/projects/elesse-identity/src/lib/login/login.component.ts
+++ sources/identity/client/projects/elesse-identity/src/lib/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { BusyService } from 'elesse-shared';
+import { IdentityService } from '../identity.service';
 
 @Component({
    selector: 'identity-login',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-   constructor() { }
+   constructor(private service: IdentityService, private busy: BusyService,
+      private fb: FormBuilder, private activatedRoute: ActivatedRoute) { }
 
-   ngOnInit(): void {
+   public inputForm: FormGroup;
+   public get IsBusy(): boolean { return this.busy.IsBusy; }
+   private returnUrl: string;
+
+   public ngOnInit(): void {
+      this.returnUrl = this.activatedRoute.snapshot.queryParams['returnUrl'] || '/';
+      this.OnFormCreate();
+   }
+
+   private OnFormCreate() {
+      this.inputForm = this.fb.group({
+         UserName: ['', [Validators.required, Validators.email]],
+         Password: ['', Validators.required]
+      });
+   }
+
+   public OnClick() {
+      if (!this.inputForm.valid)
+         return;
+      this.service.Login(this.inputForm.value.UserName, this.inputForm.value.Password, this.returnUrl);
    }
 
 }
