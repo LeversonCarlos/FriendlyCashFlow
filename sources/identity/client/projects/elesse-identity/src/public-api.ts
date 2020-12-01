e3f58842fc97d532dfa7a101d8c3ff51f22f27c9
Leverson Carlos <lcjohnny@hotmail.com>
2020-12-01 19:19:48 -0300

    review the exported arquives

--- sources/identity/client/projects/elesse-identity/src/public-api.ts
+++ sources/identity/client/projects/elesse-identity/src/public-api.ts
@@ -2,7 +2,6 @@
  * Public API Surface of elesse-identity
  */
 
-export * from './lib/identity.service';
-export * from './lib/elesse-identity.component';
+export { RefreshService } from './lib/refresh/refresh.service'
 export * from './lib/elesse-identity.module';
 export { ElesseIdentityRoutes } from './lib/elesse-identity.routing';
