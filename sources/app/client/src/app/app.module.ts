9b953712e723df727b5520080d4885bbf6e3c723
Leverson Carlos <lcjohnny@hotmail.com>
2020-12-02 08:01:36 -0300

    add material to the main project

--- sources/app/client/src/app/app.module.ts
+++ sources/app/client/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -15,7 +16,7 @@ import { RequestAuthInterceptorProvider, ResponseAuthInterceptorProvider } from 
       AppComponent
    ],
    imports: [
-      BrowserModule, HttpClientModule, ElesseSharedModule,
+      BrowserModule, BrowserAnimationsModule, HttpClientModule, ElesseSharedModule,
       AppRouting
    ],
    providers: [SettingsService, InsightsService, MessageService,
