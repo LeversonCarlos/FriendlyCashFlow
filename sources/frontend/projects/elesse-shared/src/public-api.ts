5113913a78f7d602a693a4577889c6fd0ed7e33f
Leverson Carlos <lcjohnny@hotmail.com>
2020-12-21 11:00:17 -0300

    review entry points

--- sources/frontend/projects/elesse-shared/src/public-api.ts
+++ sources/frontend/projects/elesse-shared/src/public-api.ts
@@ -1,7 +1,16 @@
-/*
- * Public API Surface of elesse-shared
- */
+export { ElesseSharedModule } from './lib/elesse-shared.module';
 
-export * from './lib/elesse-shared.service';
-export * from './lib/elesse-shared.component';
-export * from './lib/elesse-shared.module';
+export { BusyService } from './lib/busy/busy.service';
+export { BusyComponent } from './lib/busy/busy.component';
+
+export { MessageData, MessageDataType } from './lib/message/message.models';
+export { MessageService } from './lib/message/message.service';
+
+export { SettingsService } from './lib/settings/settings.service';
+export { InsightsService } from './lib/insights/insights.service';
+
+export { AuthGuard } from './lib/auth/auth.guard';
+export { UnauthGuard } from './lib/auth/unauth.guard';
+
+export { TokenService } from './lib/auth/token.service';
+export { TokenVM } from './lib/auth/token.models';
