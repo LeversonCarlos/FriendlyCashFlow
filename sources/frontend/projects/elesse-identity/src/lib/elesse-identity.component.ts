d8c20ef1f1154677be473008c7a783ef495d34a6
Leverson Carlos <lcjohnny@hotmail.com>
2020-12-21 12:24:39 -0300

    create new identity project

---
+++ sources/frontend/projects/elesse-identity/src/lib/elesse-identity.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'elesse-identity-ElesseIdentity',
+  template: `
+    <p>
+      elesse-identity works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ElesseIdentityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ sources/frontend/projects/elesse-identity/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of elesse-identity
+ */
+
+export * from './lib/elesse-identity.service';
+export * from './lib/elesse-identity.component';
+export * from './lib/elesse-identity.module';
