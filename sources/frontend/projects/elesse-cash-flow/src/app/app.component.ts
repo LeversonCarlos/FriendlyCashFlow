3260900a3485e924db5bfdf6db2a82f1276bdd8f
Leverson Carlos <lcjohnny@hotmail.com>
2020-12-21 18:32:12 -0300

    adjust components

--- sources/frontend/projects/elesse-cash-flow/src/app/app.component.ts
+++ sources/frontend/projects/elesse-cash-flow/src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { InsightsService } from '@elesse/shared';
 
 @Component({
-  selector: 'elesse-cash-flow-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+   selector: 'elesse-cash-flow-root',
+   template: `
+      <div class="content">
+         <h1>Welcome to {{title}}!</h1>
+         <span style="display: block">{{ title }} app is running!</span>
+         <a routerLink="/identity/register">Register</a>
+         <a routerLink="/identity/login">Login</a>
+         <a routerLink="/identity/logout">Logout</a>
+         <a mat-raised-button routerLink="/identity/change-password">Change Password</a>
+      </div>
+      <router-outlet></router-outlet>
+      <shared-busy></shared-busy>
+   `,
+   styles: [`
+      .content {
+         height:80vh;
+         display:flex;
+         flex-direction:column;
+         justify-content:center;
+         align-items:center;
+      }
+      h1 {
+         text-transform:uppercase;
+      }
+   `]
 })
 export class AppComponent {
-  title = 'ElesseCashFlow';
+   title = 'Cash Flow';
+
+   constructor(private insights: InsightsService) {
+      this.insights.TrackEvent('Application Opened');
+   }
+
 }
