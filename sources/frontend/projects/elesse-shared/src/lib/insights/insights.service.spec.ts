c367c6b8ede1fb05e310c8a31797b41007c2b0ac
Leverson Carlos <lcjohnny@hotmail.com>
2020-12-21 12:18:22 -0300

    review insights srevice tests

--- sources/frontend/projects/elesse-shared/src/lib/insights/insights.service.spec.ts
+++ sources/frontend/projects/elesse-shared/src/lib/insights/insights.service.spec.ts
@@ -1,16 +1,28 @@
 import { TestBed } from '@angular/core/testing';
+import { SettingsService } from '../settings/settings.service';
 
 import { InsightsService } from './insights.service';
 
 describe('InsightsService', () => {
+
+   let settingsService: SettingsService;
    let service: InsightsService;
 
    beforeEach(() => {
-      TestBed.configureTestingModule({});
+      TestBed.configureTestingModule({
+         providers: [
+            {
+               provide: SettingsService,
+               useValue: jasmine.createSpyObj('SettingsService', ['getSettings'])
+            }
+         ]
+      });
+      settingsService = TestBed.inject(SettingsService);
       service = TestBed.inject(InsightsService);
    });
 
    it('should be created', () => {
       expect(service).toBeTruthy();
    });
+
 });
