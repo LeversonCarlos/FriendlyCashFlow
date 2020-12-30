448fba81dda0d87ca8a13fad07efc3b25963b4c5
Leverson Carlos <lcjohnny@hotmail.com>
2020-12-30 12:54:12 -0300

    implement container

--- sources/frontend/projects/elesse-identity/src/lib/container/container.component.ts
+++ sources/frontend/projects/elesse-identity/src/lib/container/container.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'elesse-identity-container',
-  templateUrl: './container.component.html',
-  styleUrls: ['./container.component.scss']
+   selector: 'elesse-identity-container',
+   templateUrl: './container.component.html',
+   styleUrls: ['./container.component.scss']
 })
 export class ContainerComponent implements OnInit {
 
-  constructor() { }
+   constructor() { }
 
-  ngOnInit(): void {
-  }
+   public get AppTitle(): string { return "Friendly Cash Flow"; }
+
+   ngOnInit(): void {
+   }
 
 }
