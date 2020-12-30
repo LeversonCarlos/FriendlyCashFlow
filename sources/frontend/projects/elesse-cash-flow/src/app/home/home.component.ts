2217ab06238eeff1d3f5994d329de16d895199d8
Leverson Carlos <lcjohnny@hotmail.com>
2020-12-30 20:05:17 -0300

    is auth property

--- sources/frontend/projects/elesse-cash-flow/src/app/home/home.component.ts
+++ sources/frontend/projects/elesse-cash-flow/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenService } from '@elesse/shared';
 
 @Component({
    selector: 'elesse-cash-flow-home',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-   constructor() { }
+   constructor(private tokenService: TokenService) { }
+
+   public get IsAuthenticated(): boolean { return this.tokenService && this.tokenService.HasToken; }
 
    ngOnInit(): void {
    }
