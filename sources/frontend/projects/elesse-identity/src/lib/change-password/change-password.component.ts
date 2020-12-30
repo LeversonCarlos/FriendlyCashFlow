fb75a9063e82fa6010056829e794cd3e3ed25ae3
Leverson Carlos <lcjohnny@hotmail.com>
2020-12-30 13:00:21 -0300

    rename scss

--- sources/frontend/projects/elesse-identity/src/lib/change-password/change-password.component.ts
+++ sources/frontend/projects/elesse-identity/src/lib/change-password/change-password.component.ts
@@ -7,7 +7,7 @@ import { BusyService, MessageService } from '@elesse/shared';
 @Component({
    selector: 'identity-change-password',
    templateUrl: './change-password.component.html',
-   styleUrls: ['./change-password.component.css']
+   styleUrls: ['./change-password.component.scss']
 })
 export class ChangePasswordComponent implements OnInit {
 
