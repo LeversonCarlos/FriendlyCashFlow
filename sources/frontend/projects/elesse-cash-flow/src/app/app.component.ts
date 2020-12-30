4b835ecb06a1100578cdecbce3528f6313c5e0bb
Leverson Carlos <lcjohnny@hotmail.com>
2020-12-30 19:41:57 -0300

    move initial screen into home component

--- sources/frontend/projects/elesse-cash-flow/src/app/app.component.ts
+++ sources/frontend/projects/elesse-cash-flow/src/app/app.component.ts
@@ -3,40 +3,10 @@ import { InsightsService } from '@elesse/shared';
 
 @Component({
    selector: 'elesse-cash-flow-root',
-   template: `
-      <div class="content">
-         <h1>Welcome to {{title}}!</h1>
-         <h3 style="display: block">{{ title }} app is running!</h3>
-         <div class="menu">
-            <a routerLink="/identity/register">Register</a>
-            <a routerLink="/identity/login">Login</a>
-            <a routerLink="/identity/logout">Logout</a>
-            <a routerLink="/identity/change-password">Change Password</a>
-         </div>
-         <router-outlet></router-outlet>
-      </div>
-      <shared-busy></shared-busy>
-   `,
-   styles: [`
-      .content {
-         height:100%;
-         display:flex;
-         flex-direction:column;
-         justify-content:center;
-         align-items:center;
-      }
-      .menu {
-         display:flex;
-         width:50%;
-         justify-content:space-evenly;
-      }
-      h1 {
-         text-transform:uppercase;
-      }
-   `]
+   template: `<elesse-cash-flow-home></elesse-cash-flow-home>`,
+   styles: []
 })
 export class AppComponent {
-   title = 'Cash Flow';
 
    constructor(private insights: InsightsService) {
       this.insights.TrackEvent('Application Opened');
