bfff31e16fcf09d97f1c580938d8b5963f88024d
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-01 15:28:05 -0300

    initial front end commit

---
+++ sources/frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elesse-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'CashFlow';
+}
