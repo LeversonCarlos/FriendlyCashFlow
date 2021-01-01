2020d3c5c8cb7a47144f1b2f6ff8c69c180eee93
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-01 15:42:26 -0300

    lint

--- sources/frontend/src/app/app.component.ts
+++ sources/frontend/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'elesse-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+   selector: 'elesse-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'CashFlow';
+   title = 'CashFlow';
 }
