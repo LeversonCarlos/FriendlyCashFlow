92aaf007d619e4f22997fc347e3444960fe9d492
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-02 11:40:33 -0300

    new identity module

---
+++ sources/frontend/src/app/identity/identity.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IdentityRouting } from './identity.routing';
+
+@NgModule({
+   declarations: [],
+   imports: [
+      CommonModule,
+      IdentityRouting
+   ]
+})
+export class IdentityModule { }
