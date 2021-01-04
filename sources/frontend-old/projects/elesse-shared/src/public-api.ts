1e70c010de78295f87393a20e02c82da0b345540
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-04 20:02:17 -0300

    remove old shard prokject

--- sources/frontend-old/projects/elesse-shared/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-export { ElesseSharedModule } from './lib/elesse-shared.module';
-
-export { AuthGuard } from './lib/auth/auth.guard';
-export { UnauthGuard } from './lib/auth/unauth.guard';
-
-export { TokenService } from './lib/auth/token.service';
-export { TokenVM } from './lib/auth/token.models';
