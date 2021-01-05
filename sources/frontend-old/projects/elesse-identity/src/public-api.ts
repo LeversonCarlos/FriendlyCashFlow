c295cdffea8e25d6664c050f93607be4b4524b11
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-04 22:08:27 -0300

    remove identity project

--- sources/frontend-old/projects/elesse-identity/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of elesse-identity
- */
-
-export { RefreshService } from './lib/refresh/refresh.service'
-export { ElesseIdentityModule } from './lib/elesse-identity.module';
-// export { ElesseIdentityRoutes } from './lib/elesse-identity.routing';
