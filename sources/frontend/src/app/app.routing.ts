a70be07eb18c574166ad684719285d82d08e24fd
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-04 21:14:05 -0300

    fix routing and content

--- sources/frontend/src/app/app.routing.ts
+++ sources/frontend/src/app/app.routing.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   { path: '', redirectTo: 'home', pathMatch: 'full' },
+   { path: 'home', component: HomeComponent },
+   { path: 'identity', loadChildren: () => import('./identity/identity.module').then(m => m.IdentityModule) },
+   { path: '**', component: HomeComponent }
+];
 
 @NgModule({
    imports: [RouterModule.forRoot(routes)],
