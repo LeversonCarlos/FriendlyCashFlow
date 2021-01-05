08dd42dd8825d96e2d3362fca240ad2953abd913
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-04 21:56:10 -0300

    move change password compoent into new project

--- sources/frontend/src/app/identity/change-password/change-password.component.ts
+++ sources/frontend/src/app/identity/change-password/change-password.component.ts
@@ -7,7 +7,7 @@ import { BusyService, MessageService } from '@elesse/shared';
 @Component({
    selector: 'identity-change-password',
    templateUrl: './change-password.component.html',
-   styleUrls: ['./change-password.component.scss', '../elesse-identity.common.scss']
+   styleUrls: ['./change-password.component.scss']
 })
 export class ChangePasswordComponent implements OnInit {
 
