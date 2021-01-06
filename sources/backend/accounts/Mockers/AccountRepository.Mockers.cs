5fbb14b29c3e0d5c4c82d2be91cd71f0daf79235
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-06 20:18:16 -0300

    add mocker for accounts repository

---
+++ sources/backend/accounts/Mockers/AccountRepository.Mockers.cs
@@ -0,0 +1,26 @@
+using Moq;
+
+namespace Elesse.Accounts.Tests
+{
+   internal class AccountRepositoryMocker
+   {
+
+      readonly Mock<IAccountRepository> _Mock;
+      public AccountRepositoryMocker() => _Mock = new Mock<IAccountRepository>();
+      public static AccountRepositoryMocker Create() => new AccountRepositoryMocker();
+
+      public AccountRepositoryMocker WithSearchAccounts() =>
+         WithSearchAccounts(new IAccountEntity[][] { });
+      public AccountRepositoryMocker WithSearchAccounts(params IAccountEntity[][] results)
+      {
+         var seq = new MockSequence();
+         foreach (var result in results)
+            _Mock.InSequence(seq)
+               .Setup(m => m.SearchAccountsAsync(It.IsAny<string>()))
+               .ReturnsAsync(result);
+         return this;
+      }
+
+      public IAccountRepository Build() => _Mock.Object;
+   }
+}
