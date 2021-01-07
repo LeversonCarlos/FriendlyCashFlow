c3d236142cd5eaf9f3d18c72e9d843a346a6f70b
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-07 20:42:32 -0300

    test sucess on the ChangeState method

--- sources/backend/accounts/Services/ChangeState/Service.Tests.cs
+++ sources/backend/accounts/Services/ChangeState/Service.Tests.cs
@@ -33,25 +33,22 @@ namespace Elesse.Accounts.Tests
          Assert.Equal(new string[] { WARNINGS.ACCOUNT_NOT_FOUND }, (result as Microsoft.AspNetCore.Mvc.BadRequestObjectResult).Value);
       }
 
-      /*
       [Fact]
-      public async void Update_WithValidParameters_MustReturnOkResult()
+      public async void ChangeState_WithValidParameters_MustReturnOkResult()
       {
          var accountID = new Shared.EntityID();
          var repository = AccountRepositoryMocker
             .Create()
-            .WithSearchAccounts()
             .WithGetAccountByID(new AccountEntity(accountID, "Account Text", enAccountType.General, null, null, true))
             .Build();
          var service = new AccountService(repository);
 
-         var param = new UpdateVM { AccountID = accountID, Text = "Changed Account Text", Type = enAccountType.General };
-         var result = await service.UpdateAsync(param);
+         var param = new ChangeStateVM { AccountID = accountID, State = false };
+         var result = await service.ChangeStateAsync(param);
 
          Assert.NotNull(result);
          Assert.IsType<Microsoft.AspNetCore.Mvc.OkResult>(result);
       }
-      */
 
    }
 }
