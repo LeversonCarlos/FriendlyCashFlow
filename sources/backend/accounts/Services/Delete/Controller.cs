9d5bb869988362f175df43b4fc63cb78913c02bf
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-07 20:04:50 -0300

    add entry point for the delete method and test it

---
+++ sources/backend/accounts/Services/Delete/Controller.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Elesse.Accounts
+{
+
+   partial class AccountController
+   {
+
+      [HttpDelete("delete/{id}")]
+      [AllowAnonymous]
+      public Task<IActionResult> DeleteAsync(Shared.EntityID id) =>
+         _AccountService.DeleteAsync(id);
+
+   }
+
+}
