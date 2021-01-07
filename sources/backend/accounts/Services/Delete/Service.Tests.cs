d3c18387b0f9057d563842e4578f418df7d68c09
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-07 20:01:31 -0300

    test success on delete method

--- sources/backend/accounts/Services/Delete/Service.Tests.cs
+++ sources/backend/accounts/Services/Delete/Service.Tests.cs
@@ -33,25 +33,21 @@ namespace Elesse.Accounts.Tests
          Assert.Equal(new string[] { WARNINGS.ACCOUNT_NOT_FOUND }, (result as Microsoft.AspNetCore.Mvc.BadRequestObjectResult).Value);
       }
 
-      /*
       [Fact]
-      public async void Update_WithValidParameters_MustReturnOkResult()
+      public async void Delete_WithValidParameters_MustReturnOkResult()
       {
          var accountID = new Shared.EntityID();
          var repository = AccountRepositoryMocker
             .Create()
-            .WithSearchAccounts()
             .WithGetAccountByID(new AccountEntity(accountID, "Account Text", enAccountType.General, null, null, true))
             .Build();
          var service = new AccountService(repository);
 
-         var param = new UpdateVM { AccountID = accountID, Text = "Changed Account Text", Type = enAccountType.General };
-         var result = await service.UpdateAsync(param);
+         var result = await service.DeleteAsync(accountID);
 
          Assert.NotNull(result);
          Assert.IsType<Microsoft.AspNetCore.Mvc.OkResult>(result);
       }
-      */
 
    }
 }
