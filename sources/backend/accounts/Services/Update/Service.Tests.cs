dad6d99cc538042f795939a6a54af7921aecace1
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-07 19:07:07 -0300

    test update for the same text

--- sources/backend/accounts/Services/Update/Service.Tests.cs
+++ sources/backend/accounts/Services/Update/Service.Tests.cs
@@ -30,27 +30,23 @@ namespace Elesse.Accounts.Tests
          Assert.Equal(new string[] { WARNINGS.DAYS_ONLY_VALID_FOR_CREDIT_CARD_TYPE }, (result as Microsoft.AspNetCore.Mvc.BadRequestObjectResult).Value);
       }
 
-      /*
       [Fact]
-      public async void Insert_WithExistingText_MustReturnBadRequest()
+      public async void Update_WithExistingText_MustReturnBadRequest()
       {
          var repository = AccountRepositoryMocker
             .Create()
-            .WithSearchAccounts(null, new AccountEntity[] { new AccountEntity("Account Text", enAccountType.General, null, null) })
+            .WithSearchAccounts(new AccountEntity[] { new AccountEntity(new Shared.EntityID(), "Account Text", enAccountType.General, null, null, true) })
             .Build();
          var service = new AccountService(repository);
-         var param = new InsertVM { Text = "Account Text", Type = enAccountType.General };
+         var param = new UpdateVM { AccountID = new Shared.EntityID(), Text = "Account Text", Type = enAccountType.General };
 
-         var result = await service.InsertAsync(param);
-         Assert.NotNull(result);
-         Assert.IsType<Microsoft.AspNetCore.Mvc.OkResult>(result);
-
-         result = await service.InsertAsync(param);
+         var result = await service.UpdateAsync(param);
          Assert.NotNull(result);
          Assert.IsType<Microsoft.AspNetCore.Mvc.BadRequestObjectResult>(result);
          Assert.Equal(new string[] { WARNINGS.ACCOUNT_TEXT_ALREADY_USED }, (result as Microsoft.AspNetCore.Mvc.BadRequestObjectResult).Value);
       }
 
+      /*
       [Fact]
       public async void Insert_WithValidParameters_MustReturnOkResult()
       {
