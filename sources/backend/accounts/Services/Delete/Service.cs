ad51aac7456a9c557cc39857c479542df429f72a
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-07 19:56:09 -0300

    add method on service to delete account

---
+++ sources/backend/accounts/Services/Delete/Service.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Elesse.Accounts
+{
+
+   partial class AccountService
+   {
+
+      public async Task<IActionResult> DeleteAsync(Shared.EntityID accountID)
+      {
+
+         // VALIDATE PARAMETERS
+         if (accountID == null)
+            return new BadRequestObjectResult(new string[] { WARNINGS.INVALID_DELETE_PARAMETER });
+
+         // LOCATE ACCOUNT
+         var account = (AccountEntity)(await _AccountRepository.GetAccountByIDAsync(accountID));
+         if (account == null)
+            return new BadRequestObjectResult(new string[] { WARNINGS.ACCOUNT_NOT_FOUND });
+
+         // SAVE CHANGES
+         await _AccountRepository.DeleteAccountAsync(accountID);
+
+         // RESULT
+         return new OkResult();
+      }
+
+   }
+
+   partial interface IAccountService
+   {
+      Task<IActionResult> DeleteAsync(Shared.EntityID accountID);
+   }
+
+   partial struct WARNINGS
+   {
+      internal const string INVALID_DELETE_PARAMETER = "WARNING_ACCOUNTS_INVALID_DELETE_PARAMETER";
+   }
+
+}
