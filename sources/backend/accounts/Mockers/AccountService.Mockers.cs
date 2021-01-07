5d8abb4f8f4b1b355e54a79bc3ceebc4323c7f41
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-07 19:39:28 -0300

    add entry point to insert method and test it

---
+++ sources/backend/accounts/Mockers/AccountService.Mockers.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace Elesse.Accounts.Tests
+{
+   internal class AccountServiceMocker
+   {
+
+      readonly Mock<IAccountService> _Mock;
+      public AccountServiceMocker() => _Mock = new Mock<IAccountService>();
+      public static AccountServiceMocker Create() => new AccountServiceMocker();
+
+      public AccountServiceMocker WithInsert(InsertVM param, IActionResult result)
+      {
+         _Mock.Setup(m => m.InsertAsync(param)).ReturnsAsync(result);
+         return this;
+      }
+
+      public IAccountService Build() => _Mock.Object;
+   }
+}
