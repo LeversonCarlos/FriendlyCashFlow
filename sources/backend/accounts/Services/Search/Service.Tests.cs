362a76d257b533c55a847f7c5f9e20aafa7f89d8
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-08 10:08:29 -0300

    Create a search function to the accounts api and write tests for it (#111)

---
+++ sources/backend/accounts/Services/Search/Service.Tests.cs
@@ -0,0 +1,32 @@
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Elesse.Accounts.Tests
+{
+   partial class AccountServiceTests
+   {
+
+      [Fact]
+      public async void Search_WithValidData_MustReturnOkResultWithData()
+      {
+         var account = new AccountEntity(new Shared.EntityID(), "Account Text", enAccountType.General, null, null, true);
+         var accountsList = new AccountEntity[] { account };
+         var repository = AccountRepositoryMocker
+            .Create()
+            .WithSearchAccounts(accountsList)
+            .Build();
+         var service = new AccountService(repository);
+
+         var result = await service.SearchAsync(account.Text);
+
+         Assert.NotNull(result);
+         Assert.IsType<OkObjectResult>(result.Result);
+         Assert.IsType<AccountEntity[]>((result.Result as OkObjectResult).Value);
+         var resultValue = (AccountEntity[])((result.Result as OkObjectResult).Value);
+         Assert.NotNull(resultValue);
+         Assert.Single(resultValue);
+         Assert.Equal(account.AccountID, resultValue[0].AccountID);
+      }
+
+   }
+}
