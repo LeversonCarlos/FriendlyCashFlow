96f65cd478bec4fa513e018b8d4b10f4869aab61
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-08 10:20:35 -0300

    move accounts interfaces inside the proper project

--- sources/backend/accounts/Repository/IAccountRepository.cs
+++ sources/backend/accounts/Repository/IAccountRepository.cs
@@ -2,7 +2,7 @@ using System.Threading.Tasks;
 
 namespace Elesse.Accounts
 {
-   public interface IAccountRepository
+   internal interface IAccountRepository
    {
 
       Task InsertAccountAsync(IAccountEntity value);
