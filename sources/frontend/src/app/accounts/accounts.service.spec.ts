178b8e80fcb2e34a96aa21c9503b0ccca1186235
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-09 19:54:40 -0300

    use observables

--- sources/frontend/src/app/accounts/accounts.service.spec.ts
+++ sources/frontend/src/app/accounts/accounts.service.spec.ts
@@ -16,18 +16,15 @@ describe('AccountsService', () => {
    });
 
    it('GetData WhenDataIsNull MustResult EmptyArray', () => {
-      expect(service.GetData()).toEqual([]);
-      expect(service.GetData(false)).toEqual([]);
-   });
-
-   it('GetData WhenDataIsNull MustResult EmptyArray', () => {
       const account = Object.assign(new AccountEntity, { Text: 'Account Text' });
 
-      service.SetData(false, [account])
-      const result = service.GetData(false);
+      service.GetData(false).subscribe(result => {
+         expect(result).toEqual([account]);
+         expect(result[0].Text).toEqual(account.Text);
+         // done();
+      });
 
-      expect(result).toEqual([account]);
-      expect(result[0].Text).toEqual(account.Text);
+      service.SetData(false, [account])
    });
 
 });
--- sources/frontend/src/app/accounts/accounts.service.ts
+++ sources/frontend/src/app/accounts/accounts.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AccountEntity } from './accounts.data';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
    providedIn: 'root'
@@ -8,15 +9,18 @@ export class AccountsService {
 
    constructor() { }
 
-   private _Data: { [id: number]: AccountEntity[]; } = {};
+   private _Data: { [id: number]: Subject<AccountEntity[]>; } = {
+      0: new Subject<AccountEntity[]>(),
+      1: new Subject<AccountEntity[]>()
+   };
    private _DataKey = (state: boolean): number =>
       state == true ? 1 : 0;
 
-   public GetData(state: boolean = true): AccountEntity[] {
-      return this._Data[this._DataKey(state)] ?? [];
+   public GetData(state: boolean = true): Observable<AccountEntity[]> {
+      return this._Data[this._DataKey(state)];
    }
    public SetData(state: boolean, value: AccountEntity[]): void {
-      this._Data[this._DataKey(state)] = value;
+      this._Data[this._DataKey(state)].next(value);
    }
 
 }
