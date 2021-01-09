75c5ed883f687f5a31e1b39706e59e517f0827d8
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-09 19:07:28 -0300

    add accounts to main menu

--- sources/frontend/src/app/accounts/list/list.component.spec.ts
+++ sources/frontend/src/app/accounts/list/list.component.spec.ts
@@ -3,23 +3,25 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ListComponent } from './list.component';
 
 describe('ListComponent', () => {
-  let component: ListComponent;
-  let fixture: ComponentFixture<ListComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ListComponent ]
-    })
-    .compileComponents();
-  });
+   let component: ListComponent;
+   let fixture: ComponentFixture<ListComponent>;
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+   beforeEach(async () => {
+      await TestBed.configureTestingModule({
+         declarations: [ListComponent]
+      })
+         .compileComponents();
+   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+   beforeEach(() => {
+      fixture = TestBed.createComponent(ListComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+   });
+
+   it('should create', () => {
+      expect(component).toBeTruthy();
+   });
+
 });
