a153e1bd9ff281d8e154e4dab25991979e3503dc
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-09 19:38:49 -0300

    set and get data properlly

--- sources/frontend/src/app/accounts/accounts.service.spec.ts
+++ sources/frontend/src/app/accounts/accounts.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { AccountEntity } from './accounts.data';
 import { AccountsService } from './accounts.service';
 
 describe('AccountsService', () => {
@@ -16,6 +17,17 @@ describe('AccountsService', () => {
 
    it('GetData WhenDataIsNull MustResult EmptyArray', () => {
       expect(service.GetData()).toEqual([]);
+      expect(service.GetData(false)).toEqual([]);
+   });
+
+   it('GetData WhenDataIsNull MustResult EmptyArray', () => {
+      const account = Object.assign(new AccountEntity, { Text: 'Account Text' });
+
+      service.SetData(false, [account])
+      const result = service.GetData(false);
+
+      expect(result).toEqual([account]);
+      expect(result[0].Text).toEqual(account.Text);
    });
 
 });
--- sources/frontend/src/app/accounts/accounts.service.ts
+++ sources/frontend/src/app/accounts/accounts.service.ts
@@ -11,7 +11,12 @@ export class AccountsService {
    private _Data: { [id: number]: AccountEntity[]; } = {};
    private _DataKey = (state: boolean): number =>
       state == true ? 1 : 0;
-   public GetData = (state: boolean = true): AccountEntity[] =>
-      this._Data[this._DataKey(state)] ?? [];
+
+   public GetData(state: boolean = true): AccountEntity[] {
+      return this._Data[this._DataKey(state)] ?? [];
+   }
+   public SetData(state: boolean, value: AccountEntity[]): void {
+      this._Data[this._DataKey(state)] = value;
+   }
 
 }
