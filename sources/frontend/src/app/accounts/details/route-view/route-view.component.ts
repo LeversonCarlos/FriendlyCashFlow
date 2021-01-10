f805c3f770fff243e0380ec1c10d38a662bc5f76
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-10 16:31:18 -0300

    account type change actions

--- sources/frontend/src/app/accounts/details/route-view/route-view.component.ts
+++ sources/frontend/src/app/accounts/details/route-view/route-view.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { BusyService, MessageService } from '@elesse/shared';
+import { AccountEntity, enAccountType } from '../../accounts.data';
 import { AccountsService } from '../../accounts.service';
 
 @Component({
@@ -18,16 +19,44 @@ export class DetailsRouteViewComponent implements OnInit {
    public inputForm: FormGroup;
    public get IsBusy(): boolean { return this.busy.IsBusy; }
 
-   async ngOnInit(): Promise<void> {
+   public async ngOnInit(): Promise<void> {
       const paramID = this.route.snapshot.params.id;
       const data = await this.service.LoadAccount(paramID);
+      this.OnFormCreate(data);
+   }
+
+   private OnFormCreate(data: AccountEntity) {
       this.inputForm = this.fb.group({
          AccountID: [data.AccountID],
          Text: [data.Text, Validators.required],
          Type: [data.Type, Validators.required],
-         ClosingDay: [data.ClosingDay],
-         DueDay: [data.DueDay]
+         ClosingDay: new FormControl({ value: data.ClosingDay, disabled: true }),
+         DueDay: new FormControl({ value: data.DueDay, disabled: true })
       });
+      this.inputForm.controls['Type'].valueChanges.subscribe(type => {
+         this.OnAccountTypeChanged(type, 'ClosingDay');
+         this.OnAccountTypeChanged(type, 'DueDay');
+      });
+      this.OnAccountTypeChanged(data.Type, 'ClosingDay');
+      this.OnAccountTypeChanged(data.Type, 'DueDay');
    }
+
+   private OnAccountTypeChanged(type: enAccountType, controlName: string) {
+      const control = this.inputForm.controls[controlName];
+      if (type == enAccountType.CreditCard) {
+         control.enable();
+         control.setValidators([Validators.required, Validators.min(1), Validators.max(31)]);
+         control.markAsTouched();
+      }
+      else {
+         control.clearValidators();
+         control.markAsUntouched();
+         control.setValue('');
+         control.disable();
+      }
+      control.updateValueAndValidity();
+   }
+
+
 
 }
