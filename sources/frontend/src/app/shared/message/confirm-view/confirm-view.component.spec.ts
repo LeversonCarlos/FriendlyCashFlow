7c84b21a1f4902abb20e7312fb8d65b232b01c81
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-11 11:34:29 -0300

    fix tests

--- sources/frontend/src/app/shared/message/confirm-view/confirm-view.component.spec.ts
+++ sources/frontend/src/app/shared/message/confirm-view/confirm-view.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TestsModule } from '@elesse/tests';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@elesse/material';
 import { ConfirmViewComponent } from './confirm-view.component';
 
 describe('ConfirmViewComponent', () => {
@@ -9,7 +10,15 @@ describe('ConfirmViewComponent', () => {
    beforeEach(async () => {
       await TestBed.configureTestingModule({
          declarations: [ConfirmViewComponent],
-         imports: [TestsModule]
+         imports: [TestsModule],
+         providers: [{
+            provide: MatDialogRef,
+            useValue: {}
+         },
+         {
+            provide: MAT_DIALOG_DATA,
+            useValue: {} // Add any data you wish to test if it is passed/used correctly
+         }]
       })
          .compileComponents();
    });
