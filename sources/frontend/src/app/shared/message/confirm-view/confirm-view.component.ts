a5250bf12fc21d21fbb3b9e285837f49692383c9
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-11 11:34:19 -0300

    implement confirm view dialog details

--- sources/frontend/src/app/shared/message/confirm-view/confirm-view.component.ts
+++ sources/frontend/src/app/shared/message/confirm-view/confirm-view.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ConfirmData } from '../message.models';
 
 @Component({
    selector: 'elesse-confirm-view',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfirmViewComponent implements OnInit {
 
-   constructor() { }
+   constructor(@Inject(MAT_DIALOG_DATA) public Data: ConfirmData,
+      private dialog: MatDialogRef<ConfirmViewComponent>) { }
 
    ngOnInit(): void {
+   }
+
+   OnCancelClick(): void {
+      this.dialog.close(false);
+   }
+
+   OnConfirmClick(): void {
+      this.dialog.close(true);
    }
 
 }
