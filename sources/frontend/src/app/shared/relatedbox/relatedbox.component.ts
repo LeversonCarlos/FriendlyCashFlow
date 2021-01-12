e5b24fa9facdd35e6e976c1c49854a1f610c73c7
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-12 15:09:16 -0300

    on init

--- sources/frontend/src/app/shared/relatedbox/relatedbox.component.ts
+++ sources/frontend/src/app/shared/relatedbox/relatedbox.component.ts
@@ -1,5 +1,9 @@
-import { Component, ElementRef, EventEmitter, OnDestroy, OnInit, Optional, Output, Self } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Optional, Output, Self, ViewChild } from '@angular/core';
 import { ControlValueAccessor, NgControl } from '@angular/forms';
+import { MatAutocompleteTrigger } from '@angular/material/autocomplete';
+import { fromEvent, Observable } from 'rxjs';
+import { debounceTime, map } from 'rxjs/operators';
+import { RelatedData } from './relatedbox.models';
 
 @Component({
    selector: 'elesse-relatedbox',
@@ -16,9 +20,28 @@ export class RelatedboxComponent implements OnInit, OnDestroy, ControlValueAcces
    }
 
    ngOnInit(): void {
+      this.inputValue = this.value && this.value.description;
+      this.inputValueChanged = fromEvent(this.elRef.nativeElement, 'keyup')
+         .pipe(
+            map(() => this.inputValue),
+            debounceTime(this.delay)
+         );
+      this.inputValueChanged.subscribe(val => this.OnInputValueChanging(val));
    }
 
+   /* PROPERTIES */
+   @ViewChild(MatAutocompleteTrigger, { read: MatAutocompleteTrigger }) autoComplete: MatAutocompleteTrigger;
    @Output() public optionsChanging: EventEmitter<string>;
+   @Input() public placeholder: string = '';
+   @Input() public disabled: boolean = false;
+   @Input() public delay: number = 500;
+   @Input() public value: RelatedData<any>;
+   @Input() public minSize: number = 0;
+   @Input() public textSuffix: string = '';
+
+   /* INPUT VALUE */
+   public inputValue: string;
+   private inputValueChanged: Observable<string>;
 
    writeValue(obj: any): void {
       throw new Error('Method not implemented.');
