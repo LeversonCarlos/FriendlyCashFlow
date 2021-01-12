4a3b1b0fc457d0cb47415a20542600e322864187
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-12 19:18:50 -0300

    fix tests

--- sources/frontend/src/app/accounts/list/list.component.spec.ts
+++ sources/frontend/src/app/accounts/list/list.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TestsModule } from '@elesse/tests';
+import { ListBodyComponent } from './list-body/list-body.component';
 import { ListComponent } from './list.component';
 
 describe('ListComponent', () => {
@@ -9,7 +10,7 @@ describe('ListComponent', () => {
 
    beforeEach(async () => {
       await TestBed.configureTestingModule({
-         declarations: [ListComponent],
+         declarations: [ListComponent, ListBodyComponent],
          imports: [TestsModule]
       })
       .compileComponents();
