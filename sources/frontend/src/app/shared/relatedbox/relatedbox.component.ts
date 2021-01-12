1ca07f54ea0ffd2df4770355e624051adfbe5163
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-12 14:55:57 -0300

    implement component interfaces

--- sources/frontend/src/app/shared/relatedbox/relatedbox.component.ts
+++ sources/frontend/src/app/shared/relatedbox/relatedbox.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ControlValueAccessor } from '@angular/forms';
 
 @Component({
    selector: 'elesse-relatedbox',
    templateUrl: './relatedbox.component.html',
    styleUrls: ['./relatedbox.component.scss']
 })
-export class RelatedboxComponent implements OnInit {
+export class RelatedboxComponent implements OnInit, OnDestroy, ControlValueAccessor {
 
    constructor() { }
 
    ngOnInit(): void {
+   }
+
+   writeValue(obj: any): void {
+      throw new Error('Method not implemented.');
+   }
+   registerOnChange(fn: any): void {
+      throw new Error('Method not implemented.');
+   }
+   registerOnTouched(fn: any): void {
+      throw new Error('Method not implemented.');
+   }
+   setDisabledState?(isDisabled: boolean): void {
+      throw new Error('Method not implemented.');
+   }
+
+   ngOnDestroy(): void {
+      throw new Error('Method not implemented.');
    }
 
 }
