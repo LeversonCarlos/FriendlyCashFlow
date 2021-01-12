41f59d2b945f838805509af14944e01ba457462e
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-12 15:28:59 -0300

    account type data class

--- sources/frontend/src/app/accounts/accounts.data.ts
+++ sources/frontend/src/app/accounts/accounts.data.ts
@@ -1,4 +1,8 @@
+import { EnumData } from "@elesse/shared";
+
 export enum enAccountType { General = 0, Bank = 1, CreditCard = 2, Investment = 3, Service = 4 }
+
+export class AccountType extends EnumData<enAccountType> { }
 
 export class AccountEntity {
    AccountID: string;
