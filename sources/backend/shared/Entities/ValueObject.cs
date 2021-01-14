396fbe8907b0b926dd3978cac773799f6554df71
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-14 11:25:51 -0300

    enhance code

--- sources/backend/shared/Entities/ValueObject.cs
+++ sources/backend/shared/Entities/ValueObject.cs
@@ -35,22 +35,18 @@ namespace Elesse.Shared
       public override bool Equals(object obj)
       {
          if (obj == null || obj.GetType() != GetType())
-         {
             return false;
-         }
+
          ValueObject other = (ValueObject)obj;
          IEnumerator<object> thisValues = GetAtomicValues().GetEnumerator();
          IEnumerator<object> otherValues = other.GetAtomicValues().GetEnumerator();
+
          while (thisValues.MoveNext() && otherValues.MoveNext())
          {
             if (ReferenceEquals(thisValues.Current, null) ^ ReferenceEquals(otherValues.Current, null))
-            {
                return false;
-            }
             if (thisValues.Current != null && !thisValues.Current.Equals(otherValues.Current))
-            {
                return false;
-            }
          }
          return !thisValues.MoveNext() && !otherValues.MoveNext();
       }
