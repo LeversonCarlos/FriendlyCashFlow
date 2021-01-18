533bbaa32b6c023c41417b4b6152122b1b06b965
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-18 10:03:47 -0300

    implement the category service builder

--- sources/backend/shared/Helpers/Internals.cs
+++ sources/backend/shared/Helpers/Internals.cs
@@ -3,3 +3,4 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
 [assembly: InternalsVisibleTo("identity")]
 [assembly: InternalsVisibleTo("accounts")]
+[assembly: InternalsVisibleTo("categories")]
