89105b6240a9ff9d313031bc242d1faee3cff62a
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-18 10:01:13 -0300

    implement the service mocker

---
+++ sources/backend/categories/Mockers/CategoryService.Mockers.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace Elesse.Categories.Tests
+{
+   internal class CategoryServiceMocker
+   {
+
+      readonly Mock<ICategoryService> _Mock;
+      public CategoryServiceMocker() => _Mock = new Mock<ICategoryService>();
+      public static CategoryServiceMocker Create() => new CategoryServiceMocker();
+
+      public CategoryServiceMocker WithList(ActionResult<ICategoryEntity[]> result)
+      {
+         _Mock.Setup(m => m.ListAsync()).ReturnsAsync(result);
+         return this;
+      }
+
+      public CategoryServiceMocker WithLoad(string categoryID, ActionResult<ICategoryEntity> result)
+      {
+         _Mock.Setup(m => m.LoadAsync(categoryID)).ReturnsAsync(result);
+         return this;
+      }
+
+      public CategoryServiceMocker WithInsert(InsertVM param, IActionResult result)
+      {
+         _Mock.Setup(m => m.InsertAsync(param)).ReturnsAsync(result);
+         return this;
+      }
+
+      public CategoryServiceMocker WithUpdate(UpdateVM param, IActionResult result)
+      {
+         _Mock.Setup(m => m.UpdateAsync(param)).ReturnsAsync(result);
+         return this;
+      }
+
+      public CategoryServiceMocker WithDelete(string categoryID, IActionResult result)
+      {
+         _Mock.Setup(m => m.DeleteAsync(categoryID)).ReturnsAsync(result);
+         return this;
+      }
+
+      public ICategoryService Build() => _Mock.Object;
+   }
+}
