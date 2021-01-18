ec6d7072c9604421800a2d8cbf51f8356bc514c9
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-18 20:53:43 -0300

    info function to write results

--- sources/backend/shared/Results/Results.Message.cs
+++ sources/backend/shared/Results/Results.Message.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace Elesse.Shared
 {
@@ -6,7 +7,13 @@ namespace Elesse.Shared
    partial class Results
    {
 
-      public static BadRequestObjectResult Message<Message>(params Message messageList) =>
+      public static BadRequestObjectResult Info(params string[] messageList) =>
+         Message(enMessageType.Info, messageList);
+
+      static BadRequestObjectResult Message(enMessageType type, string[] messageList) =>
+         Message(messageList.Select(text => new Message(type, text)).ToArray());
+
+      static BadRequestObjectResult Message(Message[] messageList) =>
          new BadRequestObjectResult(messageList);
 
    }
