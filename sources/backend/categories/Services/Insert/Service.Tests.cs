e20b0434df9bb7b4d20a9792d008c2dd945a5f06
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-18 19:21:20 -0300

    write tests for the insert function

--- sources/backend/categories/Services/Insert/Service.Tests.cs
+++ sources/backend/categories/Services/Insert/Service.Tests.cs
@@ -5,11 +5,10 @@ namespace Elesse.Categories.Tests
    partial class CategoryServiceTests
    {
 
-      /*
       [Fact]
       public async void Insert_WithNullParameter_MustReturnBadResult()
       {
-         var service = AccountService.Create(null);
+         var service = CategoryService.Create(null);
 
          var result = await service.InsertAsync(null);
 
@@ -18,28 +17,16 @@ namespace Elesse.Categories.Tests
          Assert.Equal(new string[] { WARNINGS.INVALID_INSERT_PARAMETER }, (result as Microsoft.AspNetCore.Mvc.BadRequestObjectResult).Value);
       }
 
-      [Fact]
-      public async void Insert_WithInvalidType_MustReturnBadResult()
-      {
-         var service = AccountService.Create(null);
-
-         var param = new InsertVM { Text = "Account Text", Type = enAccountType.Bank, ClosingDay = 1, DueDay = 1 };
-         var result = await service.InsertAsync(param);
-
-         Assert.NotNull(result);
-         Assert.IsType<Microsoft.AspNetCore.Mvc.BadRequestObjectResult>(result);
-         Assert.Equal(new string[] { WARNINGS.DAYS_ONLY_VALID_FOR_CREDIT_CARD_TYPE }, (result as Microsoft.AspNetCore.Mvc.BadRequestObjectResult).Value);
-      }
-
+      /*
       [Fact]
       public async void Insert_WithExistingText_MustReturnBadRequest()
       {
          var repository = AccountRepositoryMocker
             .Create()
-            .WithSearchAccounts(null, new AccountEntity[] { new AccountEntity("Account Text", enAccountType.General, null, null) })
+            .WithSearchAccounts(null, new AccountEntity[] { new AccountEntity("Category Text", enAccountType.General, null, null) })
             .Build();
-         var service = AccountService.Create(repository);
-         var param = new InsertVM { Text = "Account Text", Type = enAccountType.General };
+         var service = CategoryService.Create(repository);
+         var param = new InsertVM { Text = "Category Text", Type = enAccountType.General };
 
          var result = await service.InsertAsync(param);
          Assert.NotNull(result);
@@ -50,23 +37,23 @@ namespace Elesse.Categories.Tests
          Assert.IsType<Microsoft.AspNetCore.Mvc.BadRequestObjectResult>(result);
          Assert.Equal(new string[] { WARNINGS.ACCOUNT_TEXT_ALREADY_USED }, (result as Microsoft.AspNetCore.Mvc.BadRequestObjectResult).Value);
       }
+      */
 
       [Fact]
       public async void Insert_WithValidParameters_MustReturnOkResult()
       {
-         var repository = AccountRepositoryMocker
+         var repository = CategoryRepositoryMocker
             .Create()
-            .WithSearchAccounts()
+            // .WithSearchAccounts()
             .Build();
-         var service = AccountService.Create(repository);
+         var service = CategoryService.Create(repository);
 
-         var param = new InsertVM { Text = "Account Text", Type = enAccountType.General };
+         var param = new InsertVM { Text = "Category Text", Type = enCategoryType.Income };
          var result = await service.InsertAsync(param);
 
          Assert.NotNull(result);
          Assert.IsType<Microsoft.AspNetCore.Mvc.OkResult>(result);
       }
-      */
 
    }
 }
