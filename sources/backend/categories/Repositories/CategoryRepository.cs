7e3d1fc17f6b823aae0a9889a201ece191cccc7b
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-19 11:40:25 -0300

    review repository constructor

--- sources/backend/categories/Repositories/CategoryRepository.cs
+++ sources/backend/categories/Repositories/CategoryRepository.cs
@@ -1,10 +1,21 @@
-using System.Threading.Tasks;
-using Elesse.Shared;
+using MongoDB.Driver;
 
 namespace Elesse.Categories
 {
    internal partial class CategoryRepository : ICategoryRepository
    {
+
+      public CategoryRepository(IMongoDatabase mongoDatabase, Identity.IUser currentUser)
+      {
+         _MongoDatabase = mongoDatabase;
+         _CollectionName = $"{currentUser.UserID}-categories";
+      }
+
+      readonly IMongoDatabase _MongoDatabase;
+
+      readonly string _CollectionName = null;
+      IMongoCollection<CategoryEntity> _Collection =>
+         _MongoDatabase.GetCollection<CategoryEntity>(_CollectionName);
 
    }
 }
