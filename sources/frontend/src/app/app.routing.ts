82ef39d9e578b98f0137ad40e8236164cb2887d5
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-19 13:29:31 -0300

    add module to the main app routing thrgh lazy loading

--- sources/frontend/src/app/app.routing.ts
+++ sources/frontend/src/app/app.routing.ts
@@ -8,6 +8,7 @@ const routes: Routes = [
    { path: 'home', component: HomeComponent },
    { path: 'identity', loadChildren: () => import('./identity/identity.module').then(m => m.IdentityModule) },
    { path: 'accounts', canActivate: [AuthGuard], loadChildren: () => import('./accounts/accounts.module').then(m => m.AccountsModule) },
+   { path: 'categories', canActivate: [AuthGuard], loadChildren: () => import('./categories/categories.module').then(m => m.CategoriesModule) },
    { path: '**', component: HomeComponent }
 ];
 
