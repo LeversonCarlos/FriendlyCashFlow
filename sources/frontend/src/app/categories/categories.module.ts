9367dbb079cc385a947238d74b86e165223ef274
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-19 13:27:27 -0300

    create module for the categories

---
+++ sources/frontend/src/app/categories/categories.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CategoriesRouting } from './categories.routing';
+
+@NgModule({
+   declarations: [],
+   imports: [
+      CommonModule,
+      CategoriesRouting
+   ]
+})
+export class CategoriesModule { }
