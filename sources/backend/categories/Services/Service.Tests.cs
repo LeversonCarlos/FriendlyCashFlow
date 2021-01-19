6b594ac46fde9af7fe644a6257a9e08143188dca
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-19 11:04:30 -0300

    review the results helper function

--- sources/backend/categories/Services/Service.Tests.cs
+++ sources/backend/categories/Services/Service.Tests.cs
@@ -3,5 +3,8 @@ namespace Elesse.Categories.Tests
    public partial class CategoryServiceTests
    {
 
+      Shared.Results Warning(params string[] messageList) =>
+         Shared.Results.GetResults("categories", Shared.enResultType.Warning, messageList);
+
    }
 }
