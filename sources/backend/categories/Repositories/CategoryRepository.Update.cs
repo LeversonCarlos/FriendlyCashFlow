5e991eef473649729fefb0238a4e40229bcdc611
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-20 16:09:16 -0300

    Review hierarchy text from the child categories on update function

--- sources/backend/categories/Repositories/CategoryRepository.Update.cs
+++ sources/backend/categories/Repositories/CategoryRepository.Update.cs
@@ -15,6 +15,23 @@ namespace Elesse.Categories
 
          await _Collection
             .ReplaceOneAsync(entity => entity.CategoryID == categoryEntity.CategoryID, categoryEntity);
+
+         await UpdateChildrenHierarchyText(categoryEntity.CategoryID);
+      }
+
+      async Task UpdateChildrenHierarchyText(Shared.EntityID categoryID)
+      {
+         var children = await _Collection
+            .Find(entity => entity.ParentID == categoryID)
+            .ToListAsync();
+         foreach (var child in children)
+         {
+            child.HierarchyText = $"{await GetParentText(categoryID)}{child.Text}";
+            var builder = Builders<CategoryEntity>.Update.Set(x => x.HierarchyText, child.HierarchyText);
+            await _Collection
+               .UpdateOneAsync(entity => entity.CategoryID == child.CategoryID, builder);
+            await UpdateChildrenHierarchyText(child.CategoryID);
+         }
       }
 
    }
