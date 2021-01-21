48d899db8018b10658f294fa2f4324e2a2601941
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-21 10:18:02 -0300

    implement mockers for the pattersn api sservices

---
+++ sources/backend/patterns/Mockers/PatternRepository.Mockers.cs
@@ -0,0 +1,57 @@
+using Moq;
+
+namespace Elesse.Patterns.Tests
+{
+   internal class PatternRepositoryMocker
+   {
+
+      readonly Mock<IPatternRepository> _Mock;
+      public PatternRepositoryMocker() => _Mock = new Mock<IPatternRepository>();
+      public static PatternRepositoryMocker Create() => new PatternRepositoryMocker();
+
+      public PatternRepositoryMocker WithList() =>
+         WithList(new IPatternEntity[] { });
+      public PatternRepositoryMocker WithList(params IPatternEntity[][] results)
+      {
+         var seq = new MockSequence();
+         foreach (var result in results)
+            _Mock.InSequence(seq)
+               .Setup(m => m.ListPatternsAsync())
+               .ReturnsAsync(result);
+         return this;
+      }
+
+      /*
+      public PatternRepositoryMocker WithSearchPatterns() =>
+         WithSearchPatterns(new IPatternEntity[][] { });
+      public PatternRepositoryMocker WithSearchPatterns(params IPatternEntity[][] results)
+      {
+         var seq = new MockSequence();
+         foreach (var result in results)
+            _Mock.InSequence(seq)
+               .Setup(m => m.SearchPatternsAsync(It.IsAny<enPatternType>(), It.IsAny<string>()))
+               .ReturnsAsync(result);
+         var seq2 = new MockSequence();
+         foreach (var result in results)
+            _Mock.InSequence(seq2)
+               .Setup(m => m.SearchPatternsAsync(It.IsAny<enPatternType>(), It.IsAny<Shared.EntityID>(), It.IsAny<string>()))
+               .ReturnsAsync(result);
+         return this;
+      }
+      */
+
+      public PatternRepositoryMocker WithLoadPattern() =>
+         WithLoadPattern(new IPatternEntity[] { });
+      public PatternRepositoryMocker WithLoadPattern(params IPatternEntity[] results)
+      {
+         var seq = new MockSequence();
+         foreach (var result in results)
+            _Mock.InSequence(seq)
+               .Setup(m => m.LoadPatternAsync(It.IsAny<Shared.EntityID>()))
+               .ReturnsAsync(result);
+         return this;
+      }
+
+      public IPatternRepository Build() => _Mock.Object;
+   }
+}
