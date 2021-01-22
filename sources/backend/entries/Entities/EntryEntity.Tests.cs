896f0801498ed0324e4d62bf109058454cc01b71
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-22 15:41:49 -0300

    some mre tests

--- sources/backend/entries/Entities/EntryEntity.Tests.cs
+++ sources/backend/entries/Entities/EntryEntity.Tests.cs
@@ -10,13 +10,19 @@ namespace Elesse.Entries.Tests
       [Fact]
       public void Constructor_WithValidParameters_MustResultValidInstance()
       {
+         var accountID = Shared.EntityID.NewID();
+         var dueDate = DateTime.UtcNow;
+         var entryValue = (decimal)23.45;
          var pattern = new Patterns.PatternEntity(Patterns.enPatternType.Income, Shared.EntityID.NewID(), "My Pattern");
-         var entity = new EntryEntity(pattern, Shared.EntityID.NewID(), DateTime.UtcNow, (decimal)23.45);
+         var entity = new EntryEntity(pattern, accountID, dueDate, entryValue);
 
          Assert.NotNull(entity);
          Assert.NotNull(entity.EntryID);
          Assert.Equal(36, ((string)entity.EntryID).Length);
          Assert.Equal(pattern.PatternID, entity.Pattern.PatternID);
+         Assert.Equal(accountID, entity.AccountID);
+         Assert.Equal(dueDate, entity.DueDate);
+         Assert.Equal(entryValue, entity.EntryValue);
          Assert.False(entity.Paid);
          Assert.Null(entity.PayDate);
       }
