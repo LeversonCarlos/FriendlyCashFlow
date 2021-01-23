85a3b2ca81f8a44e529ff9542bea974df4cea789
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-23 10:50:03 -0300

    write more tests for the restore function

--- sources/backend/entries/Entities/EntryEntity.Restore.Tests.cs
+++ sources/backend/entries/Entities/EntryEntity.Restore.Tests.cs
@@ -20,14 +20,37 @@ namespace Elesse.Entries.Tests
          new[] {
             new object[] { WARNINGS.INVALID_ENTRYID, null, null, null, null, null, null, null},
             new object[] { WARNINGS.INVALID_PATTERN, Shared.EntityID.NewID(), null, null, null, null, null, null},
-            new object[] { WARNINGS.INVALID_ACCOUNTID, Shared.EntityID.NewID(), new Patterns.PatternEntity(Patterns.enPatternType.Income, Shared.EntityID.NewID(), "My Pattern"), null, null, null, null, null},
-            new object[] { WARNINGS.INVALID_DUEDATE, Shared.EntityID.NewID(), new Patterns.PatternEntity(Patterns.enPatternType.Income, Shared.EntityID.NewID(), "My Pattern"), Shared.EntityID.NewID(), null, null, null, null},
-            new object[] { WARNINGS.INVALID_DUEDATE, Shared.EntityID.NewID(), new Patterns.PatternEntity(Patterns.enPatternType.Income, Shared.EntityID.NewID(), "My Pattern"), Shared.EntityID.NewID(), DateTime.MinValue, null, null, null},
-            new object[] { WARNINGS.INVALID_ENTRYVALUE, Shared.EntityID.NewID(), new Patterns.PatternEntity(Patterns.enPatternType.Income, Shared.EntityID.NewID(), "My Pattern"), Shared.EntityID.NewID(), DateTime.UtcNow, null, null, null},
-            new object[] { WARNINGS.INVALID_ENTRYVALUE, Shared.EntityID.NewID(), new Patterns.PatternEntity(Patterns.enPatternType.Income, Shared.EntityID.NewID(), "My Pattern"), Shared.EntityID.NewID(), DateTime.UtcNow, 0, null, null},
-            new object[] { WARNINGS.INVALID_ENTRYVALUE, Shared.EntityID.NewID(), new Patterns.PatternEntity(Patterns.enPatternType.Income, Shared.EntityID.NewID(), "My Pattern"), Shared.EntityID.NewID(), DateTime.UtcNow, -0.01, null, null },
-            new object[] { WARNINGS.INVALID_PAYDATE, Shared.EntityID.NewID(), new Patterns.PatternEntity(Patterns.enPatternType.Income, Shared.EntityID.NewID(), "My Pattern"), Shared.EntityID.NewID(), DateTime.UtcNow, 1, true, DateTime.MinValue }
+            new object[] { WARNINGS.INVALID_ACCOUNTID, Shared.EntityID.NewID(), Patterns.PatternEntity.Mock(), null, null, null, null, null},
+            new object[] { WARNINGS.INVALID_DUEDATE, Shared.EntityID.NewID(), Patterns.PatternEntity.Mock(), Shared.EntityID.NewID(), null, null, null, null},
+            new object[] { WARNINGS.INVALID_DUEDATE, Shared.EntityID.NewID(), Patterns.PatternEntity.Mock(), Shared.EntityID.NewID(), DateTime.MinValue, null, null, null},
+            new object[] { WARNINGS.INVALID_ENTRYVALUE, Shared.EntityID.NewID(), Patterns.PatternEntity.Mock(), Shared.EntityID.NewID(), DateTime.UtcNow, null, null, null},
+            new object[] { WARNINGS.INVALID_ENTRYVALUE, Shared.EntityID.NewID(), Patterns.PatternEntity.Mock(), Shared.EntityID.NewID(), DateTime.UtcNow, 0, null, null},
+            new object[] { WARNINGS.INVALID_ENTRYVALUE, Shared.EntityID.NewID(), Patterns.PatternEntity.Mock(), Shared.EntityID.NewID(), DateTime.UtcNow, -0.01, null, null },
+            new object[] { WARNINGS.INVALID_PAYDATE, Shared.EntityID.NewID(), Patterns.PatternEntity.Mock(), Shared.EntityID.NewID(), DateTime.UtcNow, 1, true, DateTime.MinValue }
          };
+
+      [Fact]
+      public void Restore_WithValidParameters_MustResultInstance()
+      {
+         var entryID = Shared.EntityID.NewID();
+         var pattern = Patterns.PatternEntity.Mock();
+         var accountID = Shared.EntityID.NewID();
+         var dueDate = DateTime.Now.AddDays(1);
+         var entryValue = (decimal)23.45;
+         var paid = true;
+         var payDate = DateTime.Now.AddDays(1);
+
+         var result = EntryEntity.Restore(entryID, pattern, accountID, dueDate, entryValue, paid, payDate);
+
+         Assert.NotNull(result);
+         Assert.Equal(entryID, result.EntryID);
+         Assert.Equal(pattern, result.Pattern);
+         Assert.Equal(accountID, result.AccountID);
+         Assert.Equal(dueDate, result.DueDate);
+         Assert.Equal(entryValue, result.EntryValue);
+         Assert.Equal(paid, result.Paid);
+         Assert.Equal(payDate, result.PayDate);
+      }
 
    }
 }
