b86b0af406e11ae49d96b974dc36e3755f7b00a2
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-23 08:58:57 -0300

    initialize entry repository

---
+++ sources/backend/entries/Repositories/IEntryRepository.cs
@@ -0,0 +1,16 @@
+using System.Threading.Tasks;
+
+namespace Elesse.Entries
+{
+   internal interface IEntryRepository
+   {
+
+      Task InsertAsync(IEntryEntity value);
+      Task UpdateAsync(IEntryEntity value);
+      Task DeleteAsync(Shared.EntityID entryID);
+
+      Task<IEntryEntity[]> ListAsync(int year, int month);
+      Task<IEntryEntity> LoadAsync(Shared.EntityID entryID);
+
+   }
+}
