e872f8f73b7944e07bcb1bda5c1b5e94712aa603
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-23 09:06:19 -0300

    implement the list function

--- sources/backend/entries/Repositories/EntryRepository.List.cs
+++ sources/backend/entries/Repositories/EntryRepository.List.cs
@@ -1,12 +1,21 @@
+using System;
 using System.Threading.Tasks;
+using MongoDB.Driver;
 
 namespace Elesse.Entries
 {
    partial class EntryRepository
    {
 
-      public Task<IEntryEntity[]> ListAsync(int year, int month) =>
-         throw new System.NotImplementedException();
+      public async Task<IEntryEntity[]> ListAsync(int year, int month)
+      {
+         var limitDate = new DateTime(year, month, 1, 0, 0, 0);
+
+         var list = await _Collection
+            .Find(entity => entity.SearchDate >= limitDate)
+            .ToListAsync();
+         return list.ToArray();
+      }
 
    }
 }
