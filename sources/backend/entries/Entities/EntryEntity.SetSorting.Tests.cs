8eeeb3a1947dad19b07372a68d9acb553e82741b
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-23 12:09:50 -0300

    move convert to entity class

--- sources/backend/entries/Entities/EntryEntity.SetSorting.Tests.cs
+++ sources/backend/entries/Entities/EntryEntity.SetSorting.Tests.cs
@@ -7,13 +7,25 @@ namespace Elesse.Entries.Tests
    {
 
       [Fact]
-      public void SetSorting_WithValidParameters_MustReflectChanges()
+      public void SetSorting_WithoutPayment_MustReflectChangesBasedOnDueDate()
       {
-         var entity = EntryEntity.Create(Patterns.PatternEntity.Mock(), Shared.EntityID.NewID(), DateTime.Now.AddDays(10), (decimal)54.32);
+         var entity = EntryEntity.Create(Patterns.PatternEntity.Mock(), Shared.EntityID.NewID(), DateTime.Now.AddDays(1), (decimal)54.32);
 
          entity.SetSorting();
 
          Assert.Equal(entity.DueDate, entity.SearchDate);
+         Assert.True(entity.Sorting > 0);
+      }
+
+      [Fact]
+      public void SetSorting_WithPayment_MustReflectChangesBasedOnPayDate()
+      {
+         var entity = EntryEntity.Create(Patterns.PatternEntity.Mock(), Shared.EntityID.NewID(), DateTime.Now.AddDays(1), (decimal)54.32);
+         entity.SetPayment(DateTime.Now.AddDays(2), entity.EntryValue);
+
+         entity.SetSorting();
+
+         Assert.Equal(entity.PayDate, entity.SearchDate);
          Assert.True(entity.Sorting > 0);
       }
 
--- sources/backend/entries/Entities/EntryEntity.SetSorting.cs
+++ sources/backend/entries/Entities/EntryEntity.SetSorting.cs
@@ -14,23 +14,8 @@ namespace Elesse.Entries
 
          Sorting = System.Convert.ToInt64(SearchDate.Subtract(new DateTime(1901, 1, 1)).TotalDays);
 
-         var longEntityID = Convert(EntryID);
+         var longEntityID = EntryID.ToLong();
          Sorting += ((decimal)longEntityID / (decimal)Math.Pow(10, longEntityID.ToString().Length));
-      }
-
-      long Convert(Shared.EntityID entityID)
-      {
-
-         var bytes = entityID.Value.ToByteArray();
-         Array.Resize(ref bytes, 17);
-
-         var bigInt = new System.Numerics.BigInteger(bytes);
-
-         var logValue = System.Numerics.BigInteger.Log10(bigInt);
-         var doubleValue = logValue * Math.Pow(10, 15);
-         var intValue = Math.Round(doubleValue, 0);
-
-         return (long)intValue;
       }
 
    }
