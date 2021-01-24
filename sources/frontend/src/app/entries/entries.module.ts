e5ffd9c33c04115b3bd019c58d8cd70efdda6fd9
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-24 14:16:01 -0300

    create first compoent for themodule

--- sources/frontend/src/app/entries/entries.module.ts
+++ sources/frontend/src/app/entries/entries.module.ts
@@ -3,9 +3,10 @@ import { MaterialModule } from '../material/material.exports';
 import { SharedModule } from '../shared/shared.exports';
 
 import { EntriesRouting } from './entries.routing';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
-   declarations: [],
+   declarations: [ListComponent],
    imports: [
       MaterialModule, SharedModule, ,
       EntriesRouting
--- sources/frontend/src/app/entries/entries.routing.ts
+++ sources/frontend/src/app/entries/entries.routing.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListComponent } from './list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   { path: '', redirectTo: 'list', pathMatch: 'full' },
+   { path: 'list', component: ListComponent },
+];
 
 @NgModule({
    imports: [RouterModule.forChild(routes)],
