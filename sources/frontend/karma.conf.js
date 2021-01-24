32e9599a651f65aa2be362207ea289c3c7b0437b
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-23 21:27:13 -0300

    write coverage on cobertura forma to be sent to codecov

--- sources/frontend/karma.conf.js
+++ sources/frontend/karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function (config) {
       },
       coverageIstanbulReporter: {
          dir: require('path').join(__dirname, './'),
-         reports: ['lcovonly'],
+         reports: ['lcovonly','cobertura'],
          fixWebpackSourcePaths: true,
          'report-config': {
             lcovonly: {
