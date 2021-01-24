e96cbc626218dd679c940a042b6a4d48c6421395
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-24 14:22:09 -0300

    add service for the module

---
+++ sources/frontend/src/app/entries/entries.service.spec.ts
@@ -0,0 +1,19 @@
+import { TestBed } from '@angular/core/testing';
+import { TestsModule } from '@elesse/tests';
+import { EntriesService } from './entries.service';
+
+describe('EntriesService', () => {
+   let service: EntriesService;
+
+   beforeEach(() => {
+      TestBed.configureTestingModule({
+         imports: [TestsModule]
+      });
+      service = TestBed.inject(EntriesService);
+   });
+
+   it('should be created', () => {
+      expect(service).toBeTruthy();
+   });
+
+});
