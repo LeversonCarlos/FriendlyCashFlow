fa9a24ee10d86243cd878c59e11daa6ed63b81e4
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-24 10:16:23 -0300

    rite tests for the ok result

---
+++ sources/backend/shared/Results/Results.Ok.Tests.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace Elesse.Shared.Tests
+{
+   public partial class ResultsTests
+   {
+
+      [Fact]
+      public void Ok_WithData_MustResultOkObjectResult()
+      {
+         var param = EntityID.MockerID();
+
+         var result = Results.Ok(param);
+
+         Assert.NotNull(result);
+         Assert.IsType<OkObjectResult>(result);
+      }
+
+      [Fact]
+      public void Ok_WithoutData_MustResultOkResult()
+      {
+
+         var result = Results.Ok();
+
+         Assert.NotNull(result);
+         Assert.IsType<OkResult>(result);
+      }
+
+   }
+}
