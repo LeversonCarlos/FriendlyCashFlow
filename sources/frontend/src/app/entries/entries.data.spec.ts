3dfda6a8089c4ba668bfb8c8b5c28614546f1837
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-24 14:57:04 -0300

    write mocker for the entry entury class

--- sources/frontend/src/app/entries/entries.data.spec.ts
+++ sources/frontend/src/app/entries/entries.data.spec.ts
@@ -7,15 +7,63 @@ describe('EntryEntity', () => {
    });
 
    it('should parse anonymous type and materialize properties', () => {
-      const EntryID = "EntryID";
+      const entryID = "my entry id";
       const Pattern = PatternEntityMocker.Create().Build();
 
-      const entry = EntryEntity.Parse({ EntryID, Pattern });
+      const entry = EntryEntityMocker
+         .Create()
+         .WithEntryID(entryID)
+         .Build();
 
-      expect(entry.EntryID).toEqual(EntryID);
+      expect(entry.EntryID).toEqual(entryID);
    });
 
 });
+
+export class EntryEntityMocker {
+   public static Create(): EntryEntityMocker { return new EntryEntityMocker(); }
+
+   private _EntryEntity: EntryEntity = EntryEntity.Parse({
+      EntryID: 'EntryID',
+      Pattern: PatternEntityMocker.Create().Build(),
+      AccountID: 'AccountID',
+      DueDate: new Date(),
+      EntryValue: 0.01
+   });
+
+   public WithEntryID(entryID: string): EntryEntityMocker {
+      this._EntryEntity.EntryID = entryID;
+      return this;
+   }
+
+   public WithPattern(pattern: PatternEntity): EntryEntityMocker {
+      this._EntryEntity.Pattern = pattern;
+      return this;
+   }
+
+   public WithAccountID(accountID: string): EntryEntityMocker {
+      this._EntryEntity.AccountID = accountID;
+      return this;
+   }
+
+   public WithDueDate(dueDate: Date): EntryEntityMocker {
+      this._EntryEntity.DueDate = dueDate;
+      return this;
+   }
+
+   public WithEntryValue(entryValue: number): EntryEntityMocker {
+      this._EntryEntity.EntryValue = entryValue;
+      return this;
+   }
+
+   public WithPayDate(payDate: Date): EntryEntityMocker {
+      this._EntryEntity.Paid = true;
+      this._EntryEntity.PayDate = payDate;
+      return this;
+   }
+
+   public Build(): EntryEntity { return this._EntryEntity; }
+}
 
 export class PatternEntityMocker {
    public static Create(): PatternEntityMocker { return new PatternEntityMocker(); }
