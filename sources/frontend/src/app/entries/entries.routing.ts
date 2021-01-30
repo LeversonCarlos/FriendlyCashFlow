b4c15659a60e64801d45b652ca06dc61fd9eb02d
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-30 14:06:09 -0300

    add route to new component

--- sources/frontend/src/app/entries/entries.routing.ts
+++ sources/frontend/src/app/entries/entries.routing.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetailsRouteViewComponent } from './details/route-view/route-view.component';
 import { ListComponent } from './list/list.component';
 
 const routes: Routes = [
    { path: '', redirectTo: 'list', pathMatch: 'full' },
    { path: 'list', component: ListComponent },
+   { path: 'details/:id', component: DetailsRouteViewComponent },
 ];
 
 @NgModule({
