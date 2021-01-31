ef9b01383e4f8d5ebf5403f28aac8e8612762a03
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-31 14:58:15 -0300

    write simple test to the entity parse

--- sources/frontend/src/app/patterns/model/patterns.model.spec.ts
+++ sources/frontend/src/app/patterns/model/patterns.model.spec.ts
@@ -1,9 +1,29 @@
-import { PatternsData } from './patterns.model';
+import { enCategoryType } from '@elesse/categories';
+import { PatternEntity } from './patterns.model';
 
-describe('PatternsData', () => {
+describe('PatternEntity', () => {
 
    it('should create an instance', () => {
-      expect(new PatternsData()).toBeTruthy();
+      expect(new PatternEntity()).toBeTruthy();
+   });
+
+   it('should parse to correct an instance', () => {
+      const patternID = "my pattern id";
+      const type = enCategoryType.Income;
+      const categoryID = "my category id";
+      const text = "my pattern text";
+
+      const pattern = PatternEntity.Parse({
+         PatternID: patternID,
+         Type: type,
+         CategoryID: categoryID,
+         Text: text
+      });
+
+      expect(pattern.PatternID).toEqual(patternID);
+      expect(pattern.Type).toEqual(type);
+      expect(pattern.CategoryID).toEqual(categoryID);
+      expect(pattern.Text).toEqual(text);
    });
 
 });
