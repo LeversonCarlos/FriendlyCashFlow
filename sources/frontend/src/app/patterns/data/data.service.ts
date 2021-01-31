c675cb338df4203efbdd5a7acd7d9adfe1e6a05d
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-31 14:50:54 -0300

    implement the refresh patterns service

--- sources/frontend/src/app/patterns/data/data.service.ts
+++ sources/frontend/src/app/patterns/data/data.service.ts
@@ -1,10 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BusyService } from '@elesse/shared';
+import { PatternsCache } from '../cache/cache.service';
+import { PatternEntity } from '../model/patterns.model';
 
 @Injectable({
    providedIn: 'root'
 })
 export class PatternsData {
 
-   constructor() { }
+   constructor(private busy: BusyService,
+      private http: HttpClient) {
+      this.Cache = new PatternsCache();
+   }
+
+   private Cache: PatternsCache;
+
+   public async RefreshPatterns(): Promise<void> {
+      try {
+         this.busy.show();
+
+         const url = `api/patterns/list`;
+         const values = await this.http.get<PatternEntity[]>(url).toPromise();
+         if (!values)
+            return;
+
+         this.Cache.SetPatterns(values);
+      }
+      catch { /* error absorber */ }
+      finally { this.busy.hide(); }
+   }
 
 }
