ef96826794ca8577cfac12ab2e9e4040f6bc9cde
Leverson Carlos <lcjohnny@hotmail.com>
2021-01-31 14:10:00 -0300

    new module for the patterns entries

---
+++ sources/frontend/src/app/patterns/patterns.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { MaterialModule } from '../material/material.exports';
+import { SharedModule } from '../shared/shared.exports';
+
+@NgModule({
+   declarations: [],
+   imports: [
+      MaterialModule, SharedModule
+   ]
+})
+export class PatternsModule { }
