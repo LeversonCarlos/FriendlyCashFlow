ac92d1ebd7672a94562aff06de1dd3669b6d13d8
Leverson Carlos <lcjohnny@hotmail.com>
2021-02-01 18:57:52 -0300

    extends the storage service

--- sources/frontend/src/app/categories/cache/cache.service.ts
+++ sources/frontend/src/app/categories/cache/cache.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { StorageService } from '@elesse/shared';
+import { CategoryEntity, enCategoryType } from '../model/categories.model';
 
 @Injectable({
    providedIn: 'root'
 })
-export class CategoriesCache {
+export class CategoriesCache extends StorageService<enCategoryType, CategoryEntity[]> {
 
-   constructor() { }
+   constructor() {
+      super("CategoriesCache");
+      this.InitializeValues(...this.CategoryTypes);
+   }
+
+   private get CategoryTypes(): enCategoryType[] {
+      return [
+         enCategoryType.Income,
+         enCategoryType.Expense
+      ];
+   }
+
 }
