60da2bf980d523f33502d95c84c6b4dd7648d832
Leverson Carlos <lcjohnny@hotmail.com>
2021-02-01 19:04:58 -0300

    implement set categories function on cache service

--- sources/frontend/src/app/categories/cache/cache.service.ts
+++ sources/frontend/src/app/categories/cache/cache.service.ts
@@ -19,4 +19,30 @@ export class CategoriesCache extends StorageService<enCategoryType, CategoryEnti
       ];
    }
 
+   public SetCategories(categories: CategoryEntity[]) {
+
+      // SORTER FUNCTION
+      const sorter = (a: CategoryEntity, b: CategoryEntity): number => {
+         let result = 0;
+         if (a.HierarchyText > b.HierarchyText) result += 1;
+         if (a.HierarchyText < b.HierarchyText) result -= 1;
+         return result;
+      }
+
+      // LOOP THROUGH CATEGORY TYPES
+      for (const categoryType of this.CategoryTypes) {
+
+         // FILTER, SORT AND PARSE OBJECTS
+         const value = categories
+            .filter(category => category.Type == categoryType)
+            .map(category => CategoryEntity.Parse(category))
+            .sort(sorter);
+
+         // SET VALUE
+         this.SetValue(categoryType, value);
+
+      }
+
+   }
+
 }
