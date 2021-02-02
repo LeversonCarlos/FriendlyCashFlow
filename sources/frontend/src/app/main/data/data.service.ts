790518892afe1a771f36d8fb63b1f021b466ecfd
Leverson Carlos <lcjohnny@hotmail.com>
2021-02-02 20:05:32 -0300

    must not try to refresh data if has no user authenticated yet

--- sources/frontend/src/app/main/data/data.service.ts
+++ sources/frontend/src/app/main/data/data.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Injector } from '@angular/core';
 import { AccountsData } from '@elesse/accounts';
 import { CategoriesData } from '@elesse/categories';
 import { EntriesData } from '@elesse/entries';
+import { TokenService } from '@elesse/identity';
 import { PatternsData } from '@elesse/patterns';
 
 @Injectable({
@@ -12,6 +13,8 @@ export class MainData {
    constructor(private injector: Injector) { }
 
    public RefreshAll() {
+      if (!this.injector.get<TokenService>(TokenService).HasToken)
+         return;
       console.log("RefreshAll")
       this.injector.get<AccountsData>(AccountsData).RefreshAccounts();
       this.injector.get<CategoriesData>(CategoriesData).RefreshCategories();
