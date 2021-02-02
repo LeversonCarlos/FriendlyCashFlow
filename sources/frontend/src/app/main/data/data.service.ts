fee7734cd2c7ba1dd9fda2578076e63d6ccaf964
Leverson Carlos <lcjohnny@hotmail.com>
2021-02-01 21:15:28 -0300

    request all data service on constructor

--- sources/frontend/src/app/main/data/data.service.ts
+++ sources/frontend/src/app/main/data/data.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
+import { AccountsData } from '@elesse/accounts';
+import { CategoriesData } from '@elesse/categories';
+import { EntriesData } from '@elesse/entries';
 
 @Injectable({
    providedIn: 'root'
 })
 export class MainData {
 
-   constructor() { }
+   constructor(private accounts: AccountsData, categories: CategoriesData, entries: EntriesData) { }
 
 }
