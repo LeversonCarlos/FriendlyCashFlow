81734d5a45340cecd041ecb556be6de851766a07
Leverson Carlos <lcjohnny@hotmail.com>
2021-02-03 19:35:40 -0300

    starting the implemrntation

--- sources/frontend/src/app/entries/details/due-date-view/due-date-view.component.ts
+++ sources/frontend/src/app/entries/details/due-date-view/due-date-view.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { EntryEntity } from '../../model/entries.model';
 
 @Component({
-   selector: 'elesse-due-date-view',
+   selector: 'entries-details-due-date',
    templateUrl: './due-date-view.component.html',
    styleUrls: ['./due-date-view.component.scss']
 })
@@ -10,6 +12,21 @@ export class DueDateViewComponent implements OnInit {
    constructor() { }
 
    ngOnInit(): void {
+      this.OnDataInit();
+      this.OnFormInit();
+   }
+
+   @Input() data: EntryEntity;
+   @Input() form: FormGroup;
+
+   private OnDataInit() {
+      if (!this.data)
+         return;
+   }
+
+   private OnFormInit() {
+      if (!this.form)
+         return;
    }
 
 }
