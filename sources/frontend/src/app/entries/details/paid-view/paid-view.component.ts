54563295dd2015fb933c3373c443c7ad126d113f
Leverson Carlos <lcjohnny@hotmail.com>
2021-02-03 21:56:40 -0300

    implement paid component

--- sources/frontend/src/app/entries/details/paid-view/paid-view.component.ts
+++ sources/frontend/src/app/entries/details/paid-view/paid-view.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { EntryEntity } from '../../model/entries.model';
 
 @Component({
    selector: 'entries-details-paid',
@@ -10,6 +12,44 @@ export class PaidViewComponent implements OnInit {
    constructor() { }
 
    ngOnInit(): void {
+      this.OnDataInit();
+      this.OnFormInit();
+   }
+
+   @Input() data: EntryEntity;
+   @Input() form: FormGroup;
+
+   private OnDataInit() {
+      if (!this.data)
+         return;
+   }
+
+   private OnFormInit() {
+      if (!this.form)
+         return;
+      this.form.addControl("Paid", new FormControl(this.data?.Paid ?? false));
+      this.form.get("Paid").valueChanges.subscribe((val: boolean) => {
+         this.data.Paid = val;
+         /*
+      const payDateControl = this.inputForm.controls['PayDate'];
+      if (paid == true) {
+         const today = new Date();
+         let payDate = new Date(this.Data.DueDate);
+         if (today < payDate) { payDate = today; }
+         payDateControl.enable();
+         payDateControl.setValidators([Validators.required]);
+         if (setValue) { payDateControl.setValue(payDate); }
+         payDateControl.markAsTouched();
+      }
+      else {
+         payDateControl.clearValidators();
+         payDateControl.markAsUntouched();
+         if (setValue) { payDateControl.setValue(''); }
+         payDateControl.disable();
+      }
+      payDateControl.updateValueAndValidity();
+          */
+      });
    }
 
 }
