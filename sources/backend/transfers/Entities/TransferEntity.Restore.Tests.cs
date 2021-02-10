d36e19857721a3d0c467e58a94722fd7c40f4ab0
Leverson Carlos <lcjohnny@hotmail.com>
2021-02-10 19:47:13 -0300

    improve mocks for the restore tests

--- sources/backend/transfers/Entities/TransferEntity.Restore.Tests.cs
+++ sources/backend/transfers/Entities/TransferEntity.Restore.Tests.cs
@@ -10,11 +10,11 @@ namespace Elesse.Transfers.Tests
       [Fact]
       public void Restore_WithValidParameters_MustResultInstance()
       {
-         var transferID = Shared.EntityID.NewID();
-         var expenseAccountID = Shared.EntityID.NewID();
-         var incomeAccountID = Shared.EntityID.NewID();
-         var date = DateTime.Now.AddDays(1);
-         var value = (decimal)23.45;
+         var transferID = Shared.EntityID.MockerID();
+         var expenseAccountID = Shared.EntityID.MockerID();
+         var incomeAccountID = Shared.EntityID.MockerID();
+         var date = Shared.Faker.GetFaker().Date.Soon();
+         var value = Shared.Faker.GetFaker().Random.Decimal(0, 10000);
 
          var result = TransferEntity.Restore(transferID, expenseAccountID, incomeAccountID, date, value);
 
@@ -38,13 +38,13 @@ namespace Elesse.Transfers.Tests
       public static IEnumerable<object[]> Restore_WithInvalidParameters_MustThrowException_Data() =>
          new[] {
             new object[] { WARNINGS.INVALID_TRANSFERID, null, null, null, null, null},
-            new object[] { WARNINGS.INVALID_EXPENSEACCOUNTID, Shared.EntityID.NewID(), null, null, null, null},
-            new object[] { WARNINGS.INVALID_INCOMEACCOUNTID, Shared.EntityID.NewID(), Shared.EntityID.NewID(), null, null, null},
-            new object[] { WARNINGS.INVALID_DATE, Shared.EntityID.NewID(), Shared.EntityID.NewID(), Shared.EntityID.NewID(), null, null},
-            new object[] { WARNINGS.INVALID_DATE, Shared.EntityID.NewID(), Shared.EntityID.NewID(), Shared.EntityID.NewID(), DateTime.MinValue, null},
-            new object[] { WARNINGS.INVALID_VALUE, Shared.EntityID.NewID(), Shared.EntityID.NewID(), Shared.EntityID.NewID(), DateTime.UtcNow, null},
-            new object[] { WARNINGS.INVALID_VALUE, Shared.EntityID.NewID(), Shared.EntityID.NewID(), Shared.EntityID.NewID(), DateTime.UtcNow, 0},
-            new object[] { WARNINGS.INVALID_VALUE, Shared.EntityID.NewID(), Shared.EntityID.NewID(), Shared.EntityID.NewID(), DateTime.UtcNow, -0.01}
+            new object[] { WARNINGS.INVALID_EXPENSEACCOUNTID, Shared.EntityID.MockerID(), null, null, null, null},
+            new object[] { WARNINGS.INVALID_INCOMEACCOUNTID, Shared.EntityID.MockerID(), Shared.EntityID.MockerID(), null, null, null},
+            new object[] { WARNINGS.INVALID_DATE, Shared.EntityID.MockerID(), Shared.EntityID.MockerID(), Shared.EntityID.MockerID(), null, null},
+            new object[] { WARNINGS.INVALID_DATE, Shared.EntityID.MockerID(), Shared.EntityID.MockerID(), Shared.EntityID.MockerID(), DateTime.MinValue, null},
+            new object[] { WARNINGS.INVALID_VALUE, Shared.EntityID.MockerID(), Shared.EntityID.MockerID(), Shared.EntityID.MockerID(), Shared.Faker.GetFaker().Date.Soon(), null},
+            new object[] { WARNINGS.INVALID_VALUE, Shared.EntityID.MockerID(), Shared.EntityID.MockerID(), Shared.EntityID.MockerID(), Shared.Faker.GetFaker().Date.Soon(), 0},
+            new object[] { WARNINGS.INVALID_VALUE, Shared.EntityID.MockerID(), Shared.EntityID.MockerID(), Shared.EntityID.MockerID(), Shared.Faker.GetFaker().Date.Soon(), Shared.Faker.GetFaker().Random.Decimal(decimal.MinValue, (decimal)(0-0.01))}
          };
 
    }
