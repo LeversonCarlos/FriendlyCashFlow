588fc91f83e0ce2fd10efd15fe996e516f0caf85
Leverson Carlos <lcjohnny@hotmail.com>
2021-02-10 21:27:02 -0300

    implement the list function

---
+++ sources/backend/transfers/Services/List/Service.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Elesse.Transfers
+{
+
+   partial class TransferService
+   {
+
+      public async Task<ActionResult<ITransferEntity[]>> ListAsync(int year, int month)
+      {
+
+         var transfersList = await _TransferRepository.ListAsync(year, month);
+
+         return Ok(transfersList);
+      }
+
+   }
+
+   partial interface ITransferService
+   {
+      Task<ActionResult<ITransferEntity[]>> ListAsync(int year, int month);
+   }
+
+}
