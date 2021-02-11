15cce26778fe037f1eeef944c62da2139eea7438
Leverson Carlos <lcjohnny@hotmail.com>
2021-02-11 16:59:47 -0300

    insert function for the transfers service

---
+++ sources/backend/transfers/Services/Insert/Service.Tests.cs
@@ -0,0 +1,100 @@
+using System;
+using Xunit;
+
+namespace Elesse.Transfers.Tests
+{
+   partial class TransferServiceTests
+   {
+
+      [Fact]
+      public async void Insert_WithNullParameter_MustReturnBadResult()
+      {
+         var service = TransferService.Builder().Build();
+
+         var result = await service.InsertAsync(null);
+
+         Assert.NotNull(result);
+         Assert.IsType<Microsoft.AspNetCore.Mvc.BadRequestObjectResult>(result);
+         Assert.Equal(Warning(WARNINGS.INVALID_INSERT_PARAMETER), (result as Microsoft.AspNetCore.Mvc.BadRequestObjectResult).Value);
+      }
+
+      /*
+      [Fact]
+      public async void Insert_WithInvalidPattern_MustReturnBadResult()
+      {
+         var patternService = Patterns.Tests.PatternServiceMocker
+            .Create()
+            .WithIncrease(new ArgumentException(Patterns.WARNINGS.INVALID_INCREASE_PARAMETER))
+            .Build();
+         var service = EntryService.Mock(patternService);
+
+         var result = await service.InsertAsync(new InsertVM { });
+
+         Assert.NotNull(result);
+         Assert.IsType<Microsoft.AspNetCore.Mvc.BadRequestObjectResult>(result);
+         Assert.Equal(Warning(Patterns.WARNINGS.INVALID_INCREASE_PARAMETER), (result as Microsoft.AspNetCore.Mvc.BadRequestObjectResult).Value);
+      }
+
+      [Fact]
+      public async void Insert_WithInvaidAccount_MustReturnBadResult()
+      {
+         var pattern = Patterns.PatternEntity.Builder().Build();
+         var patternService = Patterns.Tests.PatternServiceMocker
+            .Create()
+            .WithIncrease(pattern)
+            .Build();
+         var service = EntryService.Mock(patternService);
+
+         var param = new InsertVM { Pattern = pattern, AccountID = null };
+         var result = await service.InsertAsync(param);
+
+         Assert.NotNull(result);
+         Assert.IsType<Microsoft.AspNetCore.Mvc.BadRequestObjectResult>(result);
+         Assert.Equal(Warning(WARNINGS.INVALID_ACCOUNTID), (result as Microsoft.AspNetCore.Mvc.BadRequestObjectResult).Value);
+      }
+
+      [Fact]
+      public async void Insert_WithValidParametersAndWithoutPayment_MustReturnOkResult()
+      {
+         var pattern = Patterns.PatternEntity.Builder().Build();
+         var patternService = Patterns.Tests.PatternServiceMocker
+            .Create()
+            .WithIncrease(pattern)
+            .Build();
+         var service = EntryService.Mock(patternService);
+
+         var param = new InsertVM { Pattern = pattern, AccountID = Shared.EntityID.NewID(), DueDate = DateTime.Now.AddDays(1), EntryValue = (decimal)12.34 };
+         var result = await service.InsertAsync(param);
+
+         Assert.NotNull(result);
+         Assert.IsType<Microsoft.AspNetCore.Mvc.OkResult>(result);
+      }
+
+      [Fact]
+      public async void Insert_WithValidParametersAndWithPayment_MustReturnOkResult()
+      {
+         var pattern = Patterns.PatternEntity.Builder().Build();
+         var patternService = Patterns.Tests.PatternServiceMocker
+            .Create()
+            .WithIncrease(pattern)
+            .Build();
+         var service = EntryService.Mock(patternService);
+
+         var param = new InsertVM
+         {
+            Pattern = pattern,
+            AccountID = Shared.EntityID.NewID(),
+            DueDate = DateTime.Now.AddDays(1),
+            EntryValue = (decimal)12.34,
+            Paid = true,
+            PayDate = DateTime.Now
+         };
+         var result = await service.InsertAsync(param);
+
+         Assert.NotNull(result);
+         Assert.IsType<Microsoft.AspNetCore.Mvc.OkResult>(result);
+      }
+      */
+
+   }
+}
---
+++ sources/backend/transfers/Services/Insert/Service.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Elesse.Transfers
+{
+
+   partial class TransferService
+   {
+
+      public async Task<IActionResult> InsertAsync(InsertVM insertVM)
+      {
+         try
+         {
+
+            // VALIDATE PARAMETERS
+            if (insertVM == null)
+               return Warning(WARNINGS.INVALID_INSERT_PARAMETER);
+
+            // CREATE INSTANCE
+            TransferEntity transfer = null;
+            try { transfer = TransferEntity.Create(insertVM.ExpenseAccountID, insertVM.IncomeAccountID, insertVM.Date, insertVM.Value); }
+            catch (Exception valEx) { return Warning(valEx.Message); }
+
+            // SAVE ENTITY
+            await _TransferRepository.InsertAsync(transfer);
+
+            // TRACK EVENT
+            _InsightsService.TrackEvent("Transfer Service Insert");
+
+            // RESULT
+            return Ok();
+         }
+         catch (Exception ex) { return Shared.Results.Exception(ex); }
+      }
+
+   }
+
+   partial interface ITransferService
+   {
+      Task<IActionResult> InsertAsync(InsertVM insertVM);
+   }
+
+   partial struct WARNINGS
+   {
+      internal const string INVALID_INSERT_PARAMETER = "INVALID_INSERT_PARAMETER";
+   }
+
+}
