caaf7f68831243b0574fec87c04b003b33f63c90
Leverson Carlos <lcjohnny@hotmail.com>
2021-02-10 21:40:07 -0300

    implement the load function

---
+++ sources/backend/transfers/Services/Load/Service.cs
@@ -0,0 +1,33 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Elesse.Transfers
+{
+
+   partial class TransferService
+   {
+
+      public async Task<ActionResult<ITransferEntity>> LoadAsync(string id)
+      {
+
+         if (!Shared.EntityID.TryParse(id, out var transferID))
+            return Warning(WARNINGS.INVALID_LOAD_PARAMETER);
+
+         var transfer = await _TransferRepository.LoadAsync(transferID);
+
+         return Ok(transfer);
+      }
+
+   }
+
+   partial interface ITransferService
+   {
+      Task<ActionResult<ITransferEntity>> LoadAsync(string id);
+   }
+
+   partial struct WARNINGS
+   {
+      internal const string INVALID_LOAD_PARAMETER = "INVALID_LOAD_PARAMETER";
+   }
+
+}
