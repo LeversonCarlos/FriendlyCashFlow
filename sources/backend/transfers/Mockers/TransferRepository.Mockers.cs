77d488ef8a2f9302f5a81c3a9dad17af0cca3d2a
Leverson Carlos <lcjohnny@hotmail.com>
2021-02-11 17:29:19 -0300

    test an repository exception

--- sources/backend/transfers/Mockers/TransferRepository.Mockers.cs
+++ sources/backend/transfers/Mockers/TransferRepository.Mockers.cs
@@ -1,3 +1,4 @@
+using System;
 using Moq;
 
 namespace Elesse.Transfers.Tests
@@ -30,6 +31,14 @@ namespace Elesse.Transfers.Tests
             _Mock.InSequence(seq)
                .Setup(m => m.LoadAsync(It.IsAny<Shared.EntityID>()))
                .ReturnsAsync(result);
+         return this;
+      }
+
+      public TransferRepositoryMocker WithInsert(Exception ex)
+      {
+         _Mock
+            .Setup(m => m.InsertAsync(It.IsAny<ITransferEntity>()))
+            .ThrowsAsync(ex);
          return this;
       }
 
--- sources/backend/transfers/Services/Insert/Service.Tests.cs
+++ sources/backend/transfers/Services/Insert/Service.Tests.cs
@@ -32,6 +32,30 @@ namespace Elesse.Transfers.Tests
       }
 
       [Fact]
+      public async void Insert_WithExceptionOnRepository_MustReturnBadResult()
+      {
+         var repositoryException = new Exception(Shared.Faker.GetFaker().Lorem.Sentence(5));
+         var repository = TransferRepositoryMocker
+            .Create()
+            .WithInsert(repositoryException)
+            .Build();
+         var service = TransferService.Builder().With(repository).Build();
+
+         var param = new InsertVM
+         {
+            ExpenseAccountID = Shared.EntityID.MockerID(),
+            IncomeAccountID = Shared.EntityID.MockerID(),
+            Date = Shared.Faker.GetFaker().Date.Soon(),
+            Value = Shared.Faker.GetFaker().Random.Decimal(0, 10000)
+         };
+         var result = await service.InsertAsync(param);
+
+         Assert.NotNull(result);
+         Assert.IsType<Microsoft.AspNetCore.Mvc.BadRequestObjectResult>(result);
+         Assert.Equal(new Shared.Results(repositoryException), (result as Microsoft.AspNetCore.Mvc.BadRequestObjectResult).Value);
+      }
+
+      [Fact]
       public async void Insert_WithValidParameters_MustReturnOkResult()
       {
          var service = TransferService.Builder().Build();
