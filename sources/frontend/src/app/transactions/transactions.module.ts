70ae14d83af586aa9d68b82cb3e9d4108b59e307
Leverson Carlos <lcjohnny@hotmail.com>
2021-02-14 11:24:39 -0300

    add material and shared module

--- sources/frontend/src/app/transactions/transactions.module.ts
+++ sources/frontend/src/app/transactions/transactions.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../material/material.exports';
+import { SharedModule } from '../shared/shared.exports';
 import { TransactionsRouting } from './transactions.routing';
 import { ListComponent } from './list/list.component';
 
 @NgModule({
    declarations: [ListComponent],
    imports: [
-      CommonModule,
+      MaterialModule, SharedModule,
       TransactionsRouting
    ]
 })
