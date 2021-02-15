0fc3e145c2ada3eb8bd8c2ec830e180ae5a750e5
Leverson Carlos <lcjohnny@hotmail.com>
2021-02-15 14:19:32 -0300

    evolve the days component

--- sources/frontend/src/app/transactions/list/days/days.component.ts
+++ sources/frontend/src/app/transactions/list/days/days.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ResponsiveService } from '@elesse/shared';
+import { TransactionDay } from '../../model/transactions.model';
 
 @Component({
-   selector: 'elesse-days',
+   selector: 'transactions-days',
    templateUrl: './days.component.html',
    styleUrls: ['./days.component.scss']
 })
 export class DaysComponent implements OnInit {
 
-   constructor() { }
+   constructor(private responsive: ResponsiveService) { }
 
    ngOnInit(): void {
    }
+
+   @Input() Days: TransactionDay[]
+   public get IsMobile(): boolean { return this.responsive && this.responsive.IsMobile; }
 
 }
