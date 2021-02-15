d944ec119404c0e38abe8226ded7df5fe694d955
Leverson Carlos <lcjohnny@hotmail.com>
2021-02-15 15:16:09 -0300

    evolve the transactions list

--- sources/frontend/src/app/transactions/list/item/item.component.ts
+++ sources/frontend/src/app/transactions/list/item/item.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { TransactionBase } from '../../model/transactions.model';
 
 @Component({
-   selector: 'elesse-item',
+   selector: 'transactions-item',
    templateUrl: './item.component.html',
    styleUrls: ['./item.component.scss']
 })
@@ -11,5 +12,7 @@ export class ItemComponent implements OnInit {
 
    ngOnInit(): void {
    }
+
+   @Input() Transaction: TransactionBase
 
 }
