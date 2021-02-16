74d7bb61db439fec92527b5d9067f3c08de8d65e
Leverson Carlos <lcjohnny@hotmail.com>
2021-02-16 10:55:16 -0300

    cretae new component to the details

--- sources/frontend/src/app/transfers/transfers.module.ts
+++ sources/frontend/src/app/transfers/transfers.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { MaterialModule } from '../material/material.exports';
 import { SharedModule } from '../shared/shared.exports';
 import { TransfersRouting } from './transfers.routing';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
-   declarations: [],
+   declarations: [DetailsComponent],
    imports: [
       MaterialModule, SharedModule,
       TransfersRouting
