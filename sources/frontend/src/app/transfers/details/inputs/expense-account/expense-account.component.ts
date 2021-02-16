033a368f9fc602451956c8f29da3a256467204a9
Leverson Carlos <lcjohnny@hotmail.com>
2021-02-16 19:17:22 -0300

    review expense account component

--- sources/frontend/src/app/transfers/details/inputs/expense-account/expense-account.component.ts
+++ sources/frontend/src/app/transfers/details/inputs/expense-account/expense-account.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AccountEntity, AccountsData } from '@elesse/accounts';
+import { RelatedData } from '@elesse/shared';
 import { TransferEntity } from 'src/app/transfers/model/transfers.model';
 
 @Component({
@@ -9,10 +11,12 @@ import { TransferEntity } from 'src/app/transfers/model/transfers.model';
 })
 export class ExpenseAccountComponent implements OnInit {
 
-   constructor() { }
+   constructor(private accountsData: AccountsData) { }
 
    @Input() data: TransferEntity;
    @Input() form: FormGroup;
+   public AccountOptions: RelatedData<AccountEntity>[] = [];
+   public AccountFiltered: RelatedData<AccountEntity>[] = [];
 
    ngOnInit(): void {
       this.OnDataInit();
@@ -22,15 +26,34 @@ export class ExpenseAccountComponent implements OnInit {
    private OnDataInit() {
       if (!this.data)
          return;
+      this.AccountOptions = this.accountsData.GetAccounts(true)
+         .map(entity => Object.assign(new RelatedData, {
+            id: entity.AccountID,
+            description: entity.Text,
+            value: entity
+         }));
+      if (this.data.ExpenseAccountID)
+         this.AccountFiltered = this.AccountOptions
+            .filter(entity => entity.value.AccountID == this.data.ExpenseAccountID)
    }
 
    private OnFormInit() {
       if (!this.form)
          return;
-      this.form.addControl("ExpenseAccountID", new FormControl(this.data.ExpenseAccountID ?? null, [Validators.required]));
-      this.form.get("ExpenseAccountID").valueChanges.subscribe((val: any) => {
-         this.data.ExpenseAccountID = val;
+      this.form.addControl("ExpenseAccountID", new FormControl(this.data.ExpenseAccountID ?? null));
+      this.form.addControl("ExpenseAccountRow", new FormControl(this.GetFirstAccount(), Validators.required));
+      this.form.get("ExpenseAccountRow").valueChanges.subscribe((row: RelatedData<AccountEntity>) => {
+         this.data.ExpenseAccountID = row?.value?.AccountID ?? null;
       });
    }
+
+   public OnAccountChanging(val: string) {
+      this.AccountFiltered = this.AccountOptions
+         .filter(entity => entity.value.Text.search(new RegExp(val, 'i')) != -1)
+         .sort((a, b) => a.description > b.description ? 1 : -1)
+   }
+
+   private GetFirstAccount = (): RelatedData<AccountEntity> =>
+      this.AccountFiltered?.length == 1 ? this.AccountFiltered[0] : null;
 
 }
