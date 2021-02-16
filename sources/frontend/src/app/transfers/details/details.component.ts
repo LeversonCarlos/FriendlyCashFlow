74556b6b7be7636780fc3a4ff1315f644a8b0005
Leverson Carlos <lcjohnny@hotmail.com>
2021-02-16 14:11:46 -0300

    initialize the details component

--- sources/frontend/src/app/transfers/details/details.component.ts
+++ sources/frontend/src/app/transfers/details/details.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BusyService, MessageService } from '@elesse/shared';
+import { TransfersData } from '../data/transfers.data';
+import { TransferEntity } from '../model/transfers.model';
 
 @Component({
    selector: 'transfers-details',
@@ -7,9 +12,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-   constructor() { }
+   constructor(private transferData: TransfersData,
+      private busy: BusyService, private msg: MessageService,
+      private activatedRoute: ActivatedRoute, private router: Router, private fb: FormBuilder) { }
 
-   ngOnInit(): void {
+   public async ngOnInit(): Promise<void> {
+      const paramID = this.activatedRoute.snapshot.params.id;
+
+      this.data = await this.transferData.LoadTransfer(paramID);
+      if (!this.data) {
+         this.router.navigate(["/transactions/list"]);
+         return;
+      }
+
+      this.form = this.fb.group({});
+   }
+
+   public data: TransferEntity;
+   public form: FormGroup;
+   public get IsBusy(): boolean { return this.busy.IsBusy; }
+
+   public async OnCancelClick() {
+      if (!this.form.pristine)
+         if (!await this.msg.Confirm('shared.ROLLBACK_TEXT', 'shared.ROLLBACK_CONFIRM_COMMAND', 'shared.ROLLBACK_ABORT_COMMAND'))
+            return;
+      this.router.navigate(["/transactions/list"])
+   }
+
+   public async OnSaveClick() {
+      if (!this.form.valid)
+         return;
+      if (!await this.transferData.SaveTransfer(this.data))
+         return;
+      this.router.navigate(["/transactions/list"])
    }
 
 }
