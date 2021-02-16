9899060e974f33fd9f37cc2e9ab66b675cf1ae5b
Leverson Carlos <lcjohnny@hotmail.com>
2021-02-16 18:45:35 -0300

    new component for the value input

--- sources/frontend/src/app/transfers/details/inputs/value/value.component.ts
+++ sources/frontend/src/app/transfers/details/inputs/value/value.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { TransferEntity } from 'src/app/transfers/model/transfers.model';
 
 @Component({
-   selector: 'elesse-value',
+   selector: 'transfers-details-inputs-value',
    templateUrl: './value.component.html',
    styleUrls: ['./value.component.scss']
 })
@@ -9,7 +11,26 @@ export class ValueComponent implements OnInit {
 
    constructor() { }
 
+   @Input() data: TransferEntity;
+   @Input() form: FormGroup;
+
    ngOnInit(): void {
+      this.OnDataInit();
+      this.OnFormInit();
+   }
+
+   private OnDataInit() {
+      if (!this.data)
+         return;
+   }
+
+   private OnFormInit() {
+      if (!this.form)
+         return;
+      this.form.addControl("Value", new FormControl(this.data.Value ?? null, [Validators.required, Validators.min(0.01)]));
+      this.form.get("Value").valueChanges.subscribe((val: any) => {
+         this.data.Value = val;
+      });
    }
 
 }
