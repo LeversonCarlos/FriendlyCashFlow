0203ebfb8974b53cb713424e6d3cdd78e16ed5e1
Leverson Carlos <lcjohnny@hotmail.com>
2021-02-16 18:55:40 -0300

    implement the date component

--- sources/frontend/src/app/transfers/details/inputs/date/date.component.ts
+++ sources/frontend/src/app/transfers/details/inputs/date/date.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { TransferEntity } from 'src/app/transfers/model/transfers.model';
 
 @Component({
-   selector: 'elesse-date',
+   selector: 'transfers-details-inputs-date',
    templateUrl: './date.component.html',
    styleUrls: ['./date.component.scss']
 })
@@ -9,7 +11,28 @@ export class DateComponent implements OnInit {
 
    constructor() { }
 
+   @Input() data: TransferEntity;
+   @Input() form: FormGroup;
+
    ngOnInit(): void {
+      this.OnDataInit();
+      this.OnFormInit();
+   }
+
+   private OnDataInit() {
+      if (!this.data)
+         return;
+   }
+
+   private OnFormInit() {
+      if (!this.form)
+         return;
+      this.form.addControl("Date", new FormControl(this.data.Date ?? null, Validators.required));
+      this.form.get("Date").valueChanges.subscribe((val: string) => {
+         let date = new Date(val);
+         date = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 12);
+         this.data.Date = date;
+      });
    }
 
 }
