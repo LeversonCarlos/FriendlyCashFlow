72e812f3f81bb29091ca1b7dfdaa29c42c722315
Leverson Carlos <lcjohnny@hotmail.com>
2021-02-18 17:59:03 -0300

    Remove unnecessary balance properties from entries classes

--- sources/frontend/src/app/entries/cache/cache.service.ts
+++ sources/frontend/src/app/entries/cache/cache.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { StorageService } from '@elesse/shared';
 import { BehaviorSubject, Observable, Subscription } from 'rxjs';
-import { BalanceItemEntity, EntryEntity } from '../model/entries.model';
+import { EntryEntity } from '../model/entries.model';
 
 @Injectable({
    providedIn: 'root'
@@ -31,50 +31,8 @@ export class EntriesCache extends StorageService<string, EntryEntity[]> {
    }
 
    public SetEntries(key: string, value: EntryEntity[]) {
-
-      // SORTER FUNCTION
-      const sorter = (a: EntryEntity, b: EntryEntity): number => {
-         let result = 0;
-         if (a.Sorting > b.Sorting) result += 1;
-         if (a.Sorting < b.Sorting) result -= 1;
-         return result;
-      }
-
-      // SORT AND PARSE OBJECTS
       const entries = value
-         .map(entry => EntryEntity.Parse(entry))
-         .sort(sorter);
-
-      // INITIALIZE BALANCE
-      const totalBalance = new BalanceItemEntity();
-      const accountsBalance: Record<string, BalanceItemEntity> = {};
-
-      // LOOP THROUGH ENTRIES
-      for (let index = 0; index < entries.length; index++) {
-         const entry = entries[index];
-
-         // CURRENT ENTRY VALUES
-         const expectedValue = entry.EntryValue;
-         const realizedValue = entry.Paid ? entry.EntryValue : 0;
-
-         // ACCUMULATE TOTAL BALANCE
-         totalBalance.ExpectedBalance += expectedValue;
-         totalBalance.RealizedBalance += realizedValue;
-         entry.Balance.Total.ExpectedBalance = totalBalance.ExpectedBalance;
-         entry.Balance.Total.RealizedBalance = totalBalance.RealizedBalance;
-
-         // ADD CURRENT ACCOUNT TO DICTIONARY IF HASNT YET
-         if (accountsBalance[entry.AccountID] == null)
-            accountsBalance[entry.AccountID] = new BalanceItemEntity();
-
-         // ACCUMULATE CURRENT ACCOUNT BALANCE
-         const accountBalance = accountsBalance[entry.AccountID];
-         accountBalance.ExpectedBalance += expectedValue;
-         accountBalance.RealizedBalance += realizedValue;
-         entry.Balance.Account.ExpectedBalance = accountBalance.ExpectedBalance;
-         entry.Balance.Account.RealizedBalance = accountBalance.RealizedBalance;
-      }
-
+         .map(entry => EntryEntity.Parse(entry));
       this.SetValue(key, entries);
    }
 
--- sources/frontend/src/app/entries/model/entries.model.ts
+++ sources/frontend/src/app/entries/model/entries.model.ts
@@ -1,23 +1,4 @@
-import { AccountEntity } from "@elesse/accounts";
 import { PatternEntity } from "@elesse/patterns";
-
-export class AccountEntries {
-   Account: AccountEntity;
-   Balance: BalanceItemEntity;
-   Days: DayEntries[] = []
-}
-
-export class DayEntries {
-   Day: Date;
-   Balance: BalanceItemEntity
-   Entries: EntryEntity[] = []
-}
-
-export class EntryGroupEntity {
-   Day: Date;
-   Entries: EntryEntity[] = []
-   Balance: BalanceEntity
-}
 
 export class EntryEntity {
    EntryID: string;
@@ -30,7 +11,6 @@ export class EntryEntity {
    Paid: boolean;
    PayDate?: Date;
 
-   Balance: BalanceEntity = new BalanceEntity();
    Sorting: number;
    SearchDate: Date
 
@@ -38,14 +18,4 @@ export class EntryEntity {
       return Object.assign(new EntryEntity, value);
    }
 
-}
-
-export class BalanceEntity {
-   Account: BalanceItemEntity = new BalanceItemEntity();
-   Total: BalanceItemEntity = new BalanceItemEntity();
-}
-
-export class BalanceItemEntity {
-   ExpectedBalance: number = 0.00;
-   RealizedBalance: number = 0.00;
 }
