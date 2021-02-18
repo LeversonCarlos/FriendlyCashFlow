39c502f6c2bd0d49a8cd05ba4959893d393b7843
Leverson Carlos <lcjohnny@hotmail.com>
2021-02-18 15:50:13 -0300

    extends storage service

--- sources/frontend/src/app/transfers/cache/cache.service.ts
+++ sources/frontend/src/app/transfers/cache/cache.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
+import { StorageService } from '@elesse/shared';
+import { TransferEntity } from '../model/transfers.model';
 
 @Injectable({
    providedIn: 'root'
 })
-export class CacheService {
+export class CacheService extends StorageService<string, TransferEntity[]> {
 
-   constructor() { }
+   constructor() {
+      super("TransfersCache");
+   }
 
 }
