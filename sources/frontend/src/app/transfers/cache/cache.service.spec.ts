7c5c56dc39d270978f4a57243a841ed605e9e7a8
Leverson Carlos <lcjohnny@hotmail.com>
2021-02-19 17:01:12 -0300

    write test for the setTransfers function to validate its signature

--- sources/frontend/src/app/transfers/cache/cache.service.spec.ts
+++ sources/frontend/src/app/transfers/cache/cache.service.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { TestsModule } from '@elesse/tests';
+import { TransferEntity } from '../model/transfers.model';
 import { TransfersCache } from './cache.service';
 
 describe('TransfersCache', () => {
@@ -14,6 +15,27 @@ describe('TransfersCache', () => {
 
    it('should be created', () => {
       expect(service).toBeTruthy();
+   });
+
+   it('SetTransfers parses and raises observable with list', (done) => {
+      const entries: TransferEntity[] = [
+         TransferEntity.Parse({ ExpenseAccountID: "account1", IncomeAccountID: "account2", Value: 1000, Sorting: 10 }),
+         TransferEntity.Parse({ ExpenseAccountID: "account1", IncomeAccountID: "account2", Value: -100, Sorting: 30 }),
+         TransferEntity.Parse({ ExpenseAccountID: "account1", IncomeAccountID: "account2", Value: -200.50, Sorting: 50 }),
+         TransferEntity.Parse({ ExpenseAccountID: "account2", IncomeAccountID: "account3", Value: 0.50, Sorting: 40 }),
+         TransferEntity.Parse({ ExpenseAccountID: "account3", IncomeAccountID: "account1", Value: -888, Sorting: 70 })
+      ];
+      const month = "201001";
+
+      service.InitializeValue(month);
+      service.GetObservable(month).subscribe(resultTransfers => {
+         if (resultTransfers != null) {
+            expect(resultTransfers.length).toBe(5)
+            done();
+         }
+      });
+
+      service.SetTransfers(month, entries)
    });
 
 });
