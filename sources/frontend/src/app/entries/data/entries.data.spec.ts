fc0994b93edf14c793abb78ba60adb68bb29d838
Leverson Carlos <lcjohnny@hotmail.com>
2021-02-19 17:15:54 -0300

    start writing tests for the load entry function

--- sources/frontend/src/app/entries/data/entries.data.spec.ts
+++ sources/frontend/src/app/entries/data/entries.data.spec.ts
@@ -1,19 +1,36 @@
+import { HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { ActivatedRouteSnapshot, Params, Route } from '@angular/router';
 import { TestsModule } from '@elesse/tests';
 import { EntriesData } from './entries.data';
 
 describe('EntriesData', () => {
    let service: EntriesData;
+   let httpMock: HttpTestingController;
 
    beforeEach(() => {
       TestBed.configureTestingModule({
          imports: [TestsModule]
       });
       service = TestBed.inject(EntriesData);
+      httpMock = TestBed.inject(HttpTestingController);
    });
 
    it('should be created', () => {
       expect(service).toBeTruthy();
    });
 
+   it('LoadEntry with null snapshot parameter must result null', async () => {
+      const result = await service.LoadEntry(null);
+      expect(result).toBeNull();
+   });
+
 });
+
+function getSnapshot(path: string = null, params: Params = null): ActivatedRouteSnapshot {
+   let routeConfig: Route = null;
+   if (path != null)
+      routeConfig = { path: path }
+   const snapshot: ActivatedRouteSnapshot = { url: null, routeConfig: routeConfig, params: params, queryParams: null, fragment: null, data: null, outlet: null, component: null, root: null, parent: null, firstChild: null, children: null, pathFromRoot: null, paramMap: null, queryParamMap: null };
+   return snapshot;
+}
