1165ffbff5d9ba9c7fa2d16db92397d4d62e7a53
Leverson Carlos <lcjohnny@hotmail.com>
2021-02-21 10:15:10 -0300

    asks for confirmation before calling the remove function for each transaction type

--- sources/frontend/src/app/transactions/list/remove/remove.component.ts
+++ sources/frontend/src/app/transactions/list/remove/remove.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { EntriesData } from '@elesse/entries';
+import { LocalizationService, MessageService } from '@elesse/shared';
 import { TransfersData } from '@elesse/transfers';
 import { TransactionBase, TransactionEntry, TransactionTransfer } from '../../model/transactions.model';
 
@@ -10,19 +11,29 @@ import { TransactionBase, TransactionEntry, TransactionTransfer } from '../../mo
 })
 export class RemoveComponent implements OnInit {
 
-   constructor(private entriesData: EntriesData, private transfersData: TransfersData) { }
+   constructor(private entriesData: EntriesData, private transfersData: TransfersData,
+      private message: MessageService, private localizationService: LocalizationService) { }
 
    @Input() Transaction: TransactionBase
 
    ngOnInit(): void {
    }
 
-   public OnRemoveClick() {
+   public async OnRemoveClick() {
       if (!this.Transaction)
          return;
+
+      let confirmText = await this.localizationService.GetTranslation("transactions.REMOVE_CONFIRMATION_TEXT");
+      confirmText = confirmText.replace('{0}', this.Transaction.Text)
+
+      const confirm = await this.message.Confirm(confirmText, "shared.REMOVE_CONFIRM_COMMAND", "shared.REMOVE_CANCEL_COMMAND");
+      if (!confirm)
+         return
+
       if (this.Transaction instanceof TransactionEntry) {
          this.entriesData.RemoveEntry(this.Transaction.Entry);
       }
+
       if (this.Transaction instanceof TransactionTransfer) {
          this.transfersData.RemoveTransfer(this.Transaction.Transfer);
       }
