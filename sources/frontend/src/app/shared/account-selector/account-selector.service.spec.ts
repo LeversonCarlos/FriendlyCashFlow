2bfed28f8d46331a94223f74cac1d34ace963b98
Leverson Carlos <lcjohnny@hotmail.com>
2021-02-22 17:25:55 -0300

    write test to assert the correct implementation for the classInterface

--- sources/frontend/src/app/shared/account-selector/account-selector.service.spec.ts
+++ sources/frontend/src/app/shared/account-selector/account-selector.service.spec.ts
@@ -1,19 +1,28 @@
 import { TestBed } from '@angular/core/testing';
 import { TestsModule } from '@elesse/tests';
+import { IAccountSelectorService } from './account-selector.interface';
 import { AccountSelectorService } from './account-selector.service';
 
 describe('AccountSelectorService', () => {
-   let service: AccountSelectorService;
 
    beforeEach(() => {
       TestBed.configureTestingModule({
          imports: [TestsModule]
       });
-      service = TestBed.inject(AccountSelectorService);
+
    });
 
    it('should be created', () => {
+      const service = TestBed.inject(AccountSelectorService);
       expect(service).toBeTruthy();
+   });
+
+   it('Provider must result instance of AccountSelectorService', () => {
+      const s = TestBed.inject(IAccountSelectorService);
+      s.SetTab(1, '1')
+      expect(s.TabIndex).toEqual(1);
+      expect(s.AccountID).toEqual('1');
+      expect(s).toBeInstanceOf(AccountSelectorService);
    });
 
 });
