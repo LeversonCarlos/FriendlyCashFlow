8225c6212f2703bd90f8c53518177c75f1126ae2
Leverson Carlos <lcjohnny@hotmail.com>
2021-02-22 21:32:30 -0300

    initialize balance entity

---
+++ sources/backend/balances/Entities/IBalanceEntity.cs
@@ -0,0 +1,15 @@
+namespace Elesse.Balances
+{
+
+   public partial interface IBalanceEntity
+   {
+      Shared.EntityID BalanceID { get; }
+
+      Shared.EntityID AccountID { get; }
+      DateTime Date { get; }
+
+      decimal ExpectedValue { get; }
+      decimal RealizedValue { get; }
+   }
+
+}
