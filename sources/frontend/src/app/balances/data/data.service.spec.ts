c437b14f9fb67656c97e2a4a4c04fd6ae410d9ed
Leverson Carlos <lcjohnny@hotmail.com>
2021-03-03 20:47:24 -0300

    bugfixes

--- sources/frontend/src/app/balances/data/data.service.spec.ts
+++ sources/frontend/src/app/balances/data/data.service.spec.ts
@@ -1,11 +1,14 @@
 import { TestBed } from '@angular/core/testing';
+import { TestsModule } from '@elesse/tests';
 import { BalanceData } from './data.service';
 
 describe('BalanceData', () => {
    let service: BalanceData;
 
    beforeEach(() => {
-      TestBed.configureTestingModule({});
+      TestBed.configureTestingModule({
+         imports: [TestsModule]
+      });
       service = TestBed.inject(BalanceData);
    });
 
