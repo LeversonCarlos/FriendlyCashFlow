1c22c0f4a67a1c8d36afe57910d203298040d13f
Leverson Carlos <lcjohnny@hotmail.com>
2021-03-03 20:15:17 -0300

    evolve the cache service

--- sources/frontend/src/app/balances/cache/cache.service.ts
+++ sources/frontend/src/app/balances/cache/cache.service.ts
@@ -1,10 +1,37 @@
 import { Injectable } from '@angular/core';
+import { StorageService } from '@elesse/shared';
+import { BehaviorSubject, Observable, Subscription } from 'rxjs';
+import { BalanceEntity } from '../models/balance.model';
 
 @Injectable({
    providedIn: 'root'
 })
-export class BalanceCache {
+export class BalanceCache extends StorageService<string, BalanceEntity[]> {
 
-   constructor() { }
+   constructor() {
+      super("BalanceCache");
+   }
+
+   private SubsKey: string;
+   private Subs: Subscription;
+   private Subject: BehaviorSubject<BalanceEntity[]>;
+   public Observe: Observable<BalanceEntity[]>;
+
+   public InitializeValue(key: string) {
+      super.InitializeValue(key);
+      if (this.Subs) {
+         if (this.SubsKey == key)
+            return;
+         this.Subs.unsubscribe();
+      }
+      this.Subs = this.GetObservable(key).subscribe(values => this.Subject.next(values));
+      this.SubsKey = key;
+   }
+
+   public SetBalances(key: string, value: BalanceEntity[]) {
+      const entries = value
+         .map(entry => BalanceEntity.Parse(entry));
+      this.SetValue(key, entries);
+   }
 
 }
