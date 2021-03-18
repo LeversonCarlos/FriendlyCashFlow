26107573ff83af5c0fb5c38cdbe3e186ecc21947
Leverson Carlos <lcjohnny@hotmail.com>
2021-03-18 19:50:55 -0300

    write test for the recurrence call

--- sources/backend/entries/Services/Insert/Service.Tests.cs
+++ sources/backend/entries/Services/Insert/Service.Tests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Elesse.Entries.Tests
@@ -98,17 +99,27 @@ namespace Elesse.Entries.Tests
       public async void Insert_WithValidParametersAndWithThreeRecurrences_MustInsertThreeEntriesAndReturnOkResult()
       {
          var faker = Shared.Faker.GetFaker();
+
          var pattern = Patterns.PatternEntity.Builder().Build();
          var patternService = Patterns.Tests.PatternServiceMocker
             .Create()
             .WithRetrieve(pattern)
             .WithIncrease(pattern)
             .Build();
+
          var recurrenceID = Shared.EntityID.MockerID();
          var recurrenceService = Recurrences.RecurrenceService.Mocker()
             .WithGetNewRecurrence(recurrenceID)
             .Build();
+
+         Moq.Mock<IEntryRepository> entryRepositoryMock = null;
+         var entryRepository = EntryRepositoryMocker.Create()
+            .WithInsert(Task.CompletedTask, Task.CompletedTask, Task.CompletedTask)
+            .With(mock => entryRepositoryMock = mock)
+            .Build(); ;
+
          var service = EntryService.Builder()
+            .With(entryRepository)
             .With(patternService)
             .With(recurrenceService)
             .Build();
@@ -124,6 +135,7 @@ namespace Elesse.Entries.Tests
          };
          var result = await service.InsertAsync(param);
 
+         entryRepositoryMock.Verify(m => m.InsertAsync(Moq.It.IsAny<EntryEntity>()), Moq.Times.Exactly(3));
          Assert.NotNull(result);
          Assert.IsType<Microsoft.AspNetCore.Mvc.OkResult>(result);
       }
