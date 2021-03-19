1a32546a498a5db4cb8027390f3e193a945ecb98
Leverson Carlos <lcjohnny@hotmail.com>
2021-03-19 20:11:51 -0300

    write tests for the get new recurrence function

--- sources/backend/recurrences/Services/GetNewRecurrence/Service.Tests.cs
+++ sources/backend/recurrences/Services/GetNewRecurrence/Service.Tests.cs
@@ -9,7 +9,7 @@ namespace Elesse.Recurrences.Tests
    {
 
       [Fact]
-      public async void GetNewRecurrenceAsync_WithInvalidParameter_MustThrowException()
+      public async void GetNewRecurrence_WithInvalidParameter_MustThrowException()
       {
          var service = RecurrenceService.Builder().Build();
 
@@ -19,57 +19,17 @@ namespace Elesse.Recurrences.Tests
          Assert.Equal(WARNINGS.INVALID_PROPERTIES, exception.Message);
       }
 
-      /*
-      [Theory]
-      [InlineData(false)]
-      [InlineData(true)]
-      public async void Increase_WithExistingBalance_MustUpdate_AndReturnChangedBalance(bool paid)
+      [Fact]
+      public async void GetNewRecurrence_WithValidParameters_MustReturnNewID()
       {
-         var param = BalanceEntity.Builder().Build();
-         var repository = BalanceRepository
-            .Mocker()
-            .WithLoad(new IBalanceEntity[] { param })
-            .Build();
-         var service = BalanceService.Builder().With(repository).Build();
+         var service = RecurrenceService.Builder().Build();
+         var properties = RecurrenceProperties.Builder().Build();
 
-         var value = Shared.Faker.GetFaker().Random.Decimal(-1000, 1000);
-         var expectedValue = param.ExpectedValue + value;
-         var realizedValue = param.RealizedValue + (paid ? value : 0);
-
-         var result = await service.IncreaseAsync(param.AccountID, param.Date, value, paid);
+         var result = await service.GetNewRecurrenceAsync(properties);
 
          Assert.NotNull(result);
-         Assert.IsAssignableFrom<IBalanceEntity>(result);
-         Assert.Equal(param.BalanceID, result.BalanceID);
-         Assert.Equal(expectedValue, result.ExpectedValue);
-         Assert.Equal(realizedValue, result.RealizedValue);
+         Assert.IsAssignableFrom<Shared.EntityID>(result);
       }
-
-      [Theory]
-      [InlineData(false)]
-      [InlineData(true)]
-      public async void Increase_WithNonExistingBalance_MustCreateRecord_AndReturnChangedBalance(bool paid)
-      {
-         var repository = BalanceRepository
-            .Mocker()
-            .WithLoad()
-            .Build();
-         var service = BalanceService.Builder().With(repository).Build();
-
-         var accountID = Shared.EntityID.MockerID();
-         var date = Shared.Faker.GetFaker().Date.Soon();
-         var value = Shared.Faker.GetFaker().Random.Decimal(-1000, 1000);
-         var expectedValue = value;
-         var realizedValue = (paid ? value : 0);
-
-         var result = await service.IncreaseAsync(accountID, date, value, paid);
-
-         Assert.NotNull(result);
-         Assert.IsAssignableFrom<IBalanceEntity>(result);
-         Assert.Equal(expectedValue, result.ExpectedValue);
-         Assert.Equal(realizedValue, result.RealizedValue);
-      }
-      */
 
    }
 }
