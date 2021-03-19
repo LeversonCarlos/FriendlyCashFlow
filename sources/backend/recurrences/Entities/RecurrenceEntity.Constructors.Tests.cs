5457b8dec494c403b7f7140048e25de72bbc2b10
Leverson Carlos <lcjohnny@hotmail.com>
2021-03-19 19:40:19 -0300

    write tests for the create recurrence

--- sources/backend/recurrences/Entities/RecurrenceEntity.Constructors.Tests.cs
+++ sources/backend/recurrences/Entities/RecurrenceEntity.Constructors.Tests.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using Xunit;
 
 namespace Elesse.Recurrences.Tests
@@ -7,48 +6,37 @@ namespace Elesse.Recurrences.Tests
    partial class RecurrenceEntityTests
    {
 
-      /*
+
       [Fact]
-      public void Restore_WithValidParameters_MustResultInstance()
+      public void Create_WithValidParameters_MustResultInstance()
       {
-         var recurrenceID = Shared.EntityID.MockerID();
          var patternID = Shared.EntityID.MockerID();
          var accountID = Shared.EntityID.MockerID();
          var date = Shared.Faker.GetFaker().Date.Soon();
          var value = Shared.Faker.GetFaker().Random.Decimal(0, 10000);
          var type = enRecurrenceType.Monthly;
+         var properties = RecurrenceProperties.Create(patternID, accountID, date, value, type);
 
-         var result = RecurrenceEntity.Restore(recurrenceID, patternID, accountID, date, value, type);
+         var result = RecurrenceEntity.Create(properties);
 
          Assert.NotNull(result);
-         Assert.Equal(recurrenceID, result.RecurrenceID);
-         Assert.Equal(patternID, result.PatternID);
-         Assert.Equal(accountID, result.AccountID);
-         Assert.Equal(date, result.Date);
-         Assert.Equal(value, result.Value);
-         Assert.Equal(type, result.Type);
+         Assert.Equal(patternID, result.Properties.PatternID);
+         Assert.Equal(accountID, result.Properties.AccountID);
+         Assert.Equal(date, result.Properties.Date);
+         Assert.Equal(value, result.Properties.Value);
+         Assert.Equal(type, result.Properties.Type);
       }
 
-      [Theory]
-      [MemberData(nameof(Restore_WithInvalidParameters_MustThrowException_Data))]
-      public void Restore_WithInvalidParameters_MustThrowException(string exceptionText, Shared.EntityID recurrenceID, Shared.EntityID patternID, Shared.EntityID accountID, DateTime date, decimal value)
+      [Fact]
+      public void Create_WithInvalidParameters_MustThrowException()
       {
-         var type = enRecurrenceType.Monthly;
-         var exception = Assert.Throws<ArgumentException>(() => RecurrenceEntity.Restore(recurrenceID, patternID, accountID, date, value, type));
+         RecurrenceProperties properties = null;
+
+         var exception = Assert.Throws<ArgumentException>(() => RecurrenceEntity.Create(properties));
 
          Assert.NotNull(exception);
-         Assert.Equal(exceptionText, exception.Message);
+         Assert.Equal(WARNINGS.INVALID_PROPERTIES, exception.Message);
       }
-      public static IEnumerable<object[]> Restore_WithInvalidParameters_MustThrowException_Data() =>
-         new[] {
-            new object[] { WARNINGS.INVALID_RECURRENCEID, null, null, null, null, null},
-            new object[] { WARNINGS.INVALID_PATTERNID, Shared.EntityID.MockerID(), null, null, null, null},
-            new object[] { WARNINGS.INVALID_ACCOUNTID, Shared.EntityID.MockerID(), Shared.EntityID.MockerID(), null, null, null},
-            new object[] { WARNINGS.INVALID_DATE, Shared.EntityID.MockerID(), Shared.EntityID.MockerID(), Shared.EntityID.MockerID(), null, null},
-            new object[] { WARNINGS.INVALID_DATE, Shared.EntityID.MockerID(), Shared.EntityID.MockerID(), Shared.EntityID.MockerID(), DateTime.MinValue, null},
-            new object[] { WARNINGS.INVALID_VALUE, Shared.EntityID.MockerID(), Shared.EntityID.MockerID(), Shared.EntityID.MockerID(), Shared.Faker.GetFaker().Date.Soon(), Shared.Faker.GetFaker().Random.Decimal(decimal.MinValue, 0)}
-         };
-      */
 
    }
 }
--- sources/backend/recurrences/Entities/RecurrenceEntity.Constructors.cs
+++ sources/backend/recurrences/Entities/RecurrenceEntity.Constructors.cs
@@ -6,22 +6,6 @@ namespace Elesse.Recurrences
    partial class RecurrenceEntity
    {
 
-      /*
-      public static RecurrenceEntity Restore(
-         Shared.EntityID recurrenceID, Shared.EntityID patternID,
-         Shared.EntityID accountID, DateTime date, decimal value,
-         enRecurrenceType type) =>
-         new RecurrenceEntity
-         {
-            RecurrenceID = recurrenceID,
-            PatternID = patternID,
-            AccountID = accountID,
-            Date = date,
-            Value = value,
-            Type = type
-         };
-      */
-
       public static RecurrenceEntity Create(IRecurrenceProperties properties) =>
          new RecurrenceEntity
          {
