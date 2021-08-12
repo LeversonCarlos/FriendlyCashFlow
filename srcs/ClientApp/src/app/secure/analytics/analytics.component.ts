174b427ec039a88a8c687ca087862d516392d3f0
Leverson Carlos <lcjohnny@hotmail.com>
2021-08-12 19:48:56 -0300

    create slider to alternate into print version and screen version making requests that uses the new parameter route

--- srcs/ClientApp/src/app/secure/analytics/analytics.component.ts
+++ srcs/ClientApp/src/app/secure/analytics/analytics.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AnalyticsService } from './analytics.service';
 import { MediaMatcher } from '@angular/cdk/layout';
@@ -15,12 +15,14 @@ export class AnalyticsComponent implements OnInit {
       this.mobileQuery = this.media.matchMedia('(max-width: 600px)');
    }
    public mobileQuery: MediaQueryList
+   @Input() IsPrinting: boolean
 
    public ngOnInit() {
       try {
 
          let year: number = Number(this.route.snapshot.params.year);
          let month: number = Number(this.route.snapshot.params.month);
+         this.IsPrinting = Number(this.route.snapshot.params.printing) == 1;
 
          // DEFAULT YEAR/MONTH
          if (
@@ -30,16 +32,19 @@ export class AnalyticsComponent implements OnInit {
             const date = new Date();
             year = date.getFullYear();
             month = date.getMonth() + 1;
-            this.service.showIndex(year, month);
+            this.service.showIndex(year, month, this.IsPrinting);
             return;
          }
 
          const currentMonth = new Date(`${year.toString().padStart(4, "20")}-${(month + 0).toString().padStart(2, "0")}-01 12:00:00`);
-         this.service.setFilter(currentMonth);
+         this.service.setFilter(currentMonth, this.IsPrinting);
 
       }
       catch (ex) { console.error(ex); }
    }
 
+   public OnPrintVersionChanged(isPrinting:boolean) {
+      this.service.setFilter(this.service.FilterData.Month, isPrinting);
+   }
 
 }
