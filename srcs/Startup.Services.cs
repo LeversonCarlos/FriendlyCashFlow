20e3510b208d90ddbf05170bf268d7a4d0ddc621
Leverson Carlos <lcjohnny@hotmail.com>
2021-10-19 21:10:07 -0300

    run the program with startup tasks

--- srcs/Startup.Services.cs
+++ srcs/Startup.Services.cs
@@ -12,6 +12,7 @@ namespace FriendlyCashFlow
 
          // DATA CONTEXT
          services.AddDbContext<API.Base.dbContext>(x => x.ConfigureSqlServer(this.AppSettings.ConnStr));
+         services.AddStartupTask<StartupMigrator>();
 
          // CONFIGURE INJECTION FOR HELPERS
          services.AddScoped<API.Users.UsersService>();
