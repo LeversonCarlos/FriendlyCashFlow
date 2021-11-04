2501f8b91d1196ebadff07827f2e58b53ba12e26
Leverson Carlos <lcjohnny@hotmail.com>
2021-11-03 21:05:56 -0300

    create new component for the applicatino yield chart

---
+++ srcs/ClientApp/src/app/secure/analytics/application-yield/application-yield.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+   selector: 'fs-application-yield',
+   templateUrl: './application-yield.component.html',
+   styleUrls: ['./application-yield.component.scss']
+})
+export class ApplicationYieldComponent implements OnInit {
+
+   constructor() { }
+
+   ngOnInit(): void {
+   }
+
+}
