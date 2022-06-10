b1ede615af7da2ba208795e85da5b781497618a2
Leverson Carlos <lcjohnny@hotmail.com>
2022-06-10 17:05:26 -0300

    initialize new project from the backend

---
+++ sources/Backend/Program.cs
@@ -0,0 +1,14 @@
+namespace Friendly.CashFlow;
+
+public class Program
+{
+   public static void Main(string[] args)
+   {
+      var builder = WebApplication.CreateBuilder(args);
+      var app = builder.Build();
+
+      app.MapGet("/", () => "Hello World!");
+
+      app.Run();
+   }
+}
