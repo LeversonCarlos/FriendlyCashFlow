9d568a49341758d160cb2d97bb435c5a3f33b564
Leverson Carlos <lcjohnny@hotmail.com>
2022-06-14 08:27:13 -0300

    define function to get record by id and merge into sinle file

--- sources/Shared/Repository/Accounts/Repository.SaveNew.cs
+++
@@ -1,8 +0,0 @@
-using Lewio.CashFlow.Domain.Accounts;
-
-namespace Lewio.CashFlow.Repository;
-
-partial interface IAccountRepository
-{
-   Task<IAccount> SaveNew(IAccount value);
-}
--- sources/Shared/Repository/Accounts/Repository.cs
+++ sources/Shared/Repository/Accounts/Repository.cs
@@ -1,7 +1,11 @@
+using Lewio.CashFlow.Domain.Accounts;
+
 namespace Lewio.CashFlow.Repository;
 
 public partial interface IAccountRepository : IRepository
 {
+   Task<IAccount> GetByID(Guid id);
+   Task<bool> SaveNew(IAccount value);
 }
 
 partial interface IMainRepository
