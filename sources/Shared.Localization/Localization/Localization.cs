519c88e5364192c5ad08c58a23af030122dc756f
Leverson Carlos <lcjohnny@hotmail.com>
2022-06-19 11:48:46 -0300

    implement function to retrieve translationted strings from embeded resource

--- sources/Shared.Localization/Localization/Localization.cs
+++ sources/Shared.Localization/Localization/Localization.cs
@@ -1,14 +1,28 @@
-namespace Lewio.CashFlow.Shared
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
+namespace Lewio.CashFlow.Shared;
+
+public class Localizator : ILocalization
 {
-   public class Localization : ILocalization
+
+   public Localizator(IServiceProvider serviceProvider)
    {
-
-      public Task<string> GetString(string value) =>
-         Task.FromResult(value);
-
-      public void Dispose()
-      {
-      }
-
+      _StringLocalizer = serviceProvider.GetService<IStringLocalizer<Lewio.CashFlow.Shared.Localization.Resources.Strings>>()!;
    }
+   readonly IStringLocalizer<Lewio.CashFlow.Shared.Localization.Resources.Strings> _StringLocalizer;
+
+   public async Task<string> GetString(string value)
+   {
+      try
+      {
+         await Task.CompletedTask;
+         return _StringLocalizer[value];
+      }
+      catch { return $"{value.ToUpper().Replace(" ", "_")}"; }
+   }
+
+   public void Dispose()
+   {
+   }
+
 }
