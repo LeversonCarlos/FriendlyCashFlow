e0cfe6da07417ac25e78bc9f367bbe3b4054e6c9
Leverson Carlos <lcjohnny@hotmail.com>
2022-09-07 11:02:07 -0300

    provide tests for the accounts search command going through the repository

---
+++ sources/Backend.UnitTests/Accounts/Search.cs
@@ -0,0 +1,61 @@
+using Lewio.CashFlow.Accounts;
+using Lewio.Shared;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Lewio.CashFlow.UnitTests;
+
+partial class AccountsTests
+{
+
+   [Theory]
+   [InlineData("Z", "")]
+   [InlineData("AAAA", "")]
+   [InlineData("AAA BBB", "ABC|AB")]
+   [InlineData("AAA", "ABC|AB|AC")]
+   [InlineData("A", "ABC|AB|AC")]
+   [InlineData("", "ABC|AB|AC|BC")]
+   public async void SearchCommand_FoundRows_BasedOn_ProvidedSearchTerms(string searchTerms, string expectedResult)
+   {
+      var serviceProvider = SearchCommand_FoundRows_BasedOn_ProvidedSearchTerms_GetServiceProvider();
+
+      var request = new SearchRequestModel
+      {
+         SearchTerms = searchTerms
+      };
+
+      var response = await serviceProvider
+         .GetRequiredService<SearchCommand>()
+         .HandleAsync(request);
+      response.EnsureValidResponse();
+
+      var accountIDs = response.Accounts
+         ?.Select(x => (string)x.AccountID)
+         ?.ToArray() ?? new string[] { };
+      var result = string.Join("|", accountIDs);
+
+      Assert.Equal(expectedResult, result);
+   }
+
+   private IServiceProvider SearchCommand_FoundRows_BasedOn_ProvidedSearchTerms_GetServiceProvider()
+   {
+      var serviceProvider = Mocks.Builder
+         .ServiceProvider()
+         .Build();
+
+      var loggedInUser = serviceProvider
+         .GetRequiredService<Users.LoggedInUser>();
+
+      var ctx = serviceProvider
+         .GetRequiredService<Common.DataContext>();
+
+      ctx.Accounts.Add(new AccountEntity { UserID = loggedInUser, AccountID = "ABC", Text = "AAA BBB CCC" });
+      ctx.Accounts.Add(new AccountEntity { UserID = loggedInUser, AccountID = "AB", Text = "AAA BBB" });
+      ctx.Accounts.Add(new AccountEntity { UserID = loggedInUser, AccountID = "AC", Text = "AAA CCC" });
+      ctx.Accounts.Add(new AccountEntity { UserID = loggedInUser, AccountID = "BC", Text = "BBB CCC" });
+
+      ctx.SaveChanges();
+
+      return serviceProvider;
+   }
+
+}
