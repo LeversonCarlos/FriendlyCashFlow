94214c83b142a5e81aedfb517dbb2e89360bbf0d
Leverson Carlos <lcjohnny@hotmail.com>
2022-09-07 08:36:04 -0300

    command to search accounts through repository and parse into expected result

--- sources/Backend/Commands/Accounts/Search/Command.SearchData.cs
+++ sources/Backend/Commands/Accounts/Search/Command.SearchData.cs
@@ -5,9 +5,20 @@ partial class SearchCommand
 
    private async Task<bool> SearchData()
    {
-      // TODO
-      await Task.CompletedTask;
-      return true;
+      try
+      {
+
+         var dataList = await _ServiceProvider
+            .GetRequiredService<IAccountRepository>()
+            .GetListBySearchTerms(_Request.SearchTerms);
+
+         _Response.Accounts = dataList
+            ?.Select(data => data.ToModel())
+            ?.ToArray();
+
+         return true;
+      }
+      catch (Exception ex) { return SetErrorAndReturn(ex); }
    }
 
 }
