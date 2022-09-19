7b413705b3dfa32b86a3d9ce5856bfa0d5d415c7
Leverson Carlos <lcjohnny@hotmail.com>
2022-09-19 19:58:54 -0300

    add the accounts module to the main module

--- sources/Frontend/src/app/app.routing.ts
+++ sources/Frontend/src/app/app.routing.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FrontendRoute as AccountRoute } from '@models/accounts';
 
-const routes: Routes = [];
+const routes: Routes = [
+   { path: AccountRoute, loadChildren: () => import('./modules/accounts/accounts.module').then(m => m.AccountsModule) },
+];
 
 @NgModule({
    imports: [RouterModule.forRoot(routes)],
