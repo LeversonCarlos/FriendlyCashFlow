9c6ed0f51c2b3cae4c6f110a7b8fe1310b5ac867
Leverson Carlos <lcjohnny@hotmail.com>
2022-10-01 12:12:22 -0300

    eveolve the header toolbar on layout component

--- sources/Frontend/src/app/components/components.module.ts
+++ sources/Frontend/src/app/components/components.module.ts
@@ -4,15 +4,23 @@ import { HttpClientModule } from '@angular/common/http';
 import { LayoutComponent } from './layout/layout.component';
 import { ApiClient } from './api-client';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 @NgModule({
    declarations: [
       LayoutComponent
    ],
    imports: [
       CommonModule, HttpClientModule,
+      MatToolbarModule,
+      MatButtonModule, MatIconModule,
    ],
    exports: [
       LayoutComponent, HttpClientModule,
+      MatButtonModule, MatIconModule,
    ],
    providers: [
       ApiClient
