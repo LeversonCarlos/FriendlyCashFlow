55048a021dfad24df612ca9f1249f2833b84d8a5
Leverson Carlos <lcjohnny@hotmail.com>
2022-10-01 11:12:12 -0300

    add material design

--- sources/Frontend/src/app/app.module.ts
+++ sources/Frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app.routing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,8 +10,8 @@ import { AppComponent } from './app.component';
       AppComponent
    ],
    imports: [
-      BrowserModule,
-      AppRoutingModule
+      BrowserModule, BrowserAnimationsModule,
+      AppRoutingModule,
    ],
    providers: [],
    bootstrap: [AppComponent]
