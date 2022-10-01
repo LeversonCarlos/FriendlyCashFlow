27d5414c5e8179931f85a180597153623fd9be0d
Leverson Carlos <lcjohnny@hotmail.com>
2022-10-01 11:07:08 -0300

    inject client services that will be used on the entire program

--- sources/Frontend/src/app/components/components.module.ts
+++ sources/Frontend/src/app/components/components.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { LayoutComponent } from './layout/layout.component';
+import { ApiClient } from './api-client';
 
 @NgModule({
    declarations: [
       LayoutComponent
    ],
    imports: [
-      CommonModule
+      CommonModule, HttpClientModule,
    ],
    exports: [
-      LayoutComponent
+      LayoutComponent, HttpClientModule,
+   ],
+   providers: [
+      ApiClient
    ]
 })
 export class ComponentsModule { }
