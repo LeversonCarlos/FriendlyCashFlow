cebcc38bb85c5fb8a6965d0d42c5f8519b606bc1
Leverson Carlos <lcjohnny@hotmail.com>
2022-10-01 10:45:34 -0300

    create the index component for the accounts module

--- sources/Frontend/src/app/modules/accounts/accounts.module.ts
+++ sources/Frontend/src/app/modules/accounts/accounts.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ComponentsModule } from '@components/components.module';
 
 import { AccountsRouting } from './accounts.routing';
 import { AccountsState, } from '@commands/accounts';
 import { NavigateCommandProvider, InitializeCommandProvider, ShowAccountsCommandProvider } from '@commands/accounts';
 
 import { ContainerComponent } from './views/container.component';
+import { IndexComponent } from './views/index/index.component';
 
 @NgModule({
    declarations: [
-      ContainerComponent
+      ContainerComponent,
+      IndexComponent
    ],
    imports: [
-      CommonModule,
+      CommonModule, ComponentsModule,
       AccountsRouting
    ],
    providers: [
