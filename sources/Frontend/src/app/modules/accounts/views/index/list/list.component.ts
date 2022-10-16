148bc9a4e312eff7153a414e7a1c85e733a7ed65
Leverson Carlos <lcjohnny@hotmail.com>
2022-10-16 09:06:09 -0300

    feat: initialize component for the acconts list

---
+++ sources/Frontend/src/app/modules/accounts/views/index/list/list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+   selector: 'app-index-list',
+   templateUrl: './list.component.html',
+   styleUrls: ['./list.component.scss']
+})
+export class IndexListComponent implements OnInit {
+
+   constructor() { }
+
+   ngOnInit(): void {
+   }
+
+}
