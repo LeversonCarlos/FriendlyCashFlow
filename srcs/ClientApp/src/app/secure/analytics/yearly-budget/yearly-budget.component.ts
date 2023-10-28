6624fa3bf1aa3779accf2eda6b2dd653eec5067a
Leverson Carlos <leverson.carlos@oransys.net>
2023-10-28 17:56:40 -0300

    place new compoente on screens component

--- srcs/ClientApp/src/app/secure/analytics/yearly-budget/yearly-budget.component.ts
+++ srcs/ClientApp/src/app/secure/analytics/yearly-budget/yearly-budget.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AnalyticsService } from '../analytics.service';
+import { Subscription } from 'rxjs';
 
 @Component({
    selector: 'fs-yearly-budget',
@@ -7,12 +9,24 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 })
 export class YearlyBudgetComponent implements OnInit, OnDestroy {
 
-   constructor() { }
+   constructor(
+      private service: AnalyticsService,
+   ) { }
 
    ngOnInit(): void {
+      this.OnDataRefreshedSubscription =
+         this.service.OnDataRefreshed.subscribe(x => this.OnDataRefreshed(x));
+   }
+
+   private OnDataRefreshedSubscription: Subscription
+   private OnDataRefreshed(val: boolean) {
+      // this.chart.show(this.service.YearlyBudget)
+      console.log(this.service.YearlyBudget);
    }
 
    ngOnDestroy(): void {
+      this.OnDataRefreshedSubscription?.unsubscribe()
+      this.OnDataRefreshedSubscription = null;
    }
 
 }
