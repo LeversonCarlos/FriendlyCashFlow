d02a8db8ce915d7103c5af315f39664460b9164b
Leverson Carlos <leverson.carlos@oransys.net>
2023-10-28 17:54:30 -0300

    create componente where to render the reports views

---
+++ srcs/ClientApp/src/app/secure/analytics/yearly-budget/yearly-budget.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+   selector: 'fs-yearly-budget',
+   templateUrl: './yearly-budget.component.html',
+   styleUrls: ['./yearly-budget.component.scss']
+})
+export class YearlyBudgetComponent implements OnInit, OnDestroy {
+
+   constructor() { }
+
+   ngOnInit(): void {
+   }
+
+   ngOnDestroy(): void {
+   }
+
+}
