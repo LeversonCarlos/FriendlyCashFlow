b8fc61372e464eb70f951099571b189405783d44
Leverson Carlos <leverson.carlos@oransys.net>
2023-10-29 09:14:07 -0300

    componente to present the budget totals

---
+++ srcs/ClientApp/src/app/secure/analytics/yearly-budget/totals/totals.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+   selector: 'fs-yearly-budget-totals',
+   templateUrl: './totals.component.html',
+   styleUrls: ['./totals.component.scss']
+})
+export class YearlyBudgetTotalsComponent implements OnInit {
+
+   constructor() { }
+
+   ngOnInit(): void {
+   }
+
+}
